62d9b76ee6eaf525dd2045c3f0b15cb3cec9d5e0
joebell93 <joe@joebell.co.uk>
2019-01-15 14:13:07 +0200

    feat(global): initialise with generic styles

---
+++ packages/global/src/index.ts
@@ -0,0 +1,9 @@
+import { boxSizing } from "./generic/box-sizing";
+import { fontFace } from "./generic/font-face";
+import { reset } from "./generic/reset";
+
+export const styles = [
+  fontFace, // Generic
+  boxSizing,
+  reset
+];
