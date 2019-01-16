f2d7124a48f103b18c77dde57b3f587eb397a04b
joebell93 <joe@joebell.co.uk>
2019-01-16 10:12:49 +0200

    feat(global): add elements

--- packages/global/src/index.ts
+++ packages/global/src/index.ts
@@ -1,9 +1,13 @@
 import { boxSizing } from "./generic/box-sizing";
 import { fontFace } from "./generic/font-face";
 import { reset } from "./generic/reset";
+import { page } from "./elements/page";
 
 export const styles = [
-  fontFace, // Generic
+  /* Generic */
+  fontFace,
   boxSizing,
-  reset
+  reset,
+  /* Elements */
+  page
 ];
