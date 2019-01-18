c860b646be79e139cca0e673eaf35b07cef44b72
joebell93 <joe@joebell.co.uk>
2019-01-18 17:06:48 +0200

    feat(card): create card component

---
+++ packages/components/src/card/card.tsx
@@ -0,0 +1,67 @@
+import * as React from "react";
+import { css } from "@emotion/core";
+import styled from "@emotion/styled";
+import { padding, rem } from "polished";
+import { border, colors } from "@heathmont/sportsbet-tokens";
+import { spacing } from "@heathmont/sportsbet-utils";
+import { cardGradient } from "./utils";
+
+export type CardProps = {
+  template: "front" | "back" | "outline";
+  children: JSX.Element[] | JSX.Element;
+  flex?: boolean;
+};
+
+/**
+ * Styles
+ */
+const cardBase = css({
+  display: "block",
+  minHeight: rem(170),
+  ...padding(spacing(), spacing("medium")),
+  position: "relative",
+  borderRadius: border.radius.default,
+  backgroundColor: colors.neutral[80]
+});
+
+/* Modifiers */
+const cardFlex = css({
+  display: "flex",
+  flexDirection: "column",
+  flexWrap: "wrap",
+  justifyContent: "space-between",
+  alignItems: "alignContent"
+});
+
+const cardOutline = css({
+  borderStyle: border.style,
+  borderWidth: border.width,
+  borderColor: colors.neutral[50]
+});
+
+const cardBack = css({ backgroundColor: colors.neutral[60] });
+const cardShadow = css({ boxShadow: "rgba(0, 0, 0, 0.25) 4px 4px 12px" });
+
+const cardModifiers = {
+  front: [cardGradient, cardShadow],
+  back: [cardBack, cardShadow],
+  outline: cardOutline
+};
+
+/**
+ * Component
+ */
+export const Card: React.SFC<CardProps> = ({ children, flex, template }) => {
+  const Card = styled("div")(
+    cardBase,
+    flex && cardFlex,
+    cardModifiers[template]
+  );
+
+  return <Card>{children}</Card>;
+};
+
+Card.defaultProps = {
+  flex: false,
+  template: "front"
+};
---
+++ packages/components/src/card/toggle.tsx
@@ -0,0 +1,37 @@
+/** @jsx jsx */ jsx;
+import * as React from "react";
+import { css, jsx } from "@emotion/core";
+import { spacing } from "@heathmont/sportsbet-utils";
+
+export type CardToggleProps = {
+  back?: boolean;
+};
+
+/**
+ * Styles
+ */
+const cardToggle = css({
+  position: "absolute",
+  top: spacing(),
+  right: spacing(),
+  background: "none",
+  padding: 0,
+  cursor: "pointer",
+  border: "none",
+  /* Temporary styles until Icons implemented */
+  fontSize: "0.9rem",
+  lineHeight: "1.4rem"
+});
+
+/**
+ * Component
+ */
+export const CardToggle: React.SFC<CardToggleProps> = ({ back }) => {
+  const icon = !back ? "⚙️" : "↩️";
+
+  return <button css={cardToggle}>{icon}</button>;
+};
+
+CardToggle.defaultProps = {
+  back: false
+};
