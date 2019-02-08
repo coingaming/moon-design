08e219ffeddc0743144b3b81322bcec261393f07
joebell93 <joe@joebell.co.uk>
2019-02-08 15:10:54 +0200

    feat(docs): implement code previews

--- docs/src/components/docs.tsx
+++ docs/src/components/docs.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import styled from '@emotion/styled';
 import { spacing } from '@heathmont/sportsbet-utils';
 import { border, colors } from '@heathmont/sportsbet-tokens';
-import { Children } from '../types';
 
 export const Playground = styled.div({
   position: 'relative',
