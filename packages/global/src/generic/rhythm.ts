56f723d466744ad7cec963a3887bbb53a0a0e790
joebell93 <joe@joebell.co.uk>
2019-02-18 17:03:09 +0200

    feat(global): implement vertical and horizontal rhythm

---
+++ packages/global/src/generic/rhythm.ts
@@ -0,0 +1,24 @@
+import { css } from '@emotion/core';
+import { rem } from 'polished';
+import { spacing as spacingTokens } from '@heathmont/sportsbet-tokens';
+import { spacing } from '@heathmont/sportsbet-utils';
+
+export const rhythm = css({
+  /**
+   * Apply default spacing to the bottom of all block-level elements for
+   * consistent vertical rhythm.
+   */
+  [`h1, h2, h3, h4, h5, h6, hgroup,
+    ul, ol, dl,
+    blockquote, p, address, table,
+    fieldset, figure,
+    pre`]: {
+    marginBottom: spacing(),
+  },
+  /**
+   * Indent all lists by default (with arbitrary extra pixels for alignment)
+   */
+  'ul, ol, dd': {
+    marginLeft: rem(spacingTokens.default + 2),
+  },
+});
