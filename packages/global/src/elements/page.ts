d6beb0ee88cf1fcd9e548b5893249ad87195db24
joebell93 <joe@joebell.co.uk>
2019-02-26 16:11:14 +0000

    style: switch to object styles

--- packages/global/src/elements/page.ts
+++ packages/global/src/elements/page.ts
@@ -2,22 +2,22 @@ import { css } from '@emotion/core';
 import { rem } from 'polished';
 import { base, colors, typography } from '@heathmont/sportsbet-tokens';
 
-export const page = css`
-  /**
-   * 1. Prevent automatic zooming of fonts on some mobile devices.
-   * 2. Consistent font display behavior on OSX.
-   */
-  html {
-    background-color: ${colors.neutral[80]};
-    font-size: ${rem(base.fontSize)};
-    line-height: ${base.lineHeight / base.fontSize};
-    font-family: ${typography.fontFamily};
-    color: ${colors.text};
-    min-height: 100%;
-    text-size-adjust: 100%; /* [1] */
-    font-display: swap;
-    -moz-osx-font-smoothing: grayscale; /* [2] */
-    -webkit-font-smoothing: antialiased; /* [2] */
-    text-rendering: optimizeLegibility;
-  }
-`;
+/**
+ * 1. Prevent automatic zooming of fonts on some mobile devices.
+ * 2. Consistent font display behavior on OSX.
+ */
+export const page = css({
+  html: {
+    backgroundColor: colors.neutral[80],
+    fontSize: rem(base.fontSize),
+    lineHeight: base.lineHeight / base.fontSize,
+    fontFamily: typography.fontFamily,
+    color: colors.text,
+    minHeight: '100%',
+    textSizeAdjust: '100%' /* [1] */,
+    fontDisplay: 'swap',
+    textRendering: 'optimizeLegibility',
+    '-moz-osx-font-smoothing': 'grayscale' /* [2] */,
+    '-webkit-font-smoothing': 'antialiased' /* [2] */,
+  },
+});
