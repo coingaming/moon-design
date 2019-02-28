d3f4e1139d0f2f3d312ba1ebbf232d1b50641c01
joebell93 <joe@joebell.co.uk>
2019-02-28 16:33:36 +0000

    feat(header): create header component

---
+++ packages/components/src/header/header.tsx
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+import { rem } from 'polished';
+import { typography, breakpoints } from '@heathmont/sportsbet-tokens';
+import { mq, spacing } from '@heathmont/sportsbet-utils';
+
+const Header = styled.header({
+  position: 'relative',
+  display: 'flex',
+  marginBottom: spacing('small'),
+  flexWrap: 'wrap',
+  alignItems: 'baseline',
+});
+
+const HeaderTitle = styled.h1({
+  display: 'inline-block',
+  marginRight: spacing('large'),
+  fontSize: rem(20),
+  lineHeight: rem(25),
+  fontWeight: typography.fontWeight.bold,
+  [mq(breakpoints.small)]: {
+    fontSize: rem(32),
+    lineHeight: rem(39),
+  },
+});
+
+export { Header, HeaderTitle };
