ae7f72438b24cc1200d06d59b54fe14931d4fd80
joebell93 <joe@joebell.co.uk>
2019-03-04 16:29:11 +0200

    refactor(wip-user-portal): create custom Emotion SSR config

--- packages/wip-user-portal/src/client.tsx
+++ packages/wip-user-portal/src/client.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { ClientSideApp } from './app/app';
+import { ClientSideApp } from './app';
 
 const app = document.getElementById('app');
 ReactDOM.hydrate(<ClientSideApp />, app);
