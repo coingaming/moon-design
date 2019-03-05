7fb3937f12d82d956c14dee9bd0f6e29981a564e
joebell93 <joe@joebell.co.uk>
2019-03-05 16:51:25 +0200

    chore(inlineSVG): switch to third-party encoder

--- packages/utils/src/background/index.ts
+++ packages/utils/src/background/index.ts
@@ -1,7 +1,8 @@
 import { renderToStaticMarkup } from 'react-dom/server';
 import { ReactElement } from 'react';
+import miniSvgDataUri from 'mini-svg-data-uri';
 
 const inlineSVG = (svg: ReactElement<string>) =>
-  `url('data:image/svg+xml,${encodeURIComponent(renderToStaticMarkup(svg))}')`;
+  `url("${miniSvgDataUri(renderToStaticMarkup(svg))}")`;
 
 export { inlineSVG };
