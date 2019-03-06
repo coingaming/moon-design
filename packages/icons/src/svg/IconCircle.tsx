da23b3b6ea12c820f0acf0545e2d75a5a1308a24
joebell93 <joe@joebell.co.uk>
2019-03-06 10:50:01 +0200

    feat(TextInput): add validation icons

---
+++ packages/icons/src/svg/IconCircle.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import styled from '@emotion/styled';
+
+const Svg = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <circle cx={10} cy={10} r={10} fill="currentColor" fillRule="evenodd" />
+  </svg>
+);
+
+export const IconCircle = styled(Svg)(
+  ({ backgroundColor }: { backgroundColor?: string }) => [
+    {
+      verticalAlign: 'middle',
+    },
+    backgroundColor && {
+      backgroundColor,
+      padding: backgroundColor ? '0.25em' : 0,
+      overflow: 'visible',
+      borderRadius: '50%',
+    },
+  ]
+);
