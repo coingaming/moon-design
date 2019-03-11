1b21af5050ace4730d3598688be5194a89ef71d5
joebell93 <joe@joebell.co.uk>
2019-03-11 17:02:19 +0200

    refactor: categorize packages into `design` and `portals`

--- design/icons/jest.config.js
+++ design/icons/jest.config.js
@@ -3,7 +3,7 @@ const base = require('../../.config/jest/jest.config.base.js');
 
 module.exports = {
   ...base,
-  roots: ['<rootDir>/packages/icons'],
+  roots: ['<rootDir>/design/icons'],
   name: package.name,
   displayName: package.name,
 };
