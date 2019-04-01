682126c6d93f10a80d0d2d7d4383e1e474d44cdc
Karl Kallavus <karl.kallavus@gmail.com>
2019-04-01 16:32:37 +0300

    feat(button): introduce outline prop

--- design/components/src/button/button.tsx
+++ design/components/src/button/button.tsx
@@ -6,6 +6,7 @@ import { border, typography, base } from '@heathmont/sportsbet-tokens';
 import { spacing, disabled } from '@heathmont/sportsbet-utils';
 
 import { buttonModifiers, ButtonModifiers } from './modifiers';
+import { outlineModifiers } from './outline';
 import { buttonMockStateClass, ButtonMockState } from './states';
 
 type ButtonProps = {
@@ -16,6 +17,7 @@ type ButtonProps = {
   mockState?: ButtonMockState;
   fullWidth?: boolean;
   uppercase?: boolean;
+  outline?: boolean;
   onClick?: () => void;
 };
 
@@ -50,6 +52,7 @@ const Button: React.FC<ButtonProps> = ({
   modifier,
   fullWidth,
   uppercase,
+  outline,
   ...props
 }) => {
   const ButtonElement = !href ? 'button' : 'a';
@@ -68,6 +71,7 @@ const Button: React.FC<ButtonProps> = ({
     css: [
       button,
       modifier && buttonModifiers[modifier],
+      outline && outlineModifiers[modifier],
       uppercase && { textTransform: 'uppercase' },
       fullWidth && { width: '100%' },
     ],
