96009783294590bc29dd84ea636b3809698e3f3b
Karl Kallavus <karl.kallavus@gmail.com>
2019-04-01 16:09:04 +0300

    feat(button): introduce uppercase prop

--- design/components/src/button/__tests__/button.test.tsx
+++ design/components/src/button/__tests__/button.test.tsx
@@ -29,6 +29,16 @@ describe('Button', () => {
     expect(button).toMatchSnapshot();
   });
 
+  test('renders uppercased', () => {
+    const button = create(
+      <Button modifier="primary" uppercase>
+        Uppercase Primary Button
+      </Button>
+    );
+
+    expect(button).toMatchSnapshot();
+  });
+
   test('renders as an anchor tag', () => {
     const button = create(
       <Button modifier="primary" href="#some-anchor">
