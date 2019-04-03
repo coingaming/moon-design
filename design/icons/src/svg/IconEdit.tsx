c0520ce597e3ba08ca7acbe07343de8bcf37ecd8
bilal-net <bilal.javed@heathmont.net>
2019-04-03 11:01:11 +0300

    feat(deposit): deposit view
    feat(deposit): approve payment method component
    
    feat(design): edit icon
    
    chore(user-portal): layout changes
    
    chore(deposit): changes related to deposit
    
    chore(deposit): minor fixes
    
    chore(deposit): fix margins colors paddings position in intial deposit
    
    fix(icons): rebuild icons
    
    Co-authored-by: Karl Kallavus <karl.kallavus@gmail.com>

---
+++ design/icons/src/svg/IconEdit.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import styled from '@emotion/styled';
+
+const Svg = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 128 512"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+    width="1em"
+    height="1em"
+  >
+    <path
+      fill="currentColor"
+      d="M64 208c26.5 0 48 21.5 48 48s-21.5 48-48 48-48-21.5-48-48 21.5-48 48-48zM16 104c0 26.5 21.5 48 48 48s48-21.5 48-48-21.5-48-48-48-48 21.5-48 48zm0 304c0 26.5 21.5 48 48 48s48-21.5 48-48-21.5-48-48-48-48 21.5-48 48z"
+    />
+  </svg>
+);
+
+export const IconEdit = styled(Svg)(
+  ({
+    backgroundColor,
+    circleColor,
+  }: {
+    backgroundColor?: string;
+    circleColor?: string;
+  }) => [
+    {
+      verticalAlign: 'middle',
+    },
+    backgroundColor && {
+      backgroundColor,
+      padding: backgroundColor ? '0.25em' : 0,
+      overflow: 'visible',
+      borderRadius: '50%',
+    },
+    circleColor && {
+      circle: {
+        fill: circleColor,
+      },
+    },
+  ]
+);
