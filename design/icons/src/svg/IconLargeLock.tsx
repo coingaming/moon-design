af4e0805816e123725be9522507f94c8e2211849
Karl Kallavus <karl.kallavus@gmail.com>
2019-04-08 17:41:32 +0300

    feat(icons): circle fill color implementation

--- design/icons/src/svg/IconLargeLock.tsx
+++ design/icons/src/svg/IconLargeLock.tsx
@@ -17,7 +17,13 @@ const Svg = (props: React.SVGProps<SVGSVGElement>) => (
 );
 
 export const IconLargeLock = styled(Svg)(
-  ({ backgroundColor }: { backgroundColor?: string }) => [
+  ({
+    backgroundColor,
+    circleColor,
+  }: {
+    backgroundColor?: string;
+    circleColor?: string;
+  }) => [
     {
       verticalAlign: 'middle',
     },
@@ -26,6 +32,11 @@ export const IconLargeLock = styled(Svg)(
       padding: backgroundColor ? '0.25em' : 0,
       overflow: 'visible',
       borderRadius: '50%',
+    },
+    circleColor && {
+      circle: {
+        fill: circleColor,
+      },
     },
   ]
 );
