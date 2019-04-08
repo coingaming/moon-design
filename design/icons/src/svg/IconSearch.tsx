7ad48944dd9e9d7ca45e1ba424270f8c4c939b11
Karl Kallavus <karl.kallavus@gmail.com>
2019-04-08 17:41:32 +0300

    feat(icons): circle fill color implementation

--- design/icons/src/svg/IconSearch.tsx
+++ design/icons/src/svg/IconSearch.tsx
@@ -24,7 +24,13 @@ const Svg = (props: React.SVGProps<SVGSVGElement>) => (
 );
 
 export const IconSearch = styled(Svg)(
-  ({ backgroundColor }: { backgroundColor?: string }) => [
+  ({
+    backgroundColor,
+    circleColor,
+  }: {
+    backgroundColor?: string;
+    circleColor?: string;
+  }) => [
     {
       verticalAlign: 'middle',
     },
@@ -33,6 +39,11 @@ export const IconSearch = styled(Svg)(
       padding: backgroundColor ? '0.25em' : 0,
       overflow: 'visible',
       borderRadius: '50%',
+    },
+    circleColor && {
+      circle: {
+        fill: circleColor,
+      },
     },
   ]
 );
