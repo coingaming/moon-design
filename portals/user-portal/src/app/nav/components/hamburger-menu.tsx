296a75f3b1e2b86f1a45a9e8d43a39e4104b0286
Karl Kallavus <karl.kallavus@gmail.com>
2019-04-14 00:34:45 +0300

    feat(wallets): overlay

--- portals/user-portal/src/app/nav/components/hamburger-menu.tsx
+++ portals/user-portal/src/app/nav/components/hamburger-menu.tsx
@@ -1,14 +1,67 @@
+/** @jsx jsx */ jsx;
 import * as React from 'react';
-import styled from '@emotion/styled';
+// tslint:disable-next-line:no-duplicate-imports
+import { useState } from 'react';
+import styled, { CSSObject } from '@emotion/styled';
+import { jsx } from '@emotion/core';
 import { colors } from '@heathmont/sportsbet-tokens';
-import { IconHamburger } from '../../../../../../design/icons/lib';
+import { IconHamburger, IconArrowLeft } from '@heathmont/sportsbet-icons';
 
-const StyledHamburgerIcon = styled.div({
+const StyledHamburgerIcon: CSSObject = {
   color: colors.neutral[20],
+};
+
+const Details = styled.details<{ open?: boolean }>(({ open }) => [
+  {
+    '&[open]': {
+      summary: {
+        position: 'absolute',
+        zIndex: 999,
+      },
+      backgroundColor: colors.neutral[90],
+      // '&::-webkit-details-marker': {
+      //   display: 'none',
+      // }
+    },
+  },
+]);
+
+const Summary = styled.summary({
+  '&::-webkit-details-marker': {
+    display: 'none',
+  },
 });
 
-export const HamburgerMenu = () => (
-  <StyledHamburgerIcon>
-    <IconHamburger />
-  </StyledHamburgerIcon>
-);
+const DetailsDialog = styled.div({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100vh',
+  background: colors.neutral[90],
+});
+
+export const HamburgerMenu = () => {
+  const [open, toggle] = useState(false);
+
+  return (
+    <Details open={open}>
+      <Summary
+        onClick={() => {
+          toggle(!open);
+        }}
+      >
+        {open ? (
+          <IconArrowLeft css={StyledHamburgerIcon} />
+        ) : (
+          <IconHamburger css={StyledHamburgerIcon} />
+        )}
+      </Summary>
+      <DetailsDialog>
+        <p className="mr-6 pr-3">
+          But you can click anywhere to dismiss without JS.
+        </p>
+      </DetailsDialog>
+    </Details>
+  );
+};
