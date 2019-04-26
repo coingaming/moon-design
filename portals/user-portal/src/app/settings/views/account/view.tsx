7630e1db537fa0981f38266c23b8cbd3ab3057ec
Jaan Tihvan <jtihvan@MBP-JTIHVAN.local>
2019-04-26 14:39:13 +0300

    feat(Account-initial): Initial commit, verifying approach legitimacy

--- portals/user-portal/src/app/settings/views/account/view.tsx
+++ portals/user-portal/src/app/settings/views/account/view.tsx
@@ -3,6 +3,7 @@ import {
   Heading,
   Form,
   FormItem,
+  FileInput,
   TextInput,
   Button,
 } from '@heathmont/sportsbet-components';
@@ -12,11 +13,55 @@ import {
   SettingsSection,
   SettingsBlock,
   BlockContent,
+  BlockColumn,
   DetailRow,
   CashierLayout,
   CashierHeading,
   HeadingDescription,
 } from '@heathmont/sportsbet-user-portal-components';
+import styled from '@emotion/styled';
+import { mq } from '@heathmont/sportsbet-utils';
+import { border, breakpoints, colors } from '@heathmont/sportsbet-tokens';
+import { spacing } from '@heathmont/sportsbet-utils/lib';
+
+const ContentWrapper = styled.div((props: any) => ({
+  display: 'flex',
+  'flex-direction': 'column',
+  ...props.css,
+  [mq(breakpoints.medium)]: {
+    'align-items': 'center',
+    'flex-direction': 'row',
+    'flex-wrap': 'wrap',
+    ...props.cssMqMedium,
+  },
+}));
+
+const DynamicContentItem = styled.div((props: any) => ({
+  order: 0,
+  margin: props.margin || 0,
+  'flex-basis': '100%',
+  ...props.css,
+  [mq(breakpoints.medium)]: {
+    'flex-basis': 'auto',
+    ...props.cssMqMedium,
+    order: props.order,
+    margin: props.mqMediumMargin || props.margin || 0,
+  },
+}));
+
+const Container = styled.div((props: any) => ({
+  ...props.css,
+  [mq(breakpoints.medium)]: {
+    ...props.cssMqMedium,
+    'max-width': props.maxWidth || 'auto',
+    width: props.width || 'auto',
+  },
+}));
+
+const GoogleAuthenticator = styled.div(() => ({
+  border: `${border.width}px ${border.style} ${colors.neutral[50]}`,
+  padding: spacing('default'),
+}));
 
 export const AccountView = () => (
   <CashierLayout>
@@ -33,19 +78,33 @@ export const AccountView = () => (
       </SettingsBlock>
       <SettingsBlock>
         <BlockContent>
-          <Form legend="Resend the Email" maxWidth="25rem">
-            <FormItem>
-              <TextInput label="E-mail address" type="email" required />
-            </FormItem>
-            <Button modifier="highlight">Resend the Email</Button>
-          </Form>
-          <SettingsText>
-            We've sent a message to you with a link to activate your account.
-          </SettingsText>
-          <SettingsText>
-            If you don't see an email from us within a few minutes, be sure to
-            check your spam folder.
-          </SettingsText>
+          <ContentWrapper>
+            <DynamicContentItem>
+              <Container maxWidth="25rem">
+                <Form legend="Resend the Email">
+                  <FormItem>
+                    <TextInput label="E-mail address" type="email" required />
+                  </FormItem>
+                </Form>
+              </Container>
+            </DynamicContentItem>
+            <DynamicContentItem order={2} margin={`${spacing('medium')} 0`}>
+              <SettingsText>
+                We've sent a message to you with a link to activate your
+                account.
+              </SettingsText>
+              <SettingsText>
+                If you don't see an email from us within a few minutes, be sure
+                to check your spam folder.
+              </SettingsText>
+            </DynamicContentItem>
+            <DynamicContentItem
+              order={1}
+              mqMediumMargin={`0 0 0 ${spacing('large')}`}
+            >
+              <Button modifier="highlight">Resend the Email</Button>
+            </DynamicContentItem>
+          </ContentWrapper>
         </BlockContent>
       </SettingsBlock>
       <SettingsBlock>
@@ -90,18 +149,28 @@ export const AccountView = () => (
         </Heading>
       </SettingsBlock>
       <SettingsBlock>
-        <Form legend="Enter you current password" maxWidth="25rem">
-          <FormItem>
-            <TextInput label="Enter you current password" type="password" />
-          </FormItem>
-          <FormItem>
-            <TextInput label="Enter new password" type="password" />
-          </FormItem>
-          <FormItem>
-            <TextInput label="Repeat password" type="password" />
-          </FormItem>
-          <Button modifier="secondary">Save Changes</Button>
-        </Form>
+        {/*<BlockContent>*/}
+        <Container maxWidth="25rem">
+          <Form legend="Enter you current password">
+            <FormItem>
+              <TextInput label="Enter you current password" type="password" />
+            </FormItem>
+            <FormItem>
+              <TextInput label="Enter new password" type="password" />
+            </FormItem>
+            <FormItem>
+              <TextInput label="Repeat password" type="password" />
+            </FormItem>
+            <FormItem>
+              <Container width="10rem">
+                <Button fullWidth modifier="secondary">
+                  Save Changes
+                </Button>
+              </Container>
+            </FormItem>
+          </Form>
+        </Container>
+        {/*</BlockContent>*/}
       </SettingsBlock>
     </SettingsSection>
     <SettingsSection>
@@ -111,19 +180,37 @@ export const AccountView = () => (
         </Heading>
       </SettingsBlock>
       <SettingsBlock>
-        <SettingsText>Key value: PBVWSRCIOZJG4SLUPJYVIVCC</SettingsText>
+        <BlockContent>
+          <ContentWrapper cssMqMedium={{ 'align-items': 'flex-start' }}>
+            <DynamicContentItem>
+              <GoogleAuthenticator>
+                What is google authenticator
+              </GoogleAuthenticator>
+            </DynamicContentItem>
+            <DynamicContentItem>
+              <Container
+                maxWidth="25rem"
+                css={{ margin: `${spacing('medium')} 0` }}
+                cssMqMedium={{ margin: `0 ${spacing('large')} 0` }}
+              >
+                <SettingsText>Key value: PBVWSRCIOZJG4SLUPJYVIVCC</SettingsText>
+                <Form legend="Type code">
+                  <FormItem>
+                    <TextInput label="Type code" type="text" required />
+                  </FormItem>
+                  <FormItem>
+                    <Container width="10rem">
+                      <Button fullWidth modifier="secondary">
+                        Submit Code
+                      </Button>
+                    </Container>
+                  </FormItem>
+                </Form>
+              </Container>
+            </DynamicContentItem>
+          </ContentWrapper>
+        </BlockContent>
       </SettingsBlock>
-      <SettingsBlock>
-        <Form legend="Type code" maxWidth="25rem">
-          <FormItem>
-            <TextInput label="Type code" type="text" required />
-            <Button modifier="secondary">Submit Code</Button>
-          </FormItem>
-        </Form>
-      </SettingsBlock>
-      <FormItem>
-        <TextInput label="Text" type="text" required />
-      </FormItem>
     </SettingsSection>
     <SettingsSection>
       <SettingsBlock>
@@ -140,9 +227,54 @@ export const AccountView = () => (
           your name.
         </SettingsText>
       </SettingsBlock>
+      <SettingsBlock>
+        <BlockContent>
+          <Form fullWidth legend="KYC file upload">
+            <Container
+              css={{
+                display: 'flex',
+                'flex-direction': 'column',
+              }}
+              cssMqMedium={{ 'flex-direction': 'row' }}
+            >
+              <Container cssMqMedium={{ 'flex-basis': '20rem' }}>
+                <FormItem>
+                  <FileInput placeholder="ID, driver's license or passport" />
+                </FormItem>
+              </Container>
+              <Container
+                css={{ marginTop: '1rem' }}
+                cssMqMedium={{
+                  'flex-basis': '20rem',
+                  margin: `0 ${spacing('large')} 0 0`,
+                }}
+              >
+                <FormItem>
+                  <FileInput placeholder="Photo of a utility bill" />
+                </FormItem>
+              </Container>
+            </Container>
+          </Form>
+          <Container
+            css={{ margin: `${spacing('large')} 0` }}
+            cssMqMedium={{ margin: `${spacing('medium')} 0` }}
+          >
+            <SettingsText>
+              Supported file types: .jpg, .png and .gif. Both documents are
+              required.
+            </SettingsText>
+          </Container>
+        </BlockContent>
+      </SettingsBlock>
     </SettingsSection>
-    <Button modifier="secondary" outline>
-      Delete Account
-    </Button>
+    <Container
+      width="12rem"
+      css={{ margin: `${spacing('medium')}` }}
+      cssMqMedium={{ margin: `${spacing('medium')} 0` }}
+    >
+      <Button fullWidth modifier="secondary" outline>
+        Delete Account
+      </Button>
+    </Container>
   </CashierLayout>
 );
