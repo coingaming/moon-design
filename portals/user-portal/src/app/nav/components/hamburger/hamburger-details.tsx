3f6c7496e506c16e3fe776ba4e0c428b86998128
Karl Kallavus <karl.kallavus@gmail.com>
2019-05-14 15:50:00 +0300

    fix(nav): nav details menu items pixel perfect

--- portals/user-portal/src/app/nav/components/hamburger/hamburger-details.tsx
+++ portals/user-portal/src/app/nav/components/hamburger/hamburger-details.tsx
@@ -76,8 +76,7 @@ const column: CSSObject = {
 const Balance = styled.p({
   color: colors.neutral[10],
   fontSize: rem(24),
-  lineHeight: rem(30),
-  // marginBottom: spacing('small'),
+  lineHeight: '2rem',
   '&:hover': {
     color: colors.neutral[10],
   },
@@ -104,6 +103,10 @@ const Icon = styled.div({
   },
 });
 
+const LinkItem = styled.p({
+  marginTop: 0,
+});
+
 export const HamburgerDetails = () => {
   return (
     <DetailsContainer>
@@ -123,19 +126,19 @@ export const HamburgerDetails = () => {
       >
         <Link css={column} href="#" optional>
           <Balance>1535.82</Balance>
-          Balance
+          <LinkItem>Balance</LinkItem>
         </Link>
         <Link css={column} href="#" optional>
           <IconNotification
             css={{ fontSize: '1.5rem', marginBottom: spacing('small') }}
           />
-          Notification
+          <LinkItem>Notifications</LinkItem>
         </Link>
         <Link css={column} href="#" optional>
           <IconLiveChat
             css={{ fontSize: '1.5rem', marginBottom: spacing('small') }}
           />
-          Live chat
+          <LinkItem>Live chat</LinkItem>
         </Link>
       </div>
       <List>
