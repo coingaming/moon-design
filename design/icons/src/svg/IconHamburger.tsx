e40a8c315ae026f069156cdaad0597d7733e67f2
Karl Kallavus <karl.kallavus@gmail.com>
2019-05-15 12:02:35 +0300

    feat(icons): update hamburger & logo icons

--- design/icons/src/svg/IconHamburger.tsx
+++ design/icons/src/svg/IconHamburger.tsx
@@ -5,14 +5,14 @@ const Svg = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     width="1em"
     height="1em"
-    viewBox="0 0 16 15"
+    viewBox="0 0 16 14"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <rect y={0.094} width={16} height={2} rx={1} fill="currentColor" />
-    <rect x={4} y={6.094} width={12} height={2} rx={1} fill="currentColor" />
-    <rect x={7} y={12.094} width={9} height={2} rx={1} fill="currentColor" />
+    <rect width={16} height={2} rx={1} fill="currentColor" />
+    <rect y={6} width={16} height={2} rx={1} fill="currentColor" />
+    <rect y={12} width={16} height={2} rx={1} fill="currentColor" />
   </svg>
 );
 
