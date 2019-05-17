bc86d84619652406b2482f75fadef30bb832f9b5
Karl Kallavus <karl.kallavus@gmail.com>
2019-05-17 07:04:07 +0300

    feat(nav): add nav bar everywhere

--- portals/user-portal/src/app/cashier/views/my-bets/view.tsx
+++ portals/user-portal/src/app/cashier/views/my-bets/view.tsx
@@ -1,5 +1,7 @@
 /** @jsx jsx */
 import * as React from 'react';
+// tslint:disable-next-line:no-duplicate-imports
+import { useState } from 'react';
 import { jsx } from '@emotion/core';
 import {
   CashierLayout,
@@ -7,14 +9,41 @@ import {
   HeadingDescription,
 } from '@heathmont/sportsbet-user-portal-components';
 import { MyBetsNav } from './dumb-components/my-bets-nav';
+import { container } from '@heathmont/sportsbet-utils/lib/container';
+import { LoggedInNav } from '../../../nav/dump-components/logged-in-nav';
+import { NonLoggedNav } from '../../../nav/dump-components/non-logged-in-nav';
 jsx;
 
 export const MyBetsView = () => {
+  const [isUserLoggedIn, login] = useState(false);
+
   return (
-    <CashierLayout>
-      <MyBetsNav />
-      <CashierHeading>My bets</CashierHeading>
-      <HeadingDescription>In progress</HeadingDescription>
-    </CashierLayout>
+    <React.Fragment>
+      {isUserLoggedIn ? (
+        <LoggedInNav />
+      ) : (
+        <NonLoggedNav login={() => login(true)} />
+      )}
+      <div
+        css={[
+          {
+            position: 'sticky',
+            top: 0,
+            zIndex: 1,
+          },
+          { ...container('default') },
+          {
+            overflowX: 'auto',
+          },
+        ]}
+      >
+        <MyBetsNav />
+      </div>
+
+      <CashierLayout>
+        <CashierHeading>My bets</CashierHeading>
+        <HeadingDescription>In progress</HeadingDescription>
+      </CashierLayout>
+    </React.Fragment>
   );
 };
