78c2d6fea057ed65cd174d22a7dd80f2cdd8870e
Jaan Tihvan <jtihvan@MBP-JTIHVAN.local>
2019-05-28 11:03:29 +0300

    refactor: pr tidy

--- portals/user-portal/src/app/cashier/views/my-bets/dumb-components/my-bets-header.tsx
+++ portals/user-portal/src/app/cashier/views/my-bets/dumb-components/my-bets-header.tsx
@@ -2,13 +2,14 @@ import styled from '@emotion/styled';
 import rem from 'polished/lib/helpers/rem';
 import { inlineSVG, mq, spacing } from '@heathmont/sportsbet-utils';
 import * as React from 'react';
+import { colors } from '@heathmont/sportsbet-tokens';
 
 export const MyBetsHeader = styled.div({
   marginTop: spacing('large'),
   display: 'grid',
   height: rem(48),
   padding: spacing(),
-  backgroundColor: '#1A212A',
+  backgroundColor: colors.secondary,
   gridTemplateColumns: '5fr 1fr 1fr 1fr 2fr',
   gridTemplateRows: '100%',
   alignItems: 'center',
