cec5af5e1c5bf9018ec3ebbd95f369d301417f37
joe-bell <joe@joebell.co.uk>
2019-05-29 13:04:14 +0300

    refactor(docs): symlink to assets

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -9,6 +9,10 @@ module.exports = {
     description,
     author,
   },
+  proxy: {
+    prefix: '/assets/fonts',
+    url: 'https://localhost:8000',
+  },
   plugins: [
     `gatsby-plugin-emotion`,
     {
