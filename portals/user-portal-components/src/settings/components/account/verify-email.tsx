868cd56d06e55873c1ef8202184ff501c2693bfb
Karl Kallavus <karl.kallavus@gmail.com>
2019-05-31 01:23:50 +0300

    feat(account): verify email view

---
+++ portals/user-portal-components/src/settings/components/account/verify-email.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import styled from '@emotion/styled';
+import rem from 'polished/lib/helpers/rem';
+import { border, colors, breakpoints } from '@heathmont/sportsbet-tokens';
+import { spacing, mq, inlineSVG } from '@heathmont/sportsbet-utils';
+import { Background1 } from '@heathmont/sportsbet-icons/lib/backgrounds/Background1';
+
+export const VerifyEmailContainer = styled.div({
+  marginTop: rem(40),
+  display: 'flex',
+  border: `${border.width}px solid ${colors.neutral[70]}`,
+  borderRadius: border.radius.small,
+  paddingLeft: spacing('small'),
+  paddingRight: spacing('small'),
+  width: '100%',
+  height: rem(400),
+  backgroundSize: `${rem(736)} ${rem(176)}`,
+  backgroundRepeat: 'no-repeat',
+  backgroundPositionX: 'left',
+  backgroundPositionY: 'top',
+  backgroundImage: inlineSVG(<Background1 />),
+  [mq(breakpoints.medium)]: {
+    backgroundSize: `${rem(660)} ${rem(290)}`,
+    backgroundPositionX: rem(370),
+    backgroundPositionY: 'center',
+    height: rem(168),
+    paddingLeft: rem(44),
+    paddingRight: rem(44),
+  },
+});
+
+export const VerifyEmailContent = styled.div({
+  marginTop: rem(120),
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+  [mq(breakpoints.medium)]: {
+    marginTop: 0,
+    flexDirection: 'row',
+  },
+});
+
+export const VerifyEmailTextContainer = styled.div({
+  textAlign: 'center',
+  [mq(breakpoints.medium)]: {
+    textAlign: 'left',
+    maxWidth: rem(530),
+  },
+});
+
+export const VerifyEmailTextHeader = styled.p({
+  fontSize: rem(20),
+});
+
+export const VerifyEmailTextContent = styled.p({
+  color: colors.neutral[20],
+  marginTop: spacing(),
+  lineHeight: rem(24),
+  [mq(breakpoints.medium)]: {
+    marginTop: spacing('small'),
+  },
+});
+
+export const VerifyEmailButtonWrapper = styled.div({
+  display: 'flex',
+  padding: spacing('small'),
+  background: colors.neutral[80],
+  borderRadius: rem(100),
+  marginTop: spacing('large'),
+  [mq(breakpoints.medium)]: {
+    marginLeft: 'auto',
+    marginTop: 0,
+  },
+});
---
+++ portals/user-portal/src/app/settings/views/account/account-edit-view.tsx
@@ -0,0 +1,276 @@
+/** @jsx jsx */
+import { useState, default as React } from 'react';
+import { jsx } from '@emotion/core';
+import rem from 'polished/lib/helpers/rem';
+import {
+  Heading,
+  Form,
+  FormItem,
+  FileInput,
+  TextInput,
+  Button,
+} from '@heathmont/sportsbet-components';
+import { AccountNav } from './dumb-components/header';
+import {
+  SettingsText,
+  SettingsSection,
+  SettingsBlock,
+  CashierLayout,
+  StickyNav,
+  ResendEmailContainer,
+  ResendEmailInputFormItem,
+  SubmitResendEmailFormItem,
+  ResendEmailText,
+  ChangeUserDetailsContainer,
+  ChangeUserDetailsColumns,
+  ChangeUserDetailsFirstColumn,
+  ChangeUserDetailsSecondColumn,
+  ChangeUserDetailsSaveButton,
+  ChangePasswordContainer,
+  ChangePasswordSaveButton,
+  GoogleAuthenticatorContainer,
+  GoogleAuthenticatorText,
+  GoogleAuthenticator,
+  CollapsibleItem,
+  Separator,
+  SettingsBlockCollapsible,
+  SettingsBlockCollapsibleContents,
+  SummaryAccount,
+} from '@heathmont/sportsbet-user-portal-components';
+import { spacing, rhythm, container, mq } from '@heathmont/sportsbet-utils/lib';
+import { LoggedInNav } from '../../../nav/dump-components/logged-in-nav';
+import { NonLoggedNav } from '../../../nav/dump-components/non-logged-in-nav';
+import { breakpoints } from '@heathmont/sportsbet-tokens';
+jsx;
+
+export const AccountView = () => {
+  const [isUserLoggedIn, login] = useState(false);
+  // const [isGeneralOpened, toggleGeneral] = useState(true);
+  // const [isChangePasswordOpened, toggleChangePassword] = useState(true);
+  // const [isGoogleAuthOpened, toggleGoogleAuth] = useState(true);
+  // const [isVerifyAccountOpened, toggleVerifyAccount] = useState(true);
+
+  return (
+    <React.Fragment>
+      {isUserLoggedIn ? (
+        <LoggedInNav />
+      ) : (
+        <NonLoggedNav login={() => login(true)} />
+      )}
+      <StickyNav>
+        <AccountNav />
+      </StickyNav>
+
+      <CashierLayout>
+        <SettingsSection>
+          <SettingsBlockCollapsible>
+            <SummaryAccount>
+              <Heading size="delta" as="h2">
+                General information
+              </Heading>
+            </SummaryAccount>
+
+            <SettingsBlockCollapsibleContents>
+              <Form fullWidth legend="Resend the Email">
+                <ResendEmailContainer>
+                  <ResendEmailInputFormItem>
+                    <TextInput label="E-mail address" type="email" required />
+                  </ResendEmailInputFormItem>
+
+                  <SubmitResendEmailFormItem>
+                    <Button round type="submit" modifier="secondary">
+                      Resend the Email
+                    </Button>
+                  </SubmitResendEmailFormItem>
+                </ResendEmailContainer>
+              </Form>
+
+              <ResendEmailText>
+                <SettingsText>
+                  We've sent a message to you with a link to activate your
+                  account.
+                </SettingsText>
+                <SettingsText>
+                  If you don't see an email from us within a few minutes, be
+                  sure to check your spam folder.
+                </SettingsText>
+              </ResendEmailText>
+            </SettingsBlockCollapsibleContents>
+
+            <Separator />
+            <SettingsBlockCollapsibleContents>
+              <Form fullWidth legend="Change user details">
+                <ChangeUserDetailsContainer>
+                  <ChangeUserDetailsColumns>
+                    <ChangeUserDetailsFirstColumn>
+                      <FormItem>
+                        <TextInput
+                          disabled
+                          label="Username"
+                          type="text"
+                          required
+                        />
+                      </FormItem>
+                      <FormItem>
+                        <TextInput label="Phone number" type="tel" />
+                      </FormItem>
+                      <FormItem>
+                        <TextInput label="City" type="text" />
+                      </FormItem>
+                      <FormItem>
+                        <TextInput label="Zip code" type="number" />
+                      </FormItem>
+                    </ChangeUserDetailsFirstColumn>
+                    <ChangeUserDetailsSecondColumn>
+                      <FormItem>
+                        <TextInput disabled label="Date of birth" type="text" />
+                      </FormItem>
+                      <FormItem>
+                        <TextInput label="Address" type="text" />
+                      </FormItem>
+                      <FormItem>
+                        <TextInput label="Country" type="text" />
+                      </FormItem>
+                      <FormItem>
+                        <TextInput label="E-mail address" type="email" />
+                      </FormItem>
+                    </ChangeUserDetailsSecondColumn>
+                  </ChangeUserDetailsColumns>
+                  <ChangeUserDetailsSaveButton>
+                    <FormItem>
+                      <Button round type="submit" modifier="primary">
+                        Save Changes
+                      </Button>
+                    </FormItem>
+                  </ChangeUserDetailsSaveButton>
+                </ChangeUserDetailsContainer>
+              </Form>
+            </SettingsBlockCollapsibleContents>
+          </SettingsBlockCollapsible>
+        </SettingsSection>
+        <SettingsSection>
+          <SettingsBlockCollapsible>
+            <SummaryAccount>
+              <Heading size="delta" as="h2">
+                Change password
+              </Heading>
+            </SummaryAccount>
+            <SettingsBlockCollapsibleContents>
+              <ChangePasswordContainer>
+                <Form legend="Enter you current password">
+                  <FormItem>
+                    <TextInput
+                      label="Enter you current password"
+                      type="password"
+                    />
+                  </FormItem>
+                  <FormItem>
+                    <TextInput label="Enter new password" type="password" />
+                  </FormItem>
+                  <FormItem>
+                    <TextInput label="Repeat password" type="password" />
+                  </FormItem>
+                  <FormItem>
+                    <ChangePasswordSaveButton>
+                      <Button fullWidth round modifier="primary">
+                        Save Changes
+                      </Button>
+                    </ChangePasswordSaveButton>
+                  </FormItem>
+                </Form>
+              </ChangePasswordContainer>
+            </SettingsBlockCollapsibleContents>
+          </SettingsBlockCollapsible>
+        </SettingsSection>
+        <SettingsSection>
+          <SettingsBlockCollapsible>
+            <SummaryAccount>
+              <Heading size="delta" as="h2">
+                Google Authenticator
+              </Heading>
+            </SummaryAccount>
+            <SettingsBlockCollapsibleContents>
+              <GoogleAuthenticator
+                caption={'What is google authenticator'}
+                link={
+                  'https://support.google.com/accounts/answer/1066447?co=GENIE.Platform%3DAndroid&hl=en'
+                }
+              >
+                <img
+                  alt="qr code"
+                  src="https://randomqr.com/assets/images/randomqr-256.png"
+                />
+              </GoogleAuthenticator>
+              <GoogleAuthenticatorContainer>
+                <GoogleAuthenticatorText>
+                  <SettingsText>
+                    Key value: PBVWSRCIOZJG4SLUPJYVIVCC
+                  </SettingsText>
+                </GoogleAuthenticatorText>
+                <Form legend="Type code">
+                  <FormItem>
+                    <TextInput label="Type code" type="text" required />
+                  </FormItem>
+                  <FormItem>
+                    <CollapsibleItem>
+                      <Button fullWidth round modifier="secondary">
+                        Submit Code
+                      </Button>
+                    </CollapsibleItem>
+                  </FormItem>
+                </Form>
+              </GoogleAuthenticatorContainer>
+            </SettingsBlockCollapsibleContents>
+          </SettingsBlockCollapsible>
+        </SettingsSection>
+        {/* <SettingsSection>
+          <SettingsBlockCollapsible>
+            <SummaryAccount>
+              <Heading size="delta" as="h2">
+                Verify Account
+              </Heading>
+            </SummaryAccount>
+            <SettingsBlock>
+              <SettingsText>
+                We ask you to upload your documents for our review to provide
+                better security. Please send us a photo of either of your ID
+                card, driver’s license or passport. For proof of address, please
+                send us a photo of a utility bill dated within the last three
+                months and clearly showing your name.
+              </SettingsText>
+            </SettingsBlock>
+            <SettingsBlockCollapsibleContents>
+              <Form fullWidth legend="KYC file upload">
+                <CollapsibleItem css={{ display: 'flex', flexFlow: 'wrap' }}>
+                  <CollapsibleItem cssMqMedium={{ flexBasis: rem(320) }}>
+                    <FormItem>
+                      <FileInput placeholder="ID, driver's license or passport" />
+                    </FormItem>
+                  </CollapsibleItem>
+                  <CollapsibleItem
+                    css={rhythm()}
+                    cssMqMedium={{
+                      flexBasis: rem(320),
+                      margin: `0 0 0 ${spacing('large')} `,
+                    }}
+                  >
+                    <FormItem>
+                      <FileInput placeholder="Photo of a utility bill" />
+                    </FormItem>
+                  </CollapsibleItem>
+                </CollapsibleItem>
+              </Form>
+              <CollapsibleItem css={rhythm()}>
+                <SettingsText>
+                  Supported file types: .jpg, .png and .gif. Both documents are
+                  required.
+                </SettingsText>
+              </CollapsibleItem>
+            </SettingsBlockCollapsibleContents>
+          </SettingsBlockCollapsible>
+        </SettingsSection>
+                  */}
+      </CashierLayout>
+    </React.Fragment>
+  );
+};
