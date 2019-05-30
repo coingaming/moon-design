f2c5d648d7fad270d92c09fc80161564b88ec18d
Karl Kallavus <karl.kallavus@gmail.com>
2019-05-30 17:18:38 +0300

    feat(icons): add background icon

---
+++ design/icons/src/config/template-backgrounds.js
@@ -0,0 +1,20 @@
+const prefix = componentName => componentName.name.replace('Svg', '');
+
+function template(
+  { template },
+  opts,
+  { imports, componentName, props, jsx, exports }
+) {
+  const typeScriptTpl = template.smart({ plugins: ['typescript'] });
+  return typeScriptTpl.ast`
+    import * as React from 'react';
+    import styled from '@emotion/styled';
+
+    const Svg = (props: React.SVGProps<SVGSVGElement>) => ${jsx};
+
+    export const ${prefix(componentName)} = styled(Svg)({
+      verticalAlign: 'middle',
+    });
+  `;
+}
+module.exports = template;
