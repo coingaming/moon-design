4f5e0af9a9ac65526ff5c765ab264305865f1143
Peeter Tomberg <peeter.tomberg@gmail.com>
2019-05-31 14:11:13 +0300

    chore: working on inital docker assets release

--- .config/nginx/entrypoint.sh
+++ .config/nginx/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 export NAMESERVERS=$(cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}' | tr '\n' ' ')
-
 envsubst '$NAMESERVERS' < /config/nginx.conf.tmpl > /etc/nginx/nginx.conf
-
-# Replace enviromental variables in the index template
-cat /etc/nginx/nginx.conf
-cat /usr/share/nginx/html/index.html
 
 exec nginx -g "daemon off;"
