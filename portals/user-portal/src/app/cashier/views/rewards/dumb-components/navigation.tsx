42bb8571ee1eee5b9bfdbc233fceda14c91c68e6
Jaan Tihvan <jtihvan@MBP-JTIHVAN.local>
2019-06-03 11:07:08 +0300

    refactor: rename navigation for consistency

--- portals/user-portal/src/app/cashier/views/rewards/dumb-components/navigation.tsx
+++ portals/user-portal/src/app/cashier/views/rewards/dumb-components/navigation.tsx
@@ -5,15 +5,16 @@ import {
   CloseButton,
 } from '@heathmont/sportsbet-user-portal-components';
 
-export const NotificationsNav = () => (
+export const RewardsNav = () => (
   <Tab>
     <TabItem to={'/cashier'}>Wallet</TabItem>
-    <TabItem to={'/my-bets'}>My bets</TabItem>
-    <TabItem to={'/account'}>Account</TabItem>
-    <TabItem active to={'/notifications'}>
-      Notifications
+    <TabItem active to={'/my-bets'}>
+      My bets
     </TabItem>
+    <TabItem to={'/account'}>Account</TabItem>
+    <TabItem to={'/notifications'}>Notifications</TabItem>
     <TabItem to={'/settings'}>Settings</TabItem>
+    <TabItem to={'/rewards'}>Rewards</TabItem>
     <TabItem to={'#'} rightAligned>
       Log out
     </TabItem>
