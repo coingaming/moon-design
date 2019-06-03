407e7b025c543dd5def2942f2117dcf781772c02
Karl Kallavus <karl.kallavus@gmail.com>
2019-06-03 14:38:44 +0300

    feat(button): border.radius.largest by default

--- design/components/src/badge/badge.tsx
+++ design/components/src/badge/badge.tsx
@@ -3,14 +3,14 @@ import * as React from 'react';
 import { jsx } from '@emotion/core';
 import { CSSObject } from '@emotion/styled';
 import rem from 'polished/lib/helpers/rem';
-import { colors } from '@heathmont/sportsbet-tokens';
+import { colors, border } from '@heathmont/sportsbet-tokens';
 import { spacing } from '@heathmont/sportsbet-utils';
 
 const badgeStyle: CSSObject = {
   padding: `0.2rem ${spacing('small')}`,
   textAlign: 'center',
   textTransform: 'uppercase',
-  borderRadius: rem(100),
+  borderRadius: border.radius.largest,
   fontSize: rem(10),
   lineHeight: rem(10),
   letterSpacing: rem(1),
