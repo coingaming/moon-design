1d0b95dd5aeec19fc371a1e89a11a14f29d13358
Karl Kallavus <karl.kallavus@gmail.com>
2019-06-04 08:16:37 +0300

    feat(button): introduce sizes

--- design/components/src/button/size.ts
+++ design/components/src/button/size.ts
@@ -13,25 +13,25 @@ type ButtonSizes = 'xsmall' | 'small' | 'medium' | 'large';
 const xsmall: CSSObject = {
   fontSize: rem(12),
   lineHeight: rem(15),
-  padding: `${spacing('small')} ${spacing('medium')}`,
+  padding: `${spacing('xsmall')} ${spacing('medium')}`,
 };
 
 const small: CSSObject = {
   fontSize: rem(14),
   lineHeight: rem(17),
-  padding: `${rem(12)} ${spacing('large')}`,
+  padding: `${spacing('small')} ${spacing('large')}`,
 };
 
 const medium: CSSObject = {
-  fontSize: rem(14),
+  fontSize: rem(16),
   lineHeight: rem(17),
-  padding: `${rem(12)} ${spacing('large')}`,
+  padding: `${rem(12)} ${spacing('xlarge')}`,
 };
 
 const large: CSSObject = {
-  fontSize: rem(14),
+  fontSize: rem(16),
   lineHeight: rem(17),
-  padding: `${rem(12)} ${spacing('large')}`,
+  padding: `${spacing()} ${rem(72)}`,
 };
 
 const buttonSizes = {
