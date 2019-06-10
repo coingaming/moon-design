6b3d8cb4fede81f9fd4dca9c86bb97ddc16a58a1
Karl Kallavus <karl.kallavus@gmail.com>
2019-06-10 14:03:50 +0300

    feat(badge): add modifier prop

---
+++ design/components/src/badge/modifiers.ts
@@ -0,0 +1,78 @@
+import { CSSObject } from '@emotion/core';
+import { colors } from '@heathmont/sportsbet-tokens';
+
+type BadgeModifiers =
+  | 'single'
+  | 'multi'
+  | 'active'
+  | 'won'
+  | 'lost'
+  | 'settled'
+  | 'void'
+  | 'doubles'
+  | 'live';
+
+const single: CSSObject = {
+  color: colors.neutral[10],
+  backgroundColor: colors.brand,
+};
+
+const multi = {
+  color: colors.neutral[10],
+  backgroundColor: colors.highlight,
+};
+
+const active: CSSObject = {
+  color: colors.neutral[10],
+  backgroundColor: colors.brand,
+};
+
+const won: CSSObject = {
+  color: colors.neutral[10],
+  backgroundColor: colors.brand,
+};
+
+const lost: CSSObject = {
+  color: colors.neutral[10],
+  backgroundColor: colors.brand,
+};
+
+const settled: CSSObject = {
+  color: colors.neutral[10],
+  backgroundColor: colors.brand,
+};
+
+const voidModifier: CSSObject = {
+  color: colors.neutral[10],
+  backgroundColor: colors.brand,
+};
+
+const doubles: CSSObject = {
+  color: colors.neutral[10],
+  backgroundColor: colors.brand,
+};
+
+const live: CSSObject = {
+  color: colors.neutral[10],
+  backgroundColor: colors.error,
+};
+
+const canceled: CSSObject = {
+  color: colors.neutral[10],
+  backgroundColor: colors.brand,
+};
+
+const badgeModifiers = {
+  single,
+  multi,
+  active,
+  won,
+  lost,
+  settled,
+  doubles,
+  live,
+  canceled,
+  void: voidModifier,
+};
+
+export { badgeModifiers, BadgeModifiers };
