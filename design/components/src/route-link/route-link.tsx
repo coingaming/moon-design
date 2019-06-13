91838bb233d2ad44c95f21741d1deda141e9a45b
Karl Kallavus <karl.kallavus@gmail.com>
2019-06-13 16:44:25 +0300

    fix(RouteLink): styling for NavLink

--- design/components/src/route-link/route-link.tsx
+++ design/components/src/route-link/route-link.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
 import { NavLink, NavLinkProps } from 'react-router-dom';
 
-export const RouteLink: React.FC<NavLinkProps & { as: any }> = ({
+export const RouteLink: React.FC<NavLinkProps & { as?: any }> = ({
   children,
   to,
   as,
 }) => {
-  return React.createElement(as || NavLink, { to }, children);
+  return React.createElement(
+    as || NavLink,
+    {
+      to,
+      style: {
+        color: 'inherit',
+        textDecoration: 'none',
+      },
+    },
+    children
+  );
 };
