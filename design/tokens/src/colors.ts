188fcd14e6bfac43ad2ec59e8b12ffed216f0caa
Jaan Tihvan <jtihvan@MBP-JTIHVAN.local>
2019-06-14 14:52:13 +0300

    refactor: split colors in two files

--- design/tokens/src/colors.ts
+++ design/tokens/src/colors.ts
@@ -1,129 +1,20 @@
-type BaseColors = { [key: number]: string };
-
-const piccolo: BaseColors = {
-  100: '#0CD664',
-  80: '#24DA74',
-  60: '#3DDE83',
-  40: '#55E292',
-  20: '#85EAB2',
-};
-
-const bulma: BaseColors = {
-  100: '#fff',
-  80: '#F3F5F6',
-  60: '#EDEFF1',
-  40: '#E7EAEC',
-  20: '#DBE0E3',
-};
-
-const trunks: BaseColors = {
-  100: '#8697A2',
-  80: '#92A1AB',
-  60: '#9EACB5',
-  40: '#AAB6BE',
-  20: '#C2CBD0',
-};
-
-const gohan: BaseColors = {
-  100: '#31373F',
-  80: '#3D434B',
-  60: '#5A5F65',
-  40: '#6F7379',
-  20: '#989B9F',
-};
-
-const goku: BaseColors = {
-  100: '#1F262F',
-  80: '#353C44',
-  60: '#4C5159',
-  40: '#62676D',
-  20: '#8F9297',
-};
-
-const roshi: BaseColors = {
-  100: '#FF5111',
-  80: '#FF6229',
-  60: '#FF7441',
-  40: '#FF8558',
-  20: '#FFA888',
-};
-
-const whis: BaseColors = {
-  100: '#0F75BD',
-  80: '#2783C4',
-  60: '#3F91CA',
-  40: '#579ED1',
-  20: '#87BADE',
-};
-
-const vegeta: BaseColors = {
-  100: '#118BFF',
-  80: '#2997FF',
-  60: '#41A2FF',
-  40: '#58AEFF',
-  20: '#88C5FF',
-};
-
-const frieza: BaseColors = {
-  100: '#9182DE',
-  80: '#9C8EE1',
-  60: '#A79BE5',
-  40: '#B2A7E8',
-  20: '#C8C0EF',
-};
-
-const krillin: BaseColors = {
-  100: '#FFD600',
-  80: '#FFDE33',
-  60: '#FFE666',
-  40: '#FFEF99',
-  20: '#FFF7CC',
-};
-
-const cell: BaseColors = {
-  100: '#0AB758',
-  80: '#22BE69',
-  60: '#3BC579',
-  40: '#53CD8A',
-  20: '#84DBAB',
-};
-const dodoria: BaseColors = {
-  100: '#D33030',
-  80: '#D74545',
-  60: '#DC5959',
-  40: '#E06E6E',
-  20: '#E99797',
-};
-const raditz: BaseColors = {
-  100: '#B3804A',
-  80: '#BB8D5C',
-  60: '#C2996E',
-  40: '#CAA680',
-  20: '#D9BFA4',
-};
-
-const nappa: BaseColors = {
-  100: '#725550',
-  80: '#806661',
-  60: '#8E7773',
-  40: '#9C8884',
-  20: '#B8AAA7',
-};
-const chichi: BaseColors = {
-  100: '#FF4E64',
-  80: '#FF6074',
-  60: '#FF7183',
-  40: '#FF8392',
-  20: '#FFA7B2',
-};
-
-const hit: BaseColors = {
-  100: '#010812',
-  80: '#1A212A',
-  60: '#343941',
-  40: '#4D5259',
-  20: '#808388',
-};
+import {
+  bulma,
+  cell,
+  chichi,
+  dodoria,
+  frieza,
+  gohan,
+  goku,
+  hit,
+  krillin,
+  nappa,
+  piccolo,
+  raditz,
+  trunks,
+  vegeta,
+  whis,
+} from './coreColors';
 
 const primaryColors: { brand: string; neutral: BaseColors } = {
   brand: piccolo[100],
