a405aad50b5a318f0a3068a2a49b2ae015f47a6d
joe-bell <joe@joebell.co.uk>
2019-07-02 10:23:56 +0300

    fix(icons): currentColor conversion

--- design/icons/src/svg/IconLogo.tsx
+++ design/icons/src/svg/IconLogo.tsx
@@ -14,13 +14,13 @@ const Svg = (props: React.SVGProps<SVGSVGElement>) => (
       fillRule="evenodd"
       clipRule="evenodd"
       d="M13.955 4.274c5.424 0 9.821 4.387 9.821 9.8 0 5.411-4.397 9.798-9.821 9.798-5.424 0-9.821-4.387-9.821-9.799s4.397-9.799 9.82-9.799M14 2c6.627 0 12 5.373 12 12 0 6.628-5.373 12-12 12-6.628 0-12-5.372-12-12C2 7.373 7.372 2 14 2"
-      fill="currentColor"
+      fill="#fff"
     />
     <path
       fillRule="evenodd"
       clipRule="evenodd"
       d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0"
-      fill="currentColor"
+      fill="#0CD664"
     />
   </svg>
 );
