92826ac141ddc888c7276199e6a6358bd95a9cf5
lauaviin <j.tihvan@gmail.com>
2019-07-11 16:23:16 +0300

    refactor: backgroundstripes for joe

--- portals/user-portal-components/src/myBets/components/shared/utils.tsx
+++ portals/user-portal-components/src/myBets/components/shared/utils.tsx
@@ -5,7 +5,7 @@ export const BadgeWrapper = styled.div({
   marginLeft: spacing('xsmall'),
 });
 
-export const backGroundStripes = {
+export const backgroundStripes = {
   backgroundImage:
     'linear-gradient(135deg, #010812 25%, rgba(255,255,255,0.08) 25%, rgba(255,255,255,0.08) 50%, #010812 50%, #010812 75%, rgba(255,255,255,0.08) 75%, rgba(255,255,255,0.08) 100%)',
   backgroundSize: '5.66px 5.66px',
