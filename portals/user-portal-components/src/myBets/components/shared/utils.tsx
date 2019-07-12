70674f255326c3982ea6c4a9783f69728c1b5d6c
lauaviin <j.tihvan@gmail.com>
2019-07-12 13:23:58 +0300

    refactor: Update scoreboards colors

--- portals/user-portal-components/src/myBets/components/shared/utils.tsx
+++ portals/user-portal-components/src/myBets/components/shared/utils.tsx
@@ -1,16 +1,12 @@
 import { colors } from '@heathmont/sportsbet-tokens';
 
 export const backgroundStripes = {
-  backgroundImage:
-    'linear-gradient(135deg, #010812 25%, rgba(255,255,255,0.08) 25%, rgba(255,255,255,0.08) 50%, #010812 50%, #010812 75%, rgba(255,255,255,0.08) 75%, rgba(255,255,255,0.08) 100%)',
+  backgroundImage: `linear-gradient(135deg, ${colors.palette.goku[100]} 25%, rgba(255,255,255,0.08) 25%, rgba(255,255,255,0.08) 50%, ${colors.palette.goku[100]} 50%, ${colors.palette.goku[100]} 75%, rgba(255,255,255,0.08) 75%, rgba(255,255,255,0.08) 100%)`,
   backgroundSize: '5.66px 5.66px',
 };
 
-// export const myBetsColors = {
-//   scoreBoard: {
-//     competitors:
-//   }
-// };
+export const scoreboardBackgroundColor = colors.palette.goku[100];
+export const scoreboardScoreColor = colors.palette.hit[100];
 
 export const betBoostHighlightColors = {
   text: colors.palette.vegeta[100],
