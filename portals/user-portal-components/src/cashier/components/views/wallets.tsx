a1f9c7ba995d27e114dc153021e0ce504d3c25c4
joe-bell <joe@joebell.co.uk>
2019-07-19 13:31:43 +0300

    feat(carousel): create and implement
    Co-authored-by: Bilal Javed <bilal.javed@heathmont.net>

--- portals/user-portal-components/src/cashier/components/views/wallets.tsx
+++ portals/user-portal-components/src/cashier/components/views/wallets.tsx
@@ -1,37 +1,45 @@
-import styled from '@emotion/styled';
+import styled, { CSSObject } from '@emotion/styled';
 import rem from 'polished/lib/helpers/rem';
 import { spacing, mq } from '@heathmont/sportsbet-utils';
 import { breakpoints } from '@heathmont/sportsbet-tokens';
-import {
-  userPortalContainer,
-  userPortalContainerInner,
-} from '../../../shared/container';
+import { CarouselSlider } from '@heathmont/sportsbet-components';
 import { USER_PORTAL_CONTAINER_SIZE } from '../../../shared/globals';
 
-export const WalletsContainer = styled.div({
-  ...userPortalContainer,
-  ...userPortalContainerInner,
+export const WalletsContainer = styled(CarouselSlider)({
   display: 'flex',
   flexDirection: 'row',
   overflow: 'auto',
   flexFlow: 'row nowrap',
-  scrollSnapType: 'x mandatory',
   marginTop: spacing('large'),
 });
+
+export const walletItemStyle: CSSObject = {
+  paddingRight: spacing(),
+  /**
+   * For the first child, below the container, add extra spacing
+   */
+  [mq(USER_PORTAL_CONTAINER_SIZE, 'max-width')]: {
+    '&:first-of-type': {
+      marginLeft: spacing(),
+    },
+  },
+  /**
+   * From small upwards, display in a row of 3.
+   */
+  [mq(breakpoints.small)]: {
+    minWidth: rem(USER_PORTAL_CONTAINER_SIZE / 3),
+    paddingRight: spacing('large'),
+  },
+};
 
 export const WalletWrapper = styled.div<{ mobileHidden?: boolean }>(
   ({ mobileHidden }) => [
     {
       minWidth: rem(280),
-      paddingRight: spacing(),
-      scrollSnapAlign: 'center',
-      [mq(breakpoints.small)]: {
-        minWidth: rem(USER_PORTAL_CONTAINER_SIZE / 3),
-        paddingRight: spacing('large'),
-      },
     },
     mobileHidden && {
       [mq(breakpoints.medium, 'max-width')]: {
+        minWidth: 0,
         display: 'none',
       },
     },
