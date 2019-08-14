1f11ac5eebcd78df8be244cb790261db1ba192cf
Bilal <bilal.javed@heathmont.net>
2019-08-14 18:25:55 +0300

    chore(icons): added new currency icons

--- packages/icons/src/group/IconCurrency.tsx
+++ packages/icons/src/group/IconCurrency.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { colors } from '@heathmont/sportsbet-tokens';
 
 import { IconCurrencyArs } from '../svg/IconCurrencyArs';
+import { IconCurrencyBdt } from '../svg/IconCurrencyBdt';
 import { IconCurrencyBob } from '../svg/IconCurrencyBob';
 import { IconCurrencyBrl } from '../svg/IconCurrencyBrl';
 import { IconCurrencyBtc } from '../svg/IconCurrencyBtc';
@@ -13,21 +14,26 @@ import { IconCurrencyEth } from '../svg/IconCurrencyEth';
 import { IconCurrencyEur } from '../svg/IconCurrencyEur';
 import { IconCurrencyIdr } from '../svg/IconCurrencyIdr';
 import { IconCurrencyInr } from '../svg/IconCurrencyInr';
+import { IconCurrencyIqd } from '../svg/IconCurrencyIqd';
 import { IconCurrencyJpy } from '../svg/IconCurrencyJpy';
+import { IconCurrencyKrw } from '../svg/IconCurrencyKrw';
 import { IconCurrencyLtc } from '../svg/IconCurrencyLtc';
 import { IconCurrencyMxn } from '../svg/IconCurrencyMxn';
 import { IconCurrencyMyr } from '../svg/IconCurrencyMyr';
 import { IconCurrencyPen } from '../svg/IconCurrencyPen';
+import { IconCurrencyPkr } from '../svg/IconCurrencyPkr';
 import { IconCurrencyRub } from '../svg/IconCurrencyRub';
 import { IconCurrencySoc } from '../svg/IconCurrencySoc';
 import { IconCurrencyThb } from '../svg/IconCurrencyThb';
 import { IconCurrencyTry } from '../svg/IconCurrencyTry';
+import { IconCurrencyUah } from '../svg/IconCurrencyUah';
 import { IconCurrencyUsd } from '../svg/IconCurrencyUsd';
 import { IconCurrencyVes } from '../svg/IconCurrencyVes';
 import { IconCurrencyVnd } from '../svg/IconCurrencyVnd';
 
 type CurrencyName =
   | 'ARS'
+  | 'BDT'
   | 'BOB'
   | 'BRL'
   | 'BTC'
@@ -38,16 +44,20 @@ type CurrencyName =
   | 'ETH'
   | 'EUR'
   | 'IDR'
+  | 'IQD'
   | 'INR'
   | 'JPY'
+  | 'KRW'
   | 'LTC'
   | 'MXN'
   | 'MYR'
   | 'PEN'
+  | 'PKR'
   | 'RUB'
   | 'SOC'
   | 'THB'
   | 'TRY'
+  | 'UAH'
   | 'USD'
   | 'VES'
   | 'VND';
@@ -59,6 +69,7 @@ export type GroupIconCurrencyProps = {
 
 const currencyIcons = {
   ARS: IconCurrencyArs,
+  BDT: IconCurrencyBdt,
   BOB: IconCurrencyBob,
   BRL: IconCurrencyBrl,
   BTC: IconCurrencyBtc,
@@ -70,16 +81,20 @@ const currencyIcons = {
   EUR: IconCurrencyEur,
   IDR: IconCurrencyIdr,
   INR: IconCurrencyInr,
+  IQD: IconCurrencyIqd,
   JPY: IconCurrencyJpy,
+  KRW: IconCurrencyKrw,
   LTC: IconCurrencyLtc,
   MXN: IconCurrencyMxn,
   MYR: IconCurrencyMyr,
   PEN: IconCurrencyPen,
+  PKR: IconCurrencyPkr,
   RUB: IconCurrencyRub,
   SOC: IconCurrencySoc,
   THB: IconCurrencyThb,
   TRY: IconCurrencyTry,
   USD: IconCurrencyUsd,
+  UAH: IconCurrencyUah,
   VES: IconCurrencyVes,
   VND: IconCurrencyVnd,
 };
