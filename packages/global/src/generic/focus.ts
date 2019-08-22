38e5ae146aaf39e2ffc38fb122c42204aae5e0a7
joe-bell <joe@joebell.co.uk>
2019-08-22 11:02:55 +0300

    feat(global): enable focus-visible polyfil

---
+++ packages/global/src/generic/focus.ts
@@ -0,0 +1,25 @@
+import { CSSObject } from '@emotion/core';
+
+export const focus: CSSObject = {
+  /**
+   * Prevents an unwanted focus outline from appearing around elements that
+   * might still respond to pointer events.
+   * https://github.com/chris-pearce/backpack.css
+   */
+  [`[tabindex='-1']:focus`]: {
+    outline: 'none',
+  },
+  /**
+   * Removes the outline in cases where the UA determines via heuristics that
+   * the focus should not be visible. This is mainly for ensuring that a focus
+   * indicator (a "focus ring") does not render for mouse users.
+   * https://github.com/chris-pearce/backpack.css
+   */
+  '*:focus:not(:focus-visible)': {
+    outline: 'none',
+  },
+  /* If the browser doesn't support `:focus-visible`, use the polyfil */
+  '.js-focus-visible *:focus:not(.focus-visible)': {
+    outline: 'none',
+  },
+};
