885d88b24e5703ad38f2e6ca23c2abf11acbea12
joe-bell <joe@joebell.co.uk>
2019-08-23 09:26:21 +0300

    feat(global): Global element for consuming styles, rename polyfill

--- docs/gatsby-browser.js
+++ docs/gatsby-browser.js
@@ -1,3 +1,7 @@
+import { polyfill } from '@heathmont/sportsbet-global';
+
 import { wrapRootElement as wrap } from './src/wrap-root-element';
 
 export const wrapRootElement = wrap;
+
+export const onInitialClientRender = () => polyfill;
