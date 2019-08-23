7f337375c911ee07012fb91d5b12e961cf2204d7
joe-bell <joe@joebell.co.uk>
2019-08-23 15:40:21 +0300

    refactor(badge): convert to styled component

---
+++ packages/components/src/badge/badge.ts
@@ -0,0 +1,31 @@
+import styled from '@emotion/styled';
+import rem from 'polished/lib/helpers/rem';
+import { border, colors } from '@heathmont/sportsbet-tokens';
+
+import { badgeModifiers, BadgeModifiers } from './modifiers';
+
+type BadgeProps = {
+  color?: string;
+  backgroundColor?: string;
+  modifier?: BadgeModifiers;
+};
+
+const badgeElement = 'span';
+
+const Badge = styled(badgeElement)<BadgeProps>(
+  ({ color = colors.text, backgroundColor = colors.brand, modifier }) => ({
+    color,
+    backgroundColor,
+    padding: `${rem(2)} ${rem(7)}`,
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    borderRadius: border.radius.largest,
+    fontSize: rem(8),
+    lineHeight: rem(10),
+    letterSpacing: rem(0.5),
+    fontWeight: 700,
+    ...(modifier && badgeModifiers[modifier]),
+  })
+);
+
+export { Badge, BadgeProps, badgeElement };
