523fcd4f9dec2fd57fca3adc16fa2ff63d4c5975
joe-bell <joe@joebell.co.uk>
2019-09-26 16:30:22 +0300

    feat(assets): add logos and patterns

--- docs/src/components/nav/logo.tsx
+++ docs/src/components/nav/logo.tsx
@@ -3,7 +3,7 @@ import { CSSObject } from '@emotion/core';
 import { Link } from 'gatsby';
 import hideVisually from 'polished/lib/mixins/hideVisually';
 import rem from 'polished/lib/helpers/rem';
-import { LogoSportsbet } from '@heathmont/sportsbet-components';
+import { LogoSportsbet } from '@heathmont/sportsbet-assets';
 
 const logoLink: CSSObject = {
   display: 'block',
@@ -13,6 +13,6 @@ const logoLink: CSSObject = {
 export const Logo = () => (
   <Link to="/" css={logoLink}>
     <span css={hideVisually}>Home</span>
-    <LogoSportsbet />
+    <LogoSportsbet width="100%" height="auto" />
   </Link>
 );
