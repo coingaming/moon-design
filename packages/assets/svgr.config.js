a308deea5d0e027ccb231fedbd763ec723aa7ade
joe-bell <joe@joebell.co.uk>
2019-09-26 10:27:45 +0300

    feat: refactor svgr, introduce crest assets

---
+++ packages/assets/svgr.config.js
@@ -0,0 +1,12 @@
+const config = require('../../.config/svgr/svgr.config.js');
+const dimensions = require('../../.config/svgr/transform/dimensions');
+
+module.exports = {
+  ...config,
+  plugins: ['@svgr/plugin-jsx'],
+  jsx: {
+    babelConfig: {
+      plugins: [dimensions],
+    },
+  },
+};
