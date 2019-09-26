c2e27fbcb3d96cae6fad860df92a85e013b3a1b1
joe-bell <joe@joebell.co.uk>
2019-09-26 10:27:45 +0300

    feat: refactor svgr, introduce crest assets

--- .config/svgr/svgr.config.base.js
+++ .config/svgr/svgr.config.base.js
@@ -1,8 +1,4 @@
-const template = require('./template');
-
 module.exports = {
-  icon: true,
   svgProps: { xmlns: 'http://www.w3.org/2000/svg' },
   replaceAttrValues: { '#DE1E7E': 'currentColor' },
-  template,
 };
