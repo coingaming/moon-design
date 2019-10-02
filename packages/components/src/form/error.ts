816b0f3905b81f0e5eceed18b755730f7075a0d6
joe-bell <joe@joebell.co.uk>
2019-10-02 15:15:20 +0300

    chore: align form elements with design

---
+++ packages/components/src/form/error.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+import { colors } from '@heathmont/sportsbet-tokens';
+import { rem, spacing } from '@heathmont/sportsbet-utils';
+
+export const FormError = styled.p({
+  display: 'inline-block',
+  marginTop: spacing('small'),
+  fontSize: rem(12),
+  color: colors.error,
+});
+
+FormError.defaultProps = {
+  role: 'alert',
+};
