3aaca0e5dc4b3ff723e5efbf3804004b8572ee73
joe-bell <joe@joebell.co.uk>
2019-10-17 12:05:23 +0300

    feat(checkbox): implement uuid, add ariaLabel, support JSX label

--- packages/components/src/checkbox/__tests__/checkbox.test.tsx
+++ packages/components/src/checkbox/__tests__/checkbox.test.tsx
@@ -9,19 +9,38 @@ const testProps = {
 
 describe('Checkbox', () => {
   test('renders by default', () => {
-    const checkbox = create(<Checkbox {...testProps} />);
+    const checkbox = create(<Checkbox id="Checkbox-Test1" {...testProps} />);
 
     expect(checkbox).toMatchSnapshot();
   });
 
   test('renders as disabled', () => {
-    const checkbox = create(<Checkbox disabled {...testProps} />);
+    const checkbox = create(
+      <Checkbox id="Checkbox-Test2" disabled {...testProps} />
+    );
 
     expect(checkbox).toMatchSnapshot();
   });
 
-  test('renders with hidden label', () => {
-    const checkbox = create(<Checkbox hiddenLabel {...testProps} />);
+  test('renders with hidden aria-label', () => {
+    const checkbox = create(
+      <Checkbox id="Checkbox-Test3" ariaLabel="This label is hidden" />
+    );
+
+    expect(checkbox).toMatchSnapshot();
+  });
+
+  test('renders label with HTML', () => {
+    const checkbox = create(
+      <Checkbox
+        id="Checkbox-Test4"
+        label={
+          <span>
+            This is a label with a <a href="#test">link</a>.
+          </span>
+        }
+      />
+    );
 
     expect(checkbox).toMatchSnapshot();
   });
