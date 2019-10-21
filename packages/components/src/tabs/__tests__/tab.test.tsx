3b5bec7fc21e8f63a04d76cbb9c80253f1cf5f75
Karl Kallavus <karl.kallavus@gmail.com>
2019-10-21 16:53:15 +0300

    feat(tab): rename TabItem -> Tab

--- packages/components/src/tabs/__tests__/tab.test.tsx
+++ packages/components/src/tabs/__tests__/tab.test.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import { create } from 'react-test-renderer';
 
-import { Tabs, TabItem } from '../tabs';
+import { Tabs, Tab } from '../tabs';
 
 describe('Nav', () => {
   test('renders correctly', () => {
     const tabLink = create(
       <Tabs id="test-tabs">
         {[1, 2, 3, 4].map(i => (
-          <TabItem key={i}>
+          <Tab key={i}>
             Item
             {i}
-          </TabItem>
+          </Tab>
         ))}
       </Tabs>
     );
