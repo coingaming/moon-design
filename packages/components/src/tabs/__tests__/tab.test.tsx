6959b9476f7072939824c40c1e80fb1271d2a5df
Karl Kallavus <karl.kallavus@gmail.com>
2019-10-21 15:33:12 +0300

    test(tabs): bump snapshots

--- packages/components/src/tabs/__tests__/tab.test.tsx
+++ packages/components/src/tabs/__tests__/tab.test.tsx
@@ -6,7 +6,7 @@ import { Tabs, TabItem } from '../tabs';
 describe('Nav', () => {
   test('renders correctly', () => {
     const tabLink = create(
-      <Tabs>
+      <Tabs id="test-tabs">
         {[1, 2, 3, 4].map(i => (
           <TabItem key={i}>
             Item
