ac150ce534e0b8b45cc8f8a368a740c732a4020c
joe-bell <joe@joebell.co.uk>
2019-11-04 10:18:54 +0200

    chore: further styled-components integration

--- packages/objects/src/stack/stack.ts
+++ packages/objects/src/stack/stack.ts
@@ -13,27 +13,9 @@ export const stack = (
   space: StackSpace,
   direction: StackDirection = 'vertical'
 ) => {
-  const margin = direction === 'vertical' ? 'marginTop' : 'marginLeft';
-
   const margins = {
     '& > * + *': {
-      [margin]: space,
-    },
-    /**
-     * With Emotion's 'out of the box' SSR set-up, <style/> tags are injected
-     * inline above each element with styles on first load.
-     *
-     * This means that if the first-child of <Stack/> has styles, the first-child
-     * of <Stack/> will  be a <style/> tag, then the actual first-child - in this
-     * case the first-child becomes the second and inherits the 'marginTop'.
-     *
-     * For now, if the first-child is a <style/> tag on load, let's make sure
-     * the adjacent element to it (our actual first-child), has an enforced
-     * 'marginTop' of 0.
-     * https://github.com/emotion-js/emotion/issues/1178
-     */
-    '& > style:first-child + *': {
-      [margin]: 0,
+      [direction === 'vertical' ? 'marginTop' : 'marginLeft']: space,
     },
   };
 
@@ -41,7 +23,7 @@ export const stack = (
     ? {
         /**
          * If a browser supports the `grid-gap` property, let's use it.
-         * Otherwise, fallback to the lobotomised owl selector to style children.
+         * Otherwise, fallback to the lobotomized owl selector to style children.
          */
         '@supports (grid-gap: 0)': {
           display: 'grid',
