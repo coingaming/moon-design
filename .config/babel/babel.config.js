848f6c70c9d2053f3e2040649a91184b4a0a3bf0
joe-bell <joe@joebell.co.uk>
2019-11-04 15:25:44 +0200

    chore: update babel config

--- .config/babel/babel.config.js
+++ .config/babel/babel.config.js
@@ -12,6 +12,11 @@ module.exports = {
   ],
   plugins: [
     '@babel/plugin-syntax-dynamic-import',
-    'babel-plugin-styled-components',
+    [
+      'babel-plugin-styled-components',
+      {
+        ssr: true,
+      },
+    ],
   ],
 };
