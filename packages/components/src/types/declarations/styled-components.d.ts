f6b4d7f76a16146002eb8aa72cbad3ac81abce6a
joe-bell <joe@joebell.co.uk>
2019-11-05 14:39:08 +0200

    chore: reinforce styled-components types

--- packages/components/src/types/declarations/styled-components.d.ts
+++
@@ -1,9 +0,0 @@
-// https://github.com/styled-components/styled-components/issues/2528
-
-import { CSSProp } from 'styled-components';
-
-declare module 'react' {
-  interface Attributes {
-    css?: CSSProp;
-  }
-}
