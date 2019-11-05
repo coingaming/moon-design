bee9d0837fffb6f71a8166da07876366eda25492
joe-bell <joe@joebell.co.uk>
2019-11-05 14:39:08 +0200

    chore: reinforce styled-components types

---
+++ packages/components/src/types/styled-components/index.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Styled Components' `css` prop gets special treatment and needs to be setup as
+ * per t̶h̶e̶ d̶o̶c̶u̶m̶e̶n̶t̶a̶t̶i̶o̶n̶ some random GitHub issue:
+ * https://github.com/DefinitelyTyped/DefinitelyTyped/issues/31245#issuecomment-463640878
+ */
+
+/* eslint-disable-next-line import/no-unresolved, no-unused-vars, @typescript-eslint/no-unused-vars */
+import * as types from 'styled-components/cssprop';
