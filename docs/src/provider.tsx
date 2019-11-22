0493e7656ea13e2078fd5dff05e833cba4a4000e
joe-bell <joe@joebell.co.uk>
2019-11-22 09:07:36 +0200

    docs: implement theming

---
+++ docs/src/provider.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import {
+  ThemeProvider,
+  sportsbetDark,
+  sportsbetLight,
+} from '@heathmont/sportsbet-themes';
+
+const themes: any = {
+  sportsbetDark,
+  sportsbetLight,
+};
+
+export const DocsThemeContext = React.createContext({
+  setTheme: (themeKey: string) => {
+    themeKey;
+  },
+  themeKeys: [''],
+  cycleTheme: () => {},
+});
+
+export const useDocsTheme = () => React.useContext(DocsThemeContext);
+
+export const DocsThemeProvider: React.FC<{ children: React.ReactChild }> = ({
+  children,
+}) => {
+  const [themeState, setThemeState] = React.useState('sportsbetDark');
+
+  const cycleTheme = () => {
+    console.log('🎨 Theme: ', themeState, themes[themeState]);
+    const themeKeys = [...Object.keys(themes || {})];
+    const i = themeKeys.indexOf(themeState);
+    const next = themeKeys[(i + 1) % themeKeys.length];
+    setThemeState(next);
+  };
+
+  const setTheme = (themeKey: string) => setThemeState(themes[themeKey]);
+
+  const themeKeys = Object.keys(themes);
+
+  return (
+    <DocsThemeContext.Provider value={{ cycleTheme, setTheme, themeKeys }}>
+      <ThemeProvider theme={themes[themeState]} hasTransition>
+        {children}
+      </ThemeProvider>
+    </DocsThemeContext.Provider>
+  );
+};
