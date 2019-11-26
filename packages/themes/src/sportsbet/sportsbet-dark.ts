9362a8be3b26329a1d946b0c87c8fc4a8771b05a
joe-bell <joe@joebell.co.uk>
2019-11-26 11:35:50 +0200

    feat(themes): update colors

--- packages/themes/src/sportsbet/sportsbet-dark.ts
+++ packages/themes/src/sportsbet/sportsbet-dark.ts
@@ -3,7 +3,24 @@ import { Theme } from '../types/theme';
 
 import { sportsbet } from './private/sportsbet';
 
+const lights = {
+  100: '#FFFFFF',
+  80: '#F3F5F6',
+  60: '#EDEFF1',
+  40: '#E7EAEC',
+  20: '#DBE0E3',
+  10: '#CFD5DA',
+};
+
 const color = {
+  hit: {
+    100: '#1A212A',
+    80: '#31373F',
+    60: '#484D55',
+    40: '#5F646A',
+    20: '#767A7F',
+    10: '#8C9095',
+  },
   goku: {
     100: '#1F262F',
     80: '#353C44',
@@ -20,14 +37,16 @@ const color = {
     20: '#767A7F',
     10: '#8C9095',
   },
-  bulma: {
-    100: '#FFFFFF',
-    80: '#F3F5F6',
-    60: '#EDEFF1',
-    40: '#E7EAEC',
-    20: '#DBE0E3',
-    10: '#CFD5DA',
+  beerus: {
+    100: '#212830',
+    80: '#2C333A',
+    60: '#373D45',
+    40: '#42484F',
+    20: '#4D5359',
+    10: '#585E64',
   },
+  goten: lights,
+  bulma: lights,
 };
 
 const {
--- packages/themes/src/sportsbet/sportsbet-light.ts
+++ packages/themes/src/sportsbet/sportsbet-light.ts
@@ -3,16 +3,27 @@ import { Theme } from '../types/theme';
 
 import { sportsbet } from './private/sportsbet';
 
+const darks = {
+  100: '#1A212A',
+  80: '#31373F',
+  60: '#484D55',
+  40: '#5F646A',
+  20: '#767A7F',
+  10: '#8C9095',
+};
+
+const lights = {
+  100: '#F7F7F7',
+  80: '#F7F7F7',
+  60: '#F0F0F0',
+  40: '#EBEBEB',
+  20: '#E6E6E6',
+  10: '#E0E0E0',
+};
+
 const color = {
+  hit: lights,
   goku: {
-    100: '#F3F5F6',
-    80: '#EDEFF1',
-    60: '#E7EAEC',
-    40: '#DBE0E3',
-    20: '#CFD5DA',
-    10: '#C9D0D5',
-  },
-  gohan: {
     100: '#FFFFFF',
     80: '#F3F5F6',
     60: '#EDEFF1',
@@ -20,14 +31,17 @@ const color = {
     20: '#DBE0E3',
     10: '#CFD5DA',
   },
-  bulma: {
-    100: '#010812',
-    80: '#1A212A',
-    60: '#272D36',
-    40: '#343941',
-    20: '#40464D',
-    10: '#4D5259',
+  gohan: lights,
+  beerus: {
+    100: '#E6E6E6',
+    80: '#E4E4E4',
+    60: '#E1E1E1',
+    40: '#DFDFDF',
+    20: '#DDDDDD',
+    10: '#DADADA',
   },
+  goten: darks,
+  bulma: darks,
 };
 
 const {
