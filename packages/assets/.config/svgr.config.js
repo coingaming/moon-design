9b0244e55d4d80325d6b78276f411fff9c8e3bc4
Karl Kallavus <karl.kallavus@heathmont.net>
2019-12-30 16:19:21 +0300

    security release (#658)
    * fix(assets): streamline SVGR config
    
    * chore(deps): bump handlebars from 4.1.2 to 4.5.3 (#657)
    
    Bumps [handlebars](https://github.com/wycats/handlebars.js) from 4.1.2 to 4.5.3.
    - [Release notes](https://github.com/wycats/handlebars.js/releases)
    - [Changelog](https://github.com/wycats/handlebars.js/blob/master/release-notes.md)
    - [Commits](https://github.com/wycats/handlebars.js/compare/v4.1.2...v4.5.3)
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- packages/assets/.config/svgr.config.js
+++ packages/assets/.config/svgr.config.js
@@ -1,8 +1,10 @@
-const config = require('../../../.config/svgr/svgr.config.js');
-const dimensions = require('../../../.config/svgr/transform/dimensions');
+const base = require('./svgr.config.base');
+const template = require('./templates/svgr.template.js');
+const dimensions = require('./transform/dimensions');
 
 module.exports = {
-  ...config,
+  ...base,
+  template,
   plugins: ['@svgr/plugin-jsx'],
   jsx: {
     babelConfig: {
@@ -10,4 +12,3 @@ module.exports = {
     },
   },
 };
-
