0dc7d445616a995134b30c4751d6e3779f365289
YulianaYarema <59561179+yarema184@users.noreply.github.com>
2020-01-29 13:52:21 +0200

    feat: SPO-5437 added scrollable behaviour (#673)
    * feat: SPO-5437 added scrollable behaviour
    
    * feat: added scroll Ponyfills
    
    * feat: SPO-5437 code refactoring after feedback
    
    * feat: fixed ts issues
    
    * feat: SPO-5437 added scrollableBar component
    
    * chore(scrollable-bar): temp: introduce scrollable bar
    
    * feat: SPO-5437 some fix and renames
    
    * feat: SPO-5437 added description
    
    * feat: SPO-5437 fixes after review
    
    * feat: SPO-5437 small fixes after review
    
    * feat: removed scrollable components
    
    * feat: remove scrollable from navigation
    
    * feat: remove extra eslint disable line
    
    * feat: added documentation page for scrollable components

---
+++ packages/components/src/behavior/withHorizontalScroll.ts
@@ -0,0 +1,128 @@
+import * as React from 'react';
+import { findLastIndex, findIndex } from 'lodash'; // eslint-disable-line
+import scrollIntoView from 'scroll-into-view-if-needed';
+import smoothScrollIntoView from 'smooth-scroll-into-view-if-needed';
+
+export const THRESHOLD = 0.75;
+
+type Options = {
+  scrollStep?: number;
+};
+
+const findLastVisibleIndex = (childRefs: any[]): any => {
+  return findLastIndex(childRefs, child => child.getAttribute('visible'));
+};
+
+const findFirstVisibleIndex = (childRefs: any[]): any => {
+  return findIndex(childRefs, child => child.getAttribute('visible'));
+};
+
+const scrollToIndex = (itemRef: HTMLElement, scrollIntoViewSmoothly: any) => {
+  if (itemRef) {
+    scrollIntoViewSmoothly(itemRef, {
+      block: 'nearest',
+      inline: 'nearest',
+      behavior: 'smooth',
+    });
+  }
+};
+
+const scrollLeftToStep = (
+  scrollStep: number,
+  itemRefs: HTMLElement[],
+  scrollIntoViewSmoothly: any
+) => {
+  const firstVisibleIndex = findFirstVisibleIndex(itemRefs);
+  const actualScrollForIndex =
+    firstVisibleIndex < scrollStep ? 0 : firstVisibleIndex - scrollStep;
+  scrollToIndex(itemRefs[actualScrollForIndex], scrollIntoViewSmoothly);
+};
+
+const scrollRightToStep = (
+  scrollStep: number,
+  itemRefs: HTMLElement[],
+  scrollIntoViewSmoothly: any
+) => {
+  const lastVisibleIndex = findLastVisibleIndex(itemRefs);
+  const lastIndex = itemRefs.length - 1;
+  const actualScrollForIndex =
+    lastIndex - lastVisibleIndex < scrollStep
+      ? lastIndex
+      : lastVisibleIndex + scrollStep;
+  scrollToIndex(itemRefs[actualScrollForIndex], scrollIntoViewSmoothly);
+};
+
+const showHideIndicator = (
+  itemRefs: HTMLElement[],
+  setLeftIndicator: (isShow: boolean) => void,
+  setRightIndicator: (isShow: boolean) => void
+) => {
+  const firstVisibleIndex = findFirstVisibleIndex(itemRefs);
+  const lastVisibleIndex = findLastVisibleIndex(itemRefs);
+  if (lastVisibleIndex === -1 && firstVisibleIndex === -1) {
+    setLeftIndicator(false);
+    setRightIndicator(false);
+  }
+  lastVisibleIndex < itemRefs.length - 1
+    ? setRightIndicator(true)
+    : setRightIndicator(false);
+  firstVisibleIndex > 0 ? setLeftIndicator(true) : setLeftIndicator(false);
+};
+
+export const withHorizontalScroll = (options: Options) => {
+  const [leftIndicator, setLeftIndicator] = React.useState(false);
+  const [rightIndicator, setRightIndicator] = React.useState(false);
+  const containerRef = React.useRef(null);
+
+  const { scrollStep } = options;
+  const itemRefs: HTMLElement[] = [];
+  let scrollIntoViewSmoothly: any = scrollIntoView;
+
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries: any) => {
+        entries.forEach((entry: any) => {
+          entry.intersectionRatio >= THRESHOLD
+            ? entry.target.setAttribute('visible', 'true')
+            : entry.target.removeAttribute('visible');
+          showHideIndicator(itemRefs, setLeftIndicator, setRightIndicator);
+        });
+      },
+      {
+        root: containerRef.current,
+        threshold: THRESHOLD,
+      }
+    );
+
+    itemRefs.forEach(item => {
+      observer.observe(item);
+    });
+  }, []);
+
+  React.useEffect(() => {
+    if (document) {
+      scrollIntoViewSmoothly =
+        'scrollBehavior' in document.documentElement.style
+          ? scrollIntoView
+          : smoothScrollIntoView;
+    }
+  });
+
+  const itemRef = (element: HTMLElement) => {
+    if (!element) return;
+    itemRefs.push(element);
+  };
+
+  return {
+    itemRef,
+    containerRef,
+    scrollLeftToStep: () =>
+      scrollLeftToStep(scrollStep || 0, itemRefs, scrollIntoViewSmoothly),
+    scrollRightToStep: () =>
+      scrollRightToStep(scrollStep || 0, itemRefs, scrollIntoViewSmoothly),
+    scrollToIndex: (index: number) =>
+      scrollToIndex(itemRefs[index], scrollIntoViewSmoothly),
+    canScrollLeft: leftIndicator,
+    canScrollRight: rightIndicator,
+  };
+};
--- packages/themes/CHANGELOG.md
+++ packages/themes/CHANGELOG.md
@@ -7,69 +7,42 @@ See [Conventional Commits](https://conventionalcommits.org) for commit guideline
 
 **Note:** Version bump only for package @heathmont/sportsbet-themes
 
-
-
-
-
 # [0.107.0](https://github.com/coingaming/sportsbet-design/compare/v0.106.0...v0.107.0) (2020-01-23)
 
 **Note:** Version bump only for package @heathmont/sportsbet-themes
-
-
-
-
 
 # [0.106.0](https://github.com/coingaming/sportsbet-design/compare/v0.105.1...v0.106.0) (2020-01-16)
 
 **Note:** Version bump only for package @heathmont/sportsbet-themes
 
-
-
-
-
 # [0.105.0](https://github.com/coingaming/sportsbet-design/compare/v0.104.3...v0.105.0) (2020-01-15)
 
 **Note:** Version bump only for package @heathmont/sportsbet-themes
 
-
-
-
-
 # [0.104.0](https://github.com/coingaming/sportsbet-design/compare/v0.103.0...v0.104.0) (2019-12-18)
-
 
 ### Features
 
-* **themes:** update sportsbet.io piccolo ([3f052b5](https://github.com/coingaming/sportsbet-design/commit/3f052b5))
-
-
-
-
+- **themes:** update sportsbet.io piccolo ([3f052b5](https://github.com/coingaming/sportsbet-design/commit/3f052b5))
 
 # [0.103.0](https://github.com/coingaming/sportsbet-design/compare/v0.102.0...v0.103.0) (2019-12-18)
 
 **Note:** Version bump only for package @heathmont/sportsbet-themes
 
-
-
-
-
 # [0.102.0](https://github.com/coingaming/sportsbet-design/compare/v0.101.0...v0.102.0) (2019-12-11)
-
 
 ### Bug Fixes
 
-* align versions with changes in develop ([44a60f9](https://github.com/coingaming/sportsbet-design/commit/44a60f9))
-* dry up greens ([1fb61ed](https://github.com/coingaming/sportsbet-design/commit/1fb61ed))
-
+- align versions with changes in develop ([44a60f9](https://github.com/coingaming/sportsbet-design/commit/44a60f9))
+- dry up greens ([1fb61ed](https://github.com/coingaming/sportsbet-design/commit/1fb61ed))
 
 ### Features
 
-* **switch:** colorScheme variant ([fa77d5e](https://github.com/coingaming/sportsbet-design/commit/fa77d5e))
-* **themes:** add bitcasino ([4f3a56f](https://github.com/coingaming/sportsbet-design/commit/4f3a56f))
-* **themes:** add boxShadow, fontSize, maxWidth, opacity, zIndex ([6cf85dd](https://github.com/coingaming/sportsbet-design/commit/6cf85dd))
-* **themes:** align version ([5d5981f](https://github.com/coingaming/sportsbet-design/commit/5d5981f))
-* implement new colors in docs components ([20b24ac](https://github.com/coingaming/sportsbet-design/commit/20b24ac))
-* **themes:** update colors ([3d66ec6](https://github.com/coingaming/sportsbet-design/commit/3d66ec6))
-* **themes:** update colors again ([2668da1](https://github.com/coingaming/sportsbet-design/commit/2668da1))
-* introduce themes ([c37ab4c](https://github.com/coingaming/sportsbet-design/commit/c37ab4c))
+- **switch:** colorScheme variant ([fa77d5e](https://github.com/coingaming/sportsbet-design/commit/fa77d5e))
+- **themes:** add bitcasino ([4f3a56f](https://github.com/coingaming/sportsbet-design/commit/4f3a56f))
+- **themes:** add boxShadow, fontSize, maxWidth, opacity, zIndex ([6cf85dd](https://github.com/coingaming/sportsbet-design/commit/6cf85dd))
+- **themes:** align version ([5d5981f](https://github.com/coingaming/sportsbet-design/commit/5d5981f))
+- implement new colors in docs components ([20b24ac](https://github.com/coingaming/sportsbet-design/commit/20b24ac))
+- **themes:** update colors ([3d66ec6](https://github.com/coingaming/sportsbet-design/commit/3d66ec6))
+- **themes:** update colors again ([2668da1](https://github.com/coingaming/sportsbet-design/commit/2668da1))
+- introduce themes ([c37ab4c](https://github.com/coingaming/sportsbet-design/commit/c37ab4c))
