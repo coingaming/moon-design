6af380c1242ddf79de4b2f038dce8a0ae6d5a266
Daniel Manasov <daniel.manasov@heathmont.net>
2020-02-14 11:59:12 +0200

    fix(pencil): remove longForm prop from dialog BREAKING CHANGE: The longForm option has been removed. Dialog maxWidth and padding have to be configured via content styles

--- packages/components/src/dialog/private/types.ts
+++ packages/components/src/dialog/private/types.ts
@@ -2,10 +2,6 @@ export type DialogMaxWidth = {
   maxWidth?: string | number;
 };
 
-export type DialogLongForm = {
-  longForm?: boolean;
-};
-
 export type DialogFromTop = {
   isFixedFromTop?: boolean;
 };
