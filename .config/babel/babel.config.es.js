d94a6be0a56fc87040ddb2f338695098c0d4a18a
Karl Kallavus <karl.kallavus@gmail.com>
2020-03-03 14:44:51 +0200

    refactor: cleanup babel config

--- .config/babel/babel.config.es.js
+++ .config/babel/babel.config.es.js
@@ -22,20 +22,14 @@ module.exports = {
         "transform": "lodash/${member}",
         "preventFullImport": true
       },
-      // "@heathmont/sportsbet-assets\/?(((\\w*)?\/?)*)": {
-      //   "transform": "@heathmont/sportsbet-assets/lib/es/${1}/${member}",
-      // },
-      // "@heathmont/sportsbet-assets": {
-      //   "transform": "@heathmont/sportsbet-assets/lib/es/${member}",
-      // },
       "@heathmont/sportsbet-components": {
         "transform": "@heathmont/sportsbet-components/es/${member}",
       },
-      "@heathmont/sportsbet-global": {
-        "transform": "@heathmont/sportsbet-global/es/${member}",
-      },
       "@heathmont/sportsbet-fonts": {
         "transform": "@heathmont/sportsbet-fonts/es/${member}",
+      },
+      "@heathmont/sportsbet-global": {
+        "transform": "@heathmont/sportsbet-global/es/${member}",
       },
       "@heathmont/sportsbet-themes": {
         "transform": "@heathmont/sportsbet-themes/es/${member}",
