688ea01d618583435879906a89d743e5750c2470
Karl Kallavus <karl.kallavus@gmail.com>
2020-03-09 17:19:13 +0200

    feat(build): cleanup

--- .config/babel/babel.config.es.js
+++ .config/babel/babel.config.es.js
@@ -22,18 +22,6 @@ module.exports = {
         "transform": "lodash/${member}",
         "preventFullImport": true
       },
-      "@heathmont/sportsbet-components": {
-        "transform": "@heathmont/sportsbet-components/es/${member}",
-      },
-      "@heathmont/sportsbet-fonts": {
-        "transform": "@heathmont/sportsbet-fonts/es/${member}",
-      },
-      "@heathmont/sportsbet-global": {
-        "transform": "@heathmont/sportsbet-global/es/${member}",
-      },
-      "@heathmont/sportsbet-themes": {
-        "transform": "@heathmont/sportsbet-themes/es/${member}",
-      },
     }],
   ],
 };
