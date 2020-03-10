e27b4ccaf1d4be271940b0630efd926754833b81
Karl Kallavus <karl.kallavus@gmail.com>
2020-03-10 13:18:32 +0200

    feat(global): flat structure

--- packages/global/src/index.ts
+++ packages/global/src/index.ts
@@ -1,3 +1,3 @@
-export { default as polyfill } from './polyfill';
-export { default as Global } from './global';
-export * from './global';
+export { default as polyfill } from './polyfill/polyfill';
+export { default as Global } from './global/global';
+export * from './global/global';
