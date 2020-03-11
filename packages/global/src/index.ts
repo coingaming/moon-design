d87bbeac909eb467558746d8c60a4ad5e79d6814
Karl Kallavus <karl.kallavus@gmail.com>
2020-03-11 17:56:40 +0200

    feat(global): capitalize Global

--- packages/global/src/index.ts
+++ packages/global/src/index.ts
@@ -1,3 +1,3 @@
 export { default as polyfill } from './polyfill/polyfill';
-export { default as Global } from './global/global';
-export * from './global/global';
+export { default as Global } from './global/Global';
+export * from './global/Global';
