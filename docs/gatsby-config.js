0625604257b9180d0afb5a5bac7456b3d969beba
Karl Kallavus <karl.kallavus@gmail.com>
2020-06-09 16:04:06 +0300

    build: fix - remove sharp from docs

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -54,8 +54,6 @@ module.exports = {
       },
     },
     'gatsby-transformer-json',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
