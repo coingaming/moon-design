b6593c704bb48ae68bfa7266cf2a731e1ff1e49c
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2020-06-16 17:40:51 +0300

    feat: add text and caption components

---
+++ packages/components/src/caption/__tests__/caption.test.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { create } from 'react-test-renderer';
+import 'jest-styled-components';
+import { sportsbetDark, ThemeProvider } from '@heathmont/moon-themes';
+
+import Caption from '../Caption';
+
+const renderWithTheme = (component: JSX.Element) => (
+  <ThemeProvider theme={sportsbetDark}>{component}</ThemeProvider>
+);
+
+describe('Caption', () => {
+  describe('Elements', () => {
+    test('renders as <span> by default', () => {
+      const caption = create(renderWithTheme(<Caption>Caption</Caption>));
+      expect(caption).toMatchSnapshot();
+    });
+  });
+
+  test('renders default color', () => {
+    const caption = create(renderWithTheme(<Caption>Caption</Caption>));
+    expect(caption).toMatchSnapshot();
+  });
+
+  test('renders with a color', () => {
+    const caption = create(
+      renderWithTheme(<Caption color="piccolo.100">Caption</Caption>)
+    );
+    expect(caption).toMatchSnapshot();
+  });
+});
