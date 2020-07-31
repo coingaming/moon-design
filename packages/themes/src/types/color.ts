9ff44c92f7674acffe2e1e288610f26c5f6dadcf
Karl Kallavus <karl.kallavus@gmail.com>
2020-07-31 12:16:55 +0300

    feat: ts colours coverage

--- packages/themes/src/types/color.ts
+++ packages/themes/src/types/color.ts
@@ -49,6 +49,44 @@ export type ColorProps = ColorValue;
 
 export type Color = ColorBase & ColorThemed & ColorShared;
 
+export type ColorNames =
+  | 'piccolo.120'
+  | 'piccolo.100'
+  | 'piccolo.80'
+  | 'hit.120'
+  | 'hit.100'
+  | 'hit.80'
+  | 'goku.100'
+  | 'goku.80'
+  | 'goku.40'
+  | 'goku.10'
+  | 'gohan.100'
+  | 'gohan.80'
+  | 'gohan.40'
+  | 'gohan.10'
+  | 'beerus.100'
+  | 'goten.100'
+  | 'bulma.100'
+  | 'trunks.100'
+  | 'krillin.100'
+  | 'krillin.10'
+  | 'chiChi.100'
+  | 'chiChi.10'
+  | 'roshi.100'
+  | 'roshi.10'
+  | 'dodoria.100'
+  | 'dodoria.10'
+  | 'cell.100'
+  | 'cell.10'
+  | 'raditz.100'
+  | 'raditz.10'
+  | 'whis.100'
+  | 'whis.10'
+  | 'frieza.100'
+  | 'frieza.10'
+  | 'nappa.100'
+  | 'nappa.10';
+
 export type ColorBase = {
   text: ColorValue;
   background: ColorValue;
