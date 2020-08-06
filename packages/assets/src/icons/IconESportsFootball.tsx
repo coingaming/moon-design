193c5d83847f7db45648aabe97f547f74027a74d
andreisbitnev <andrei.sbitnev@hethmont.net>
2020-08-06 15:57:38 +0300

    chore: add icon raw files, build icons

--- packages/assets/src/icons/IconESportsFootball.tsx
+++ packages/assets/src/icons/IconESportsFootball.tsx
@@ -5,8 +5,8 @@ import { themed } from '@heathmont/moon-utils';
 
 const Svg = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
-    width="28"
-    height="28"
+    width="1em"
+    height="1em"
     viewBox="0 0 28 28"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
@@ -15,13 +15,7 @@ const Svg = (props: React.SVGProps<SVGSVGElement>) => (
     <path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="M4 4V24H10.1553V16.8034H14.3963L16.103 12.4121H10.1553V8.33759H17.3961L19 4H4Z"
-      fill="#FF5565"
-    />
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M16 23.7533C17.3294 24.1791 18.7629 24.0503 20.0051 23.3935C21.2472 22.7367 22.2032 21.6018 22.6761 20.2229C23.149 18.8441 23.1026 17.3262 22.5466 15.9824C21.9906 14.6385 20.9673 13.571 19.6876 13L16 23.7533Z"
+      d="M4 4v20h6.155v-7.197h4.241l1.707-4.39h-5.948V8.337h7.241L19 4H4zM16 23.753a5.167 5.167 0 004.005-.36c1.242-.656 2.198-1.791 2.671-3.17a6.02 6.02 0 00-.13-4.24c-.555-1.345-1.579-2.412-2.858-2.983L16 23.753z"
       fill="#FF5565"
     />
   </svg>
@@ -29,9 +23,11 @@ const Svg = (props: React.SVGProps<SVGSVGElement>) => (
 
 type IconProps = {
   backgroundColor?: ColorProps;
+  circleColor?: ColorProps;
+  color?: ColorProps;
 };
-const IconESportsLeagueOfLegends = styled(Svg)<IconProps>(
-  ({ backgroundColor, theme }) => [
+const IconESportsFootball = styled(Svg)<IconProps>(
+  ({ backgroundColor, circleColor, color, theme }) => [
     {
       verticalAlign: 'middle',
     },
@@ -41,6 +37,14 @@ const IconESportsLeagueOfLegends = styled(Svg)<IconProps>(
       overflow: 'visible',
       borderRadius: '50%',
     },
+    color && {
+      color: themed('color', color)(theme),
+    },
+    circleColor && {
+      circle: {
+        fill: themed('color', circleColor)(theme),
+      },
+    },
   ]
 );
-export default IconESportsLeagueOfLegends;
+export default IconESportsFootball;
--- packages/assets/src/icons/IconESportsRainbow.tsx
+++ packages/assets/src/icons/IconESportsRainbow.tsx
@@ -5,39 +5,41 @@ import { themed } from '@heathmont/moon-utils';
 
 const Svg = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
-    width="28"
-    height="28"
+    width="1em"
+    height="1em"
     viewBox="0 0 28 28"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
     <path
-      d="M8.00259 4.78872V22.6429C8.00259 22.6429 7.71461 26.9137 14.315 26.7685C14.315 26.7685 20.2522 27.4838 21 22.7854V11.4378C21 11.4378 20.5402 9.11763 16.4949 9.11763C16.4949 9.11763 13.7126 9.05958 13.5699 9.9491L13.5408 5.47764C13.5408 5.47764 14.4313 4.4746 15.578 5.50667L15.549 8.17265H21V4.78872C21 4.78872 20.9867 0.261833 14.5819 0.517855C14.5792 0.517855 8.49141 0.00313705 8.00259 4.78872Z"
+      d="M8.003 4.789v17.854s-.288 4.27 6.312 4.125c0 0 5.937.716 6.685-3.983V11.438s-.46-2.32-4.505-2.32c0 0-2.782-.058-2.925.831l-.03-4.471s.891-1.003 2.038.029l-.029 2.666H21V4.789S20.987.262 14.582.518c-.003 0-6.09-.515-6.58 4.27z"
       fill="#FDDC01"
     />
     <path
-      d="M12.2778 15.3075V23.5034H12.1113V23.9442H12.5473V24.1052H13.8552V23.9257H14.0296V19.6627C14.0296 19.6627 14.5026 19.0424 15.5965 19.1797V16.9519C15.5965 16.9519 16.5477 16.4636 18.3101 16.4636V14.4258C18.3101 14.4258 17.6548 13.4941 16.1329 14.5341C16.1329 14.5341 14.3943 15.4843 14.027 14.4179H12.5922V14.5605H12.2699V14.8614H12.114V15.194H12.2725L12.2778 15.3075Z"
-      fill="black"
-      stroke="black"
-      strokeWidth="0.223706"
-      strokeMiterlimit="10"
+      d="M12.278 15.307v8.196h-.167v.441h.436v.161h1.308v-.18h.175v-4.262s.473-.62 1.567-.483v-2.228s.95-.488 2.713-.488v-2.038s-.655-.932-2.177.108c0 0-1.739.95-2.106-.116h-1.435v.143h-.322v.3h-.156v.333h.159l.005.113z"
+      fill="#000"
+      stroke="#000"
+      strokeWidth={0.224}
+      strokeMiterlimit={10}
     />
     <path
-      d="M14.167 17.7464V19.0003H15.4485V17.2634L15.2688 16.9493H14.9729L14.9809 17.2449C14.9809 17.2449 15.2847 17.2977 15.3217 17.4877L15.2081 17.596C15.2107 17.5933 14.883 17.1024 14.167 17.7464Z"
+      d="M14.167 17.746V19h1.281v-1.737l-.18-.314h-.295l.008.296s.304.053.34.243l-.113.108c.003-.003-.325-.494-1.041.15z"
       fill="#FDDC01"
-      stroke="black"
-      strokeWidth="0.223706"
-      strokeMiterlimit="10"
+      stroke="#000"
+      strokeWidth={0.224}
+      strokeMiterlimit={10}
     />
   </svg>
 );
 
 type IconProps = {
   backgroundColor?: ColorProps;
+  circleColor?: ColorProps;
+  color?: ColorProps;
 };
 const IconESportsRainbow = styled(Svg)<IconProps>(
-  ({ backgroundColor, theme }) => [
+  ({ backgroundColor, circleColor, color, theme }) => [
     {
       verticalAlign: 'middle',
     },
@@ -46,6 +48,14 @@ const IconESportsRainbow = styled(Svg)<IconProps>(
       padding: backgroundColor ? '0.25em' : 0,
       overflow: 'visible',
       borderRadius: '50%',
+    },
+    color && {
+      color: themed('color', color)(theme),
+    },
+    circleColor && {
+      circle: {
+        fill: themed('color', circleColor)(theme),
+      },
     },
   ]
 );
