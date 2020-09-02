40f563988fd6f0dc3dc5e4c4c98072577b74971f
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2020-09-02 14:56:13 +0300

    feat: add luckyslots theme, update slots.io and aposta10 themes

--- packages/themes/src/luckyslotsLight/luckyslotsLight.ts
+++ packages/themes/src/luckyslotsLight/luckyslotsLight.ts
@@ -11,51 +11,52 @@ const color = {
     80: '#3D71F5',
   },
   hit: {
-    120: '#7C8192',
-    100: '#898D9A',
-    80: '#9598A3',
+    120: '#b7bac3',
+    100: '#c0c2c9',
+    80: '#c8cad0',
   },
   goten: {
     100: '#FFFFFF',
   },
   goku: {
-    100: '#232323',
-    80: '#303030',
-    40: '#4A4A4A',
-    10: '#636363',
+    100: '#F3F5F6',
+    80: '#EAEEF0',
+    40: '#D8E0E3',
+    10: '#C4D2D9',
   },
   gohan: {
-    100: '#1D1D1D',
-    80: '#292929',
-    40: '#363636',
-    10: '#4F4F4F',
+    100: '#FFFFFF',
+    80: '#f7f7f7',
+    40: '#e8e8e8',
+    10: '#d9d9d9',
   },
   beerus: {
-    100: '#333333',
+    100: '#E7E7E7',
   },
   bulma: {
-    100: '#FFFFFF',
+    100: '#03060C',
   },
   trunks: {
-    100: '#9E9E9E',
+    100: '#A6A6A6',
   },
 };
 
-const slotsDark: Theme = {
+const luckyslotsLight: Theme = {
   ...sharedTokens,
-  brand: 'Slots.io',
+  brand: 'LuckySlots',
   fontFace: avertaStd.fontFace,
   fontFamily: avertaStd.fontStack,
   fontSize: {
     body: 14,
   },
+  fontWeight: avertaStd.fontWeight,
   color: {
     text: color.bulma[100],
     background: color.goku[100],
     ...supportColors,
     ...color,
   },
-  colorScheme: 'dark',
+  colorScheme: 'light',
 };
 
-export default slotsDark;
+export default luckyslotsLight;
