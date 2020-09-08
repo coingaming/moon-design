3641b5550f2e33378bbe5634b7c38bd384e2054e
Karl Kallavus <karl.kallavus@gmail.com>
2020-09-08 16:37:05 +0300

    feat: picker grid

--- packages/components/src/datepicker/private/Picker.tsx
+++ packages/components/src/datepicker/private/Picker.tsx
@@ -10,7 +10,28 @@ import { Month } from './Month';
 import { Sidebar } from './Sidebar';
 import { InputsPanel } from './InputsPanel';
 
+const PickerContainer = styled.div(({ theme: { color } }) => ({
+  display: 'grid',
+  gridTemplateAreas: `
+      "sidebar months"
+      "inputs inputs"
+    `,
+  paddingTop: rem(16),
+  borderRadius: rem(3),
+  backgroundColor: color.goku[100],
+  boxShadow: `4px 4px 12px rgba(43, 75, 206, 0.04),
+    0px 10px 18px rgba(43, 75, 206, 0.04)`,
+  overflow: 'hidden',
+}));
+
+const SidebarWrapper = styled.div(({ theme: { color } }) => ({
+  gridArea: 'sidebar',
+  maxWidth: rem(120),
+  background: color.goku[100],
+}));
+
 const MonthsContainer = styled.div({
+  gridArea: 'months',
   display: 'flex',
   height: rem(306),
   position: 'relative',
@@ -21,36 +42,21 @@ const FirstMonth = styled.div({
 });
 
 const SecondMonth = styled.div({
-  marginRight: rem(11),
+  // marginRight: rem(11),
 });
 
 const LeftArrow = styled(IconArrowLeft as any)(({ theme }) => ({
   cursor: 'pointer',
-  color: theme.color.goten[100],
+  color: theme.color.bulma[100],
   position: 'absolute',
-  left: '15px',
+  left: rem(15),
 }));
 
 const RightArrow = styled(IconArrowRight as any)(({ theme }) => ({
   cursor: 'pointer',
-  color: theme.color.goten[100],
+  color: theme.color.bulma[100],
   position: 'absolute',
-  right: '15px',
-}));
-
-const SidebarWrapper = styled.div(({ theme: { color } }) => ({
-  width: rem(228),
-  background: color.goku[100],
-}));
-
-const PickerContainer = styled.div(({ theme: { color } }) => ({
-  display: 'flex',
-  paddingTop: rem(16),
-  borderRadius: rem(3),
-  backgroundColor: color.goku[100],
-  boxShadow: `4px 4px 12px rgba(43, 75, 206, 0.04),
-    0px 10px 18px rgba(43, 75, 206, 0.04)`,
-  overflow: 'hidden',
+  right: rem(15),
 }));
 
 export const Picker: React.FC<any> = ({
@@ -88,52 +94,50 @@ export const Picker: React.FC<any> = ({
           config={config}
         />
       </SidebarWrapper>
-      <div>
-        <MonthsContainer>
-          <LeftArrow onClick={prevMonth} />
-          <FirstMonth>
-            <Month
-              key="first"
-              monthDays={firstMonth}
-              monthLabel={firstMonthLabel}
-              year={firstMonthYearLabel}
-              weekDayLabels={weekDayLabels}
-              onDayClick={onDayClick}
-              onMouseEnter={onDayMouseEnter}
-              cursorDate={cursorDate}
-              startDate={startDate}
-              endDate={endDate}
-              hoveredDate={hoveredDate}
-            />
-          </FirstMonth>
-          <SecondMonth>
-            <Month
-              key="second"
-              monthDays={secondMonth}
-              monthLabel={secondMonthLabel}
-              year={secondMonthYearLabel}
-              weekDayLabels={weekDayLabels}
-              onDayClick={onDayClick}
-              onMouseEnter={onDayMouseEnter}
-              cursorDate={addMonths(cursorDate, 1)}
-              startDate={startDate}
-              endDate={endDate}
-              hoveredDate={hoveredDate}
-            />
-          </SecondMonth>
-          <RightArrow onClick={nextMonth} />
-        </MonthsContainer>
-        <InputsPanel
-          startDate={startDate}
-          endDate={endDate}
-          setStartDate={setStartDate}
-          setEndDate={setEndDate}
-          onDateChange={onDateChange}
-          translations={translations}
-          apply={apply}
-          config={config}
-        />
-      </div>
+      <MonthsContainer>
+        <LeftArrow onClick={prevMonth} />
+        <FirstMonth>
+          <Month
+            key="first"
+            monthDays={firstMonth}
+            monthLabel={firstMonthLabel}
+            year={firstMonthYearLabel}
+            weekDayLabels={weekDayLabels}
+            onDayClick={onDayClick}
+            onMouseEnter={onDayMouseEnter}
+            cursorDate={cursorDate}
+            startDate={startDate}
+            endDate={endDate}
+            hoveredDate={hoveredDate}
+          />
+        </FirstMonth>
+        <SecondMonth>
+          <Month
+            key="second"
+            monthDays={secondMonth}
+            monthLabel={secondMonthLabel}
+            year={secondMonthYearLabel}
+            weekDayLabels={weekDayLabels}
+            onDayClick={onDayClick}
+            onMouseEnter={onDayMouseEnter}
+            cursorDate={addMonths(cursorDate, 1)}
+            startDate={startDate}
+            endDate={endDate}
+            hoveredDate={hoveredDate}
+          />
+        </SecondMonth>
+        <RightArrow onClick={nextMonth} />
+      </MonthsContainer>
+      <InputsPanel
+        startDate={startDate}
+        endDate={endDate}
+        setStartDate={setStartDate}
+        setEndDate={setEndDate}
+        onDateChange={onDateChange}
+        translations={translations}
+        apply={apply}
+        config={config}
+      />
     </PickerContainer>
   );
 };
