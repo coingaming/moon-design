2c08aaba907b32af7331b83bc691bd505eee8f23
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2020-09-09 14:31:04 +0300

    feat: update React imports

--- packages/assets/src/duotones/ClaimedReward.tsx
+++ packages/assets/src/duotones/ClaimedReward.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { useTheme } from '@heathmont/moon-themes';
 import { themed } from '@heathmont/moon-utils';
