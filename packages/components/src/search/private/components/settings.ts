55aee60f601e695953accea38c62aa505f2bc1cd
Karl Kallavus <karl.kallavus@gmail.com>
2020-09-11 15:23:57 +0300

    feat: introduce search component

---
+++ packages/components/src/search/private/components/settings.ts
@@ -0,0 +1,16 @@
+export const zIndex = {
+  searchResults: 15,
+  searchInputActive: 20,
+  searchIcon: 20,
+  searchInput: 20,
+  notificationsDropdown: 21, // cheeky bump
+};
+
+export const inputConfig = {
+  height: 40, // lineHeight + paddingY + padding Y
+  lineHeight: 20,
+  paddingY: 10,
+  paddingX: 20,
+  iconSize: 20,
+  transition: 'color ease, background-color ease',
+};
