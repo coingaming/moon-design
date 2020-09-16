3e874cde8e53b94d1c06b564c7c9b3055d9acb4d
Karl Kallavus <karl.kallavus@gmail.com>
2020-09-16 19:31:52 +0300

    feat(assets): introduce country icons

---
+++ packages/assets/src/flags/IconFlagYT.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  fontSize: string | number;
+}
+
+const Flag = styled.span<Props>(({ fontSize }) => ({
+  fontSize,
+}));
+
+// Mayotte
+const IconFlagYT: React.FC<Props> = ({ fontSize, ...props }) => (
+  // eslint-disable-next-line jsx-a11y/accessible-emoji
+  <Flag role="img" aria-label="flag-yt" fontSize={fontSize} {...props}>
+    🇾🇹
+  </Flag>
+);
+
+export default IconFlagYT;
