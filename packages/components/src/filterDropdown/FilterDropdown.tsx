3cb015ee700cd078c133bc895423d5bfa1dfb96e
Mikita Lisavets <mikita.lisavets@gmail.com>
2020-09-17 14:31:46 +0300

    feat: add FilterDropdown component

---
+++ packages/components/src/filterDropdown/FilterDropdown.tsx
@@ -0,0 +1,38 @@
+import { rem } from '@heathmont/moon-utils';
+import React from 'react';
+import styled from 'styled-components';
+
+import Search from './private/Search';
+
+type FilterDropdownProps = {
+  searchPlaceholder?: string;
+  onSearchChange: (value: string) => void;
+  children?: JSX.Element | JSX.Element[];
+};
+
+const FilterDropdownWrapper = styled.div(({ theme }) => ({
+  backgroundColor: theme.color.gohan[100],
+  borderRadius: theme.radius.default,
+  boxShadow: theme.boxShadow,
+  overflow: 'hidden',
+  fontSize: rem(14),
+  padding: rem(12),
+}));
+
+const Header = styled.div({
+  marginBottom: rem(22),
+});
+
+const FilterDropdown: React.FC<FilterDropdownProps> = props => {
+  const { searchPlaceholder, onSearchChange, children } = props;
+  return (
+    <FilterDropdownWrapper>
+      <Header>
+        <Search placeholder={searchPlaceholder} onChange={onSearchChange} />
+      </Header>
+      {children}
+    </FilterDropdownWrapper>
+  );
+};
+
+export default FilterDropdown;
