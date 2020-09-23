89f3dab426f9dbb70a2a6d5ad9235c2aef1b5f72
Artem Markov <drownbes@gmail.com>
2020-09-23 11:31:52 +0300

    WIP: initial commit

---
+++ packages/components/src/calendar/Calendar.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Month = styled.div`
+  display: grid;
+  grid-template-columns: repeat(7, minmax(188px, 1fr));
+  grid-auto-rows: minmax(188px, 1fr);
+`;
