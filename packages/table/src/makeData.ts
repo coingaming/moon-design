4425d60d58f3d3581285749299da7e06b03963db
Karl Kallavus <karl.kallavus@gmail.com>
2020-10-01 11:36:17 +0300

    feat: initial table component

---
+++ packages/table/src/makeData.ts
@@ -0,0 +1,38 @@
+const range = (len: number) => {
+  const arr = []
+  for (let i = 0; i < len; i++) {
+    arr.push(i)
+  }
+  return arr
+}
+
+const newPerson = () => {
+  const statusChance = Math.random()
+  return {
+    firstName: "test",
+    lastName: "test",
+    age: Math.floor(Math.random() * 30),
+    visits: Math.floor(Math.random() * 100),
+    progress: Math.floor(Math.random() * 100),
+    status:
+      statusChance > 0.66
+        ? 'relationship'
+        : statusChance > 0.33
+        ? 'complicated'
+        : 'single',
+  }
+}
+
+export default function makeData(...lens: number[]) {
+  const makeDataLevel = (depth = 0): any => {
+    const len = lens[depth]
+    return range(len).map(() => {
+      return {
+        ...newPerson(),
+        subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
+      }
+    })
+  }
+
+  return makeDataLevel()
+}
