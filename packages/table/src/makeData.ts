1d2863c0cec36a3008b5bf898813cbcb9d10164b
Karl Kallavus <karl.kallavus@gmail.com>
2020-10-02 13:06:07 +0300

    chore: lint

--- packages/table/src/makeData.ts
+++ packages/table/src/makeData.ts
@@ -1,16 +1,16 @@
 const range = (len: number) => {
-  const arr = []
+  const arr = [];
   for (let i = 0; i < len; i++) {
-    arr.push(i)
+    arr.push(i);
   }
-  return arr
-}
+  return arr;
+};
 
 const newPerson = () => {
-  const statusChance = Math.random()
+  const statusChance = Math.random();
   return {
-    firstName: "test",
-    lastName: "test",
+    firstName: 'test',
+    lastName: 'test',
     age: Math.floor(Math.random() * 30),
     visits: Math.floor(Math.random() * 100),
     progress: Math.floor(Math.random() * 100),
@@ -20,19 +20,19 @@ const newPerson = () => {
         : statusChance > 0.33
         ? 'complicated'
         : 'single',
-  }
-}
+  };
+};
 
 export default function makeData(...lens: number[]) {
   const makeDataLevel = (depth = 0): any => {
-    const len = lens[depth]
+    const len = lens[depth];
     return range(len).map(() => {
       return {
         ...newPerson(),
         subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
-      }
-    })
-  }
+      };
+    });
+  };
 
-  return makeDataLevel()
+  return makeDataLevel();
 }
