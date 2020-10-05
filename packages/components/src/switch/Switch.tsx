f594791068f583e4f10fe9ffafabbb0563b91c06
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2020-10-05 14:53:49 +0300

    fix: update imports from 'polished' package

--- packages/components/src/switch/Switch.tsx
+++ packages/components/src/switch/Switch.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { rem, uniqueId, inlineSvg } from '@heathmont/moon-utils';
-import { hideVisually } from 'polished';
+import hideVisually from 'polished/lib/mixins/hideVisually';
 
 import IconMoon from '../private/icons/IconMoon';
 import IconSun from '../private/icons/IconSun';
