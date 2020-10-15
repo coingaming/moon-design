0a24bed2bc0ff4bf5f79e073e5af1ecab329cabc
Karl Kallavus <karl.kallavus@gmail.com>
2020-10-15 16:56:26 +0300

    feat: missions logo

--- packages/assets/src/logos/index.ts
+++ packages/assets/src/logos/index.ts
@@ -4,6 +4,8 @@
 
 export { default as LogoBtcxe } from './LogoBtcxe';
 export * from './LogoBtcxe';
+export { default as LogoMissions } from './LogoMissions';
+export * from './LogoMissions';
 export { default as LogoSportsbet } from './LogoSportsbet';
 export * from './LogoSportsbet';
 export { default as LogoSportsbetSponsor } from './LogoSportsbetSponsor';
