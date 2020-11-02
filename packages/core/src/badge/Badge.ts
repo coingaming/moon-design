8ff37804ef3249c036d3d72e3cd8409b5eb24164
Karl Kallavus <karl.kallavus@gmail.com>
2020-11-02 11:05:24 +0200

    feat(core): introduce core package

---
+++ packages/core/src/badge/Badge.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { rem, themed } from '@heathmont/moon-utils';
+import { ColorProps } from '@heathmont/moon-themes';
+
+export type BadgeProps = {
+  color?: ColorProps;
+  backgroundColor?: ColorProps;
+  size?: 'xsmall' | 'small';
+};
+
+const Badge = styled.span<BadgeProps>(
+  ({ theme: { fontWeight, radius } }) => ({
+    display: 'inline-flex',
+    fontWeight: fontWeight.semibold,
+    textTransform: 'uppercase',
+    borderRadius: rem(radius.largest),
+    letterSpacing: rem(1), // TODO
+  }),
+  ({ size }) => ({
+    padding: size === 'small' ? `0 ${rem(8)}` : `0 ${rem(4)}`,
+    fontSize: size === 'small' ? rem(10) : rem(8),
+    lineHeight: size === 'small' ? rem(16) : rem(12),
+  }),
+  /* If a color or backgroundColor is set, override the modifier styles. */
+  ({ color, backgroundColor, theme }) => ({
+    color: themed('color', color)(theme),
+    backgroundColor: themed('color', backgroundColor)(theme),
+  })
+);
+
+Badge.defaultProps = {
+  size: 'xsmall',
+};
+
+export default Badge;
