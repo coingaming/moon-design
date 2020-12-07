e8bfffe11da67b0ad31d19aea746fe33fc8da6fe
Mikita Lisavets <mikita.lisavets@gmail.com>
2020-12-07 12:32:04 +0200

    improvement: update for charts

---
+++ packages/charts/src/private/icons/LineChartLoadingIcon.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LineChartLoadingIcon = () => (
+  <svg width="160" height="160" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M40 83.682C19.661 83.682 0 0 0 0v160h160v-50.209s-20.339-44.184-40-44.184c-20.339 0-20.339 2.008-40 2.008-20.339 0-20.339 16.067-40 16.067z"
+      fill="#D8E0E3"
+    />
+    <path
+      d="M52.881 93.603C26.441 93.603 0 29 0 29v131h160V45.15s-27.119 100.494-53.559 100.494C80 145.644 80 93.603 52.881 93.603z"
+      fill="#EAEEF0"
+    />
+  </svg>
+);
+
+export default LineChartLoadingIcon;
