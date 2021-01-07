d425e221172087ba789663b834f12b4a629e533e
Karl Kallavus <karl.kallavus@gmail.com>
2021-01-07 16:00:35 +0200

    feat: BallLoader -> ScreenLoader

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,8 +4,6 @@
 
 export { default as Badge } from './badge/Badge';
 export * from './badge/Badge';
-export { default as BallLoader } from './ballLoader/BallLoader';
-export * from './ballLoader/BallLoader';
 export { default as Caption } from './caption/Caption';
 export * from './caption/Caption';
 export { default as Checkbox } from './checkbox/Checkbox';
@@ -16,5 +14,7 @@ export { default as HeadingSize } from './heading/HeadingSize';
 export * from './heading/HeadingSize';
 export { default as Loader } from './loader/Loader';
 export * from './loader/Loader';
+export { default as ScreenLoader } from './screenLoader/ScreenLoader';
+export * from './screenLoader/ScreenLoader';
 export { default as Text } from './text/Text';
 export * from './text/Text';
