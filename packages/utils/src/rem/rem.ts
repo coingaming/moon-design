d3a21b9b01d524d0d1252a8c280e4c5a0b57c059
Karl Kallavus <karl.kallavus@gmail.com>
2021-01-08 14:46:28 +0200

    feat: disable rem warnings

--- packages/utils/src/rem/rem.ts
+++ packages/utils/src/rem/rem.ts
@@ -1,6 +1,7 @@
 import polishedRem from 'polished/lib/helpers/rem';
 import { sharedTokens } from '@heathmont/moon-themes';
 
+/*
 const EXCEPTIONS = [
   -1,
   -2,
@@ -23,17 +24,18 @@ const EXCEPTIONS = [
   '14px',
   '18px',
 ];
+*/
 
 const rem = (value: string | number, baseFontSize: string | number = 16) => {
-  const isNotAlloved = !EXCEPTIONS.includes(value);
-  const isNotDivisibleBy4 = !(typeof value === 'number' && value % 4 === 0);
-  const isNotProduction = process.env.NODE_ENV !== 'production';
-  if (isNotAlloved && isNotDivisibleBy4 && isNotProduction) {
-    // eslint-disable-next-line no-console
-    console.warn(
-      `ATTENTION! You've used rem(${value}) which is out of Moon DS range! Please check documentation.`
-    );
-  }
+  // const isNotAlloved = !EXCEPTIONS.includes(value);
+  // const isNotDivisibleBy4 = !(typeof value === 'number' && value % 4 === 0);
+  // const isNotProduction = process.env.NODE_ENV !== 'production';
+  // if (isNotAlloved && isNotDivisibleBy4 && isNotProduction) {
+  //   // eslint-disable-next-line no-console
+  //   console.warn(
+  //     `ATTENTION! You've used rem(${value}) which is out of Moon DS range! Please check documentation.`
+  //   );
+  // }
   return polishedRem(value, baseFontSize || sharedTokens.base.fontSize);
 };
 
