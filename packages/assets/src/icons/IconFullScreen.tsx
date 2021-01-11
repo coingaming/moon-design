34a70c4e2d9a67d951552f9acb09d7aec410d5cb
Karl Kallavus <karl.kallavus@gmail.com>
2021-01-11 17:23:21 +0200

    fix(icons): align hub88 icons

--- packages/assets/src/icons/IconFullScreen.tsx
+++ packages/assets/src/icons/IconFullScreen.tsx
@@ -7,19 +7,17 @@ const Svg = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     width="1em"
     height="1em"
-    viewBox="0 0 22 18"
+    viewBox="0 0 32 32"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <rect
-      x={1}
-      y={1}
-      width={20}
-      height={16}
-      rx={5}
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M22.818 5H8.273A6.273 6.273 0 002 11.273V20a6.273 6.273 0 006.273 6.273h14.545A6.273 6.273 0 0029.091 20v-8.727A6.273 6.273 0 0022.818 5zM8.273 4h14.545a7.273 7.273 0 017.273 7.273V20a7.273 7.273 0 01-7.273 7.273H8.273A7.273 7.273 0 011 20v-8.727A7.273 7.273 0 018.273 4z"
+      fill="currentColor"
       stroke="currentColor"
-      strokeWidth={2}
       strokeMiterlimit={10}
       strokeLinecap="round"
       strokeLinejoin="round"
