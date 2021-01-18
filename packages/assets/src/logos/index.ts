21c08551f4dedda764780166ab0af39244deb0f1
Karl Kallavus <karl.kallavus@gmail.com>
2021-01-18 16:10:27 +0200

    feat(assets): powered by hub88

--- packages/assets/src/logos/index.ts
+++ packages/assets/src/logos/index.ts
@@ -38,6 +38,8 @@ export { default as LogoMoneyball } from './LogoMoneyball';
 export * from './LogoMoneyball';
 export { default as LogoMoonDesign } from './LogoMoonDesign';
 export * from './LogoMoonDesign';
+export { default as LogoPoweredByHub88 } from './LogoPoweredByHub88';
+export * from './LogoPoweredByHub88';
 export { default as LogoSlotsFull } from './LogoSlotsFull';
 export * from './LogoSlotsFull';
 export { default as LogoSlotsShort } from './LogoSlotsShort';
