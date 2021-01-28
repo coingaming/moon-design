02374e913bf2b12ddcaaea74f1597c170249a029
Karl Kallavus <karl.kallavus@gmail.com>
2021-01-28 13:56:08 +0200

    feat: sidebar logic

---
+++ docs/src/components/sidebar/MenuItem.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export const MenuItem = ({ title, isActive }) => {
+  return (
+    <div className="space-y-1">
+      {/* Current: "bg-gray-100 text-gray-900", Default: "bg-white text-gray-600 hover:bg-gray-50 hover:text-gray-900" */}
+      <a
+        href="#"
+        className="bg-gray-100 text-gray-900 group w-full flex items-center pl-2 py-2 text-sm font-medium rounded-md"
+      >
+        {/* Current: "text-gray-600", Default: "text-gray-400 group-hover:text-gray-500" */}
+        {/* Heroicon name: home */}
+        <svg
+          className="text-gray-600 mr-3 h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+          />
+        </svg>
+        { title }
+      </a>
+    </div>
+  );
+};
