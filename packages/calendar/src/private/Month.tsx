8d01c727ef07011cd56fdcd71a6c99ff120316c5
Mikita Lisavets <mikita.lisavets@gmail.com>
2021-04-27 10:34:20 +0300

    feat: add weekend minimizing

--- packages/calendar/src/private/Month.tsx
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Month = styled.div`
-  display: grid;
-  grid-template-columns: repeat(7, minmax(50px, 1fr));
-`;
--- packages/calendar/src/private/Month/MonthGrid.tsx
+++ packages/calendar/src/private/Month/MonthGrid.tsx
@@ -1,19 +1,16 @@
 import styled from 'styled-components';
-import { rem } from '@heathmont/moon-utils';
 
 import { getGridTemplateColumns } from '../getGridTemplateColumns';
 
-export const WeekGrid = styled.div<{
+export const MonthGrid = styled.div<{
   weekStartsOn: number;
   isWeekendMinimized: boolean;
 }>(
   ({ weekStartsOn, isWeekendMinimized }) => `
-  height: 100%;
   display: grid;
   grid-template-columns: ${getGridTemplateColumns({
     weekStartsOn,
     isWeekendMinimized,
   })};
-  grid-template-rows: ${rem(24)} auto;
 `
 );
