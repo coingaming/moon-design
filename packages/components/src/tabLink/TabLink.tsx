14552648c5740cae1afe62bf14f7fab802941e9b
Ulysses <ulyssesmurja@gmail.com>
2021-05-25 16:47:15 +0300

    feat(tablink): add fullwidth prop to tablink (#1207)

--- packages/components/src/tabLink/TabLink.tsx
+++ packages/components/src/tabLink/TabLink.tsx
@@ -3,40 +3,51 @@ import rem from 'polished/lib/helpers/rem';
 
 import { underlineWidth, underlineOffset } from '../tabs/private';
 
-const TabLink = styled.a(({ theme }) => ({
-  position: 'relative',
-  padding: 0,
-  paddingBottom: `${underlineOffset(theme)}px`,
-  margin: 0,
-  fontSize: rem(14),
-  lineHeight: 1.3,
-  fontFamily: 'inherit',
-  backgroundColor: 'transparent',
-  color: theme.color.trunks[100],
-  transition: `color ${theme.transitionDuration.default}s ease`,
-  textDecoration: 'none',
-  border: 0,
-  appearance: 'none',
-  '&:after': {
-    content: '""',
-    position: 'absolute',
-    height: `${underlineWidth(theme)}px`,
-    width: '100%',
-    left: 0,
-    right: 0,
-    bottom: 0,
-    margin: '0 auto',
-    backgroundColor: theme.color.piccolo[100],
-    transform: 'scaleX(0)',
-    transition: `transform ${theme.transitionDuration.default}s ease`,
+type TabLinkProps = {
+  fullWidth?: boolean;
+};
+
+const TabLink = styled.a<TabLinkProps>(({ theme, fullWidth }) => [
+  fullWidth && {
+    display: 'flex',
+    flex: 'auto',
+    justifyContent: 'center',
   },
-  '&:hover, &:focus, &:active, &.active, &[aria-current=page]': {
-    color: theme.color.bulma[100],
-    cursor: 'pointer',
+  {
+    position: 'relative',
+    padding: 0,
+    paddingBottom: `${underlineOffset(theme)}px`,
+    margin: 0,
+    fontSize: rem(14),
+    lineHeight: 1.3,
+    fontFamily: 'inherit',
+    backgroundColor: 'transparent',
+    color: theme.color.trunks[100],
+    transition: `color ${theme.transitionDuration.default}s ease`,
+    textDecoration: 'none',
+    border: 0,
+    appearance: 'none',
     '&:after': {
-      transform: 'scaleX(1)',
+      content: '""',
+      position: 'absolute',
+      height: `${underlineWidth(theme)}px`,
+      width: '100%',
+      left: 0,
+      right: 0,
+      bottom: 0,
+      margin: '0 auto',
+      backgroundColor: theme.color.piccolo[100],
+      transform: 'scaleX(0)',
+      transition: `transform ${theme.transitionDuration.default}s ease`,
+    },
+    '&:hover, &:focus, &:active, &.active, &[aria-current=page]': {
+      color: theme.color.bulma[100],
+      cursor: 'pointer',
+      '&:after': {
+        transform: 'scaleX(1)',
+      },
     },
   },
-}));
+]);
 
 export default TabLink;
