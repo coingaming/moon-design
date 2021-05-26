8df6f7c92cb2be1fcdd3921a96b79aa58195cd53
Karl Kallavus <karl.kallavus@gmail.com>
2021-05-26 15:45:49 +0300

    feat: initial commit

---
+++ next-docs/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Moon Design System</title>
+        <meta name="description" content="Moon Design System" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Moon Design System</h1>
+      </main>
+    </>
+  );
+}
