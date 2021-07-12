e2a210413d53dc7323bbd0112eb4cae2a8e912d5
Karl Kallavus <karl.kallavus@gmail.com>
2021-07-12 17:34:54 +0300

    refactor: animation

--- next-docs/components/facing/MoonAndEarth.tsx
+++ next-docs/components/facing/MoonAndEarth.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import { useViewportScroll, useTransform } from 'framer-motion';
+import { useViewportScroll, useTransform, motion } from 'framer-motion';
 
 import imageMoon from '../../public/moon.png';
 import imageEarth from '../../public/earth.png';
@@ -15,39 +15,28 @@ const MoonAndEarthAnimation = () => {
     scrollYProgress,
     (scrollYProgress) => 1 - scrollYProgress * 0.5
   );
-
-  const [hookedEarthScale, setHookedEarthScale] = React.useState(1);
-  const [hookedMoonScale, setHookedMoonScale] = React.useState(1);
-  React.useEffect(() => {
-    const unsubscribeEarthScale = earthScale.onChange((v) =>
-      setHookedEarthScale(v)
-    );
-    const unsubscribeMoonScale = moonScale.onChange((v) =>
-      setHookedMoonScale(v)
-    );
-    return () => {
-      unsubscribeEarthScale();
-      unsubscribeMoonScale();
-    };
-  }, [earthScale, moonScale]);
+  const earthRight = useTransform(
+    scrollYProgress,
+    (scrollYProgress) => -scrollYProgress * 1000
+  );
 
   return (
     <>
       {/* Moon background image */}
-      <div
-        style={{ transform: `scale(${hookedEarthScale})` }}
-        className="fixed right-0 top-300 z-20 w-5/12"
+      <motion.div
+        style={{ scale: earthScale, top: 300, right: earthRight }}
+        className="fixed z-20 w-5/12"
       >
         <Image src={imageEarth} alt="Earth" />
-      </div>
+      </motion.div>
 
       {/* Moon background image */}
-      <div
-        style={{ transform: `scale(${hookedMoonScale}` }}
-        className="fixed right-0 top-250 z-10 w-5/12"
+      <motion.div
+        style={{ top: 250, right: 0, scale: moonScale }}
+        className="fixed z-10 w-5/12"
       >
         <Image src={imageMoon} alt="Moon" />
-      </div>
+      </motion.div>
     </>
   );
 };
