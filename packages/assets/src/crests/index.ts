75e29d371740151946761dc3ffc471b4b7c708d6
Karl Kallavus <karl.kallavus@heathmont.net>
2021-07-13 13:29:45 +0300

    feat: Add a Sao Paulo logo to the system B1M2-2626 (#1251)

--- packages/assets/src/crests/index.ts
+++ packages/assets/src/crests/index.ts
@@ -6,6 +6,8 @@ export { default as CrestArsenal } from './CrestArsenal';
 export * from './CrestArsenal';
 export { default as CrestFlamengo } from './CrestFlamengo';
 export * from './CrestFlamengo';
+export { default as CrestSaoPaulo } from './CrestSaoPaulo';
+export * from './CrestSaoPaulo';
 export { default as CrestSouthampton } from './CrestSouthampton';
 export * from './CrestSouthampton';
 export { default as CrestWatford } from './CrestWatford';
