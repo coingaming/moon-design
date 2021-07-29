f2ea0aba4e0c91aae842ee9c811a20661648500b
Karl Kallavus <karl.kallavus@heathmont.net>
2021-07-29 16:35:36 +0300

    feat: remove docs source

--- docs/src/types/declarations/mdx.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.mdx' {
-  /* Handled by MDX */
-  let MDXComponent: (props: any) => JSX.Element;
-  export default MDXComponent;
-}
-
-declare module '@mdx-js/tag';
-declare module 'gatsby-mdx/mdx-renderer';
-declare module 'mdx-utils';
--- docs/src/types/declarations/styled-components.d.ts
+++
@@ -1,7 +0,0 @@
-// import original module declarations
-import 'styled-components';
-import { Theme } from '@heathmont/moon-themes';
-
-declare module 'styled-components' {
-  export interface DefaultTheme extends Theme {}
-}
