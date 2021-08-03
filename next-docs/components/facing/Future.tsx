705e5401e4f750edf46b6ed2cb2b0038214cb50b
Karl Kallavus <karl.kallavus@heathmont.net>
2021-08-03 12:56:29 +0300

    fix: responsive Future block

--- next-docs/components/facing/Future.tsx
+++ next-docs/components/facing/Future.tsx
@@ -7,20 +7,20 @@ import TopRightArrowIcon from '../icons/TopRightArrowIcon';
 
 const Future = () => (
   <Link href="/how-to-contribute">
-    <a className="group block relative future rounded-lg p-11">
+    <a className="max-w-screen-lg group block relative rounded-lg p-11">
       <h2 className="relative text-white text-5xl tracking-tight z-10">
         Want to contribute?
       </h2>
-      <p className="mt-8 w-1/2 relative text-2xl text-white z-10 group-hover:underline">
+      <p className="mt-8 md:w-1/2 relative text-2xl text-white z-10 group-hover:underline">
         Start building with our HTML/CSS framework and rapidly go from prototype
         to production.
       </p>
       <div className="absolute top-11 right-11 text-white z-10">
         <TopRightArrowIcon />
       </div>
-      <div className="w-full h-full">
+      <div className="w-full md:h-full">
         <Image
-          className="rounded-lg"
+          className="rounded-3xl object-left"
           alt="Future"
           src={imageFuture}
           layout="fill"
