cf265e898fe05156115c6337b1abd56dea8b7880
neemeeller <neeme.eller@uptime.eu>
2021-08-04 17:31:50 +0300

    feature/B1M2-2919 base component for moon-core search

---
+++ packages/core/src/search/Search.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { SearchForm } from './private/components/Form';
+import { SearchInput, SearchInputIcon } from './private/components/Input';
+import { SearchResults } from './private/components/SearchResults';
+
+interface Result {
+  title: JSX.Element;
+  items?: [JSX.Element];
+}
+
+export type SearchProps = {
+  onSubmit?: (e: React.FormEvent<HTMLFormElement>) => void;
+  query?: string;
+  results?: [Result];
+  loadingMessage?: JSX.Element;
+  placeholder?: string;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
+const Search: React.FC<SearchProps> = ({
+  query,
+  placeholder,
+  onSubmit,
+  onChange,
+  results,
+  loadingMessage,
+  ...props
+}) => (
+    <SearchForm onSubmit={onSubmit}>
+      <SearchInput
+        value={query}
+        placeholder={placeholder}
+        type="text"
+        autoComplete="off"
+        required
+        onChange={onChange}
+        {...props}
+      />
+      <SearchInputIcon />
+      {(results || loadingMessage) && (
+        <SearchResults loadingMessage={loadingMessage} results={results} />
+      )}
+      <input type="submit" hidden />
+    </SearchForm>
+);
+
+export default Search;
---
+++ packages/core/src/search/private/components/SearchResults.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import {
+  Results,
+  ResultsItem,
+  ResultsItemText,
+  ResultsGroup,
+  ResultsLink,
+} from './Results';
+
+interface Result {
+  title: JSX.Element;
+  items?: [JSX.Element];
+}
+
+interface SearchResultsProps {
+  loadingMessage?: JSX.Element;
+  results?: [Result];
+}
+
+export const SearchResults: React.FC<SearchResultsProps> = ({
+  loadingMessage,
+  results,
+}) => (
+    <Results>
+      {loadingMessage && !results && (
+        <ResultsItem>
+          <ResultsItemText>{loadingMessage}</ResultsItemText>
+        </ResultsItem>
+      )}
+
+      {results
+        && results.map((result, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <ResultsItem key={index}>
+            <ResultsItemText>{result.title}</ResultsItemText>
+            {result.items && (
+              <ResultsGroup>
+                {result.items.map((item, itemIndex) => (
+                  // eslint-disable-next-line react/no-array-index-key
+                  <ResultsLink key={itemIndex}>{item}</ResultsLink>
+                ))}
+              </ResultsGroup>
+            )}
+          </ResultsItem>
+        ))}
+    </Results>
+);
