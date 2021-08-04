fada569617d3def55551806554eb58299f01bc0a
neemeeller <neeme.eller@uptime.eu>
2021-08-04 15:01:13 +0300

    Remove Preview component

--- next-docs/pages/components/searchPopup.tsx
+++ next-docs/pages/components/searchPopup.tsx
@@ -3,103 +3,45 @@ import React from 'react';
 
 import { SearchPopup } from '@heathmont/moon-components';
 
-import Preview from '../../components/codePreview/Preview';
-
 const PreviewSearchPopup = () => {
   return (
-    <Preview
-      title="Results"
-      preview={
-        <div className="h-96">
-          <SearchPopup
-            placeholder="Search results"
-            closeButtonText="Close"
-            onChange={(e) => {
-              console.log(e.target.value);
+    <div className="h-96">
+      <SearchPopup
+        placeholder="Search results"
+        closeButtonText="Close"
+        onChange={(e) => {
+          console.log(e.target.value);
+        }}
+        onSubmit={(e) => {
+          console.log('Submitted');
+        }}
+        results={[
+          <span
+            onClick={() => {
+              console.log('Click');
             }}
-            onSubmit={(e) => {
-              console.log('Submitted');
-            }}
-            results={[
-              <span
-                onClick={() => {
-                  console.log('Click');
-                }}
-              >
-                First Game
-              </span>,
-              <span>Second Game</span>,
-              <span>Third Game</span>,
-            ]}
-          />
-        </div>
-      }
-      code={`import { Search } from '@heathmont/moon-components';
-
-<SearchPopup
-  placeholder="Search"
-  onChange={(e) => {
-    console.log(e.target.value);
-  }}
-  onSubmit={(e) => {
-    console.log('Submitted');
-  }}
-  results={[
-    {
-      title: <span>Games</span>,
-      items: [
-        <span
-          onClick={() => {
-            console.log('Click');
-          }}
-        >
-          First Game
-        </span>,
-        <span>Second Game</span>,
-        <span>Third Game</span>,
-      ],
-    },
-    {
-      title: <span>Providers</span>,
-      items: [
-        <span>Provider One</span>,
-        <span>Provider Two</span>,
-        <span>Provider Three</span>,
-      ],
-    },
-  ]}
-/>
-`}
-    />
+          >
+            First Game
+          </span>,
+          <span>Second Game</span>,
+          <span>Third Game</span>,
+        ]}
+      />
+    </div>
   );
 };
 
 const PreviewSearchPopupLoading = () => {
   return (
-    <Preview
-      title="Loading message"
-      preview={
-        <div className="h-40">
-          <SearchPopup
-            placeholder="Search"
-            onChange={(e) => {
-              console.log(e.target.value);
-            }}
-            loadingMessage={<p>Loading...</p>}
-          />
-        </div>
-      }
-      code={`import { Search } from '@heathmont/moon-components';
-
-<SearchPopup
-  placeholder="Search"
-  onChange={e => {
-    console.log(e.target.value);
-  }}
-  loadingMessage={<p>Loading...</p>}
-/>
-`}
-    />
+    <div className="h-40">
+      <SearchPopup
+        placeholder="Search"
+        onChange={(e) => {
+          console.log(e.target.value);
+        }}
+        loadingMessage={<p>Loading...</p>}
+      />
+    </div>
   );
 };
 
