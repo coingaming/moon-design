f2f3a127e5aa02013c4ddeaf43f368f86c0f7a04
Karl Kallavus <karl.kallavus@heathmont.net>
2021-08-24 16:22:13 +0300

    feat: update Contributors

--- next-docs/components/facing/Contributors.tsx
+++ next-docs/components/facing/Contributors.tsx
@@ -8,10 +8,15 @@ import imageKarlKallavus from '../../public/contributors/karlkallavus.png';
 import imageMikitaLisavets from '../../public/contributors/mikitalisavets.jpg';
 import imageEgertTamm from '../../public/contributors/egerttamm.png';
 import imagePavelNavolochko from '../../public/contributors/pavelnavolochko.png';
+import imageDmytroKireiev from '../../public/contributors/dmytrokireiev.jpg';
 
 const Contributors = () => (
   <FacingReel>
-    <CardContributor name="Karl Kallavus" title="Lead Developer" imageSrc={imageKarlKallavus} />
+    <CardContributor
+      name="Karl Kallavus"
+      title="Lead Developer"
+      imageSrc={imageKarlKallavus}
+    />
     <CardContributor
       variant="tiny"
       name="Egert Tamm"
@@ -30,7 +35,17 @@ const Contributors = () => (
       title="Head of Design"
       imageSrc={imagePavelNavolochko}
     />
-    <CardContributor name="Mikita Lisavets" title="Developer" imageSrc={imageMikitaLisavets} />
+    <CardContributor
+      name="Mikita Lisavets"
+      title="Developer"
+      imageSrc={imageMikitaLisavets}
+    />
+    <CardContributor
+      variant="tiny"
+      name="Dmytro Kireiev"
+      title="Developer"
+      imageSrc={imageDmytroKireiev}
+    />
   </FacingReel>
 );
 
