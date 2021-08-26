686bf026ec2853114fbd14f9251799653d436389
Alexis Azanza <alexis@devtailor.com>
2021-08-26 09:34:10 +0300

    Added chip docs

--- next-docs/pages/components/chip.tsx
+++ next-docs/pages/components/chip.tsx
@@ -1,70 +1,93 @@
 import React from 'react';
 
-import { Badge } from '@heathmont/moon-core';
+import { Chip } from '@heathmont/moon-components';
+import { MediaMusic, SoftwareDownload } from '@heathmont/moon-icons';
 
 import Preview from '../../components/codePreview/Preview';
 
 export default function PageChip() {
-    return (
-      <>
-        <section className="mt-8">
-          <h1 className="text-5xl font-semibold">Badge</h1>
-          <p className="text-lg mt-4">Small count and labeling component.</p>
-        </section>
+  return (
+    <>
+      <section className="mt-8">
+        <h1 className="text-5xl font-semibold">Chip</h1>
+      </section>
+
+      {/* Default */}
+      <section className="mt-8">
+        <Preview
+          title="Default"
+          preview={
+            <div className="flex justify-around items-center w-full">
+              <Chip>Lion</Chip>
+              <Chip>Tiger</Chip>
+              <Chip>Jaguar</Chip>
+            </div>
+          }
+          code={`import { Chip } from '@heathmont/moon-core';
   
-        {/* Colours */}
-        <section className="mt-8">
-          <Preview
-            title="Colours"
-            preview={
-              <div className="flex justify-around items-center w-full">
-                <Badge color="goten.100" backgroundColor="piccolo.100">
-                  Active
-                </Badge>
-                <Badge color="piccolo.100" backgroundColor="trunks.100">
-                  Active
-                </Badge>
-                <Badge color="goten.100" backgroundColor="dodoria.100">
-                  Active
-                </Badge>
-              </div>
-            }
-            code={`import { Badge } from '@heathmont/moon-core';
-  
-  <Badge color="goten.100" backgroundColor="piccolo.100">
-    Active
-  </Badge>
-  
-  <Badge color="piccolo.100" backgroundColor="trunks.100">
-    Active
-  </Badge>
-  
-  <Badge color="goten.100" backgroundColor="dodoria.100">
-    Active
-  </Badge>
+<Chip>Lion</Chip>
+<Chip>Tiger</Chip>
+<Chip>Jaguar</Chip>
   `}
-          />
-        </section>
+        />
+      </section>
+
+      {/* Sizes */}
+      <section className="mt-8">
+        <Preview
+          title="Sizes"
+          preview={
+            <div className="flex justify-around items-center w-full">
+              <Chip size="small">Small</Chip>
+              <Chip size="medium">Medium</Chip>
+              <Chip>Default</Chip>              
+            </div>
+          }
+          code={`import { Chip } from '@heathmont/moon-core';
   
-        {/* Sizes */}
-        <section className="mt-8">
-          <Preview
-            title="Sizes"
-            preview={
-              <div className="flex justify-around items-center w-full">
-                <Badge size="small">Small</Badge>
-                <Badge>Default</Badge>
-              </div>
-            }
-            code={`import { Badge } from '@heathmont/moon-core';
-  
-  <Badge size="small">Small</Badge>
-  
-  <Badge>Default</Badge>
+<Chip size="small">Small</Chip>
+<Chip size="medium">Medium</Chip>
+<Chip>Default</Chip>     
   `}
-          />
-        </section>
-      </>
-    );
-  }
+        />
+      </section>
+
+      {/* States */}
+      <section className="mt-8">
+        <Preview
+          title="States"
+          preview={
+            <div className="flex justify-around items-center w-full">
+              <Chip isActive={true}>Active</Chip>
+              <Chip isActive={false}>Inactive</Chip>
+                           
+            </div>
+          }
+          code={`import { Chip } from '@heathmont/moon-core';
   
+<Chip isActive={true}>Active</Chip>
+<Chip isActive={false}>Inactive</Chip>        
+  `}
+        />
+      </section>
+
+      {/* Icons */}
+      <section className="mt-8">
+        <Preview
+          title="Icons"
+          preview={
+            <div className="flex justify-around items-center w-full">
+              <Chip iconLeft={(<MediaMusic />)}>Left Icon</Chip>
+              <Chip iconRight={(<SoftwareDownload />)}>Right Icon</Chip>                 
+            </div>
+          }
+          code={`import { Chip } from '@heathmont/moon-core';
+
+<Chip iconLeft={(<MediaMusic />)}>Left Icon</Chip>
+<Chip iconRight={(<OtherSun />)}>Right Icon</Chip>             
+  `}
+        />
+      </section>
+    </>
+  );
+}
