04a72a8221aafc05092308058b7c084ab149a0ad
yarema184 <yuliana.yarema@heathmont.net>
2021-09-13 11:53:43 +0300

    feat: added RadioButton [B1M2-3880]

---
+++ packages/core/src/radioButton/RadioButtonLabel.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const RadioButtonLabel = styled.label({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  cursor: 'pointer',
+});
+
+export default RadioButtonLabel;
