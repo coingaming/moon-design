9eea5fde05632d765ebac91ad21f6d03c3ecbe76
Karl Kallavus <karl.kallavus@heathmont.net>
2021-09-14 12:54:51 +0300

    fix: deps

--- packages/global/src/index.ts
+++ packages/global/src/index.ts
@@ -4,13 +4,3 @@
 
 export { default as Global } from './global/Global';
 export * from './global/Global';
-export { default as config } from './global/styles/config';
-export * from './global/styles/config';
-export { default as page } from './global/styles/elements/page';
-export * from './global/styles/elements/page';
-export { default as boxSizing } from './global/styles/generic/boxSizing';
-export * from './global/styles/generic/boxSizing';
-export { default as focus } from './global/styles/generic/focus';
-export * from './global/styles/generic/focus';
-export { default as reset } from './global/styles/generic/reset';
-export * from './global/styles/generic/reset';
