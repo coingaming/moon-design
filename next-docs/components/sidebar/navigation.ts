7b5e2e0cf5be5843e1e3671c4f85bc6ed2e180dd
Karl Kallavus <karl.kallavus@heathmont.net>
2021-10-06 15:34:32 +0300

    fix: hide badge docs

--- next-docs/components/sidebar/navigation.ts
+++ next-docs/components/sidebar/navigation.ts
@@ -11,7 +11,7 @@ const navigation = [
     children: [
       { name: 'Accordion', href: '/components/accordion' },
       { name: 'Avatar', href: '/components/avatar' },
-      { name: 'Badge', href: '/components/badge' },
+      // { name: 'Badge', href: '/components/badge' },
       { name: 'Button', href: '/components/button' },
       { name: 'Carousel', href: '/components/carousel' },
       {
