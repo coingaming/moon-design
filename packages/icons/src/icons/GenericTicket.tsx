d1b3526827e8dbc437bcef48b45112fcf62c8298
Karl Kallavus <karl.kallavus@heathmont.net>
2021-10-08 13:52:17 +0300

    feat: update icons

--- packages/icons/src/icons/GenericTicket.tsx
+++ packages/icons/src/icons/GenericTicket.tsx
@@ -14,11 +14,11 @@ const Svg = (props: React.SVGProps<SVGSVGElement>) => (
   >
     <path
       d="M5 10c0-1.105.86-2 1.92-2h18.16c1.06 0 1.92.895 1.92 2v2c0 .552-.462.983-.979 1.178-1.108.417-1.901 1.523-1.901 2.822 0 1.299.793 2.405 1.901 2.822.517.195.979.626.979 1.178v2c0 1.105-.86 2-1.92 2H6.92C5.86 24 5 23.105 5 22v-2c0-.552.462-.983.979-1.178C7.088 18.405 7.88 17.3 7.88 16c0-1.299-.792-2.405-1.901-2.822C5.462 12.983 5 12.552 5 12v-2z"
-      stroke="#1A212A"
+      stroke="currentColor"
       strokeLinecap="round"
     />
     <path
-      stroke="#1A212A"
+      stroke="currentColor"
       strokeLinecap="round"
       strokeDasharray="2.6 3.2"
       d="M11.5 8.5v15"
