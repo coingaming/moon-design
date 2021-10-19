749a1b1063fd69d17473597d82bf38f954ea6824
Alexis Azanza <alexis@devtailor.com>
2021-10-19 04:22:47 +0300

    Add types file

---
+++ packages/draggabletable/src/types/definitions/react-table-config.d.ts
@@ -0,0 +1,7 @@
+import { UseResizeColumnsColumnProps } from 'react-table';
+
+declare module 'react-table' {
+  export interface ColumnInstance<
+    D extends Record<string, unknown> = Record<string, unknown>
+  > extends UseResizeColumnsColumnProps<D> {}
+}
