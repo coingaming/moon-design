0c425888dc728ffdf1f24f5dc0d412af7cfa3b9f
Vitaliy <vitalii.iuzhakov@heathmont.net>
2021-11-03 17:59:07 +0200

    feat(table.tsx): make useSticky as optional (#1461)
    * feat(table.tsx): make useSticky as optional
    
    * fix(table.tsx): sticky class is optional
    
    Co-authored-by: Karl Kallavus <karl.kallavus@heathmont.net>

--- packages/table/src/Table.tsx
+++ packages/table/src/Table.tsx
@@ -50,6 +50,7 @@ export type TableProps = {
   defaultRowBackgroundColor?: ColorNames;
   evenRowBackgroundColor?: ColorNames;
   headerBackgroundColor?: ColorNames;
+  isSticky?: boolean;
   isSorting?: boolean;
   renderRowSubComponent?: (props: RowSubComponentProps) => any;
   getOnRowClickHandler?: (row: any) => any;
@@ -71,6 +72,7 @@ const Table: React.FC<TableProps> = ({
   defaultRowBackgroundColor = 'gohan.100',
   evenRowBackgroundColor = 'gohan.80',
   headerBackgroundColor = 'goku.100',
+  isSticky = true,
   isSorting = false,
   renderRowSubComponent,
   getOnRowClickHandler = () => undefined,
@@ -79,7 +81,7 @@ const Table: React.FC<TableProps> = ({
     layout === 'block' ? useBlockLayout : useFlexLayout,
     variant === 'calendar' ? useRowSpan : undefined,
     useResizeColumns,
-    useSticky,
+    isSticky ? useSticky : undefined,
     useExpanded,
     isSorting ? useSortBy : undefined,
   ].filter((plugin) => !!plugin) as PluginHook<{}>[];
@@ -121,7 +123,7 @@ const Table: React.FC<TableProps> = ({
       {...getTableProps()}
       ref={tableRef}
       onScroll={handleScroll}
-      className="sticky"
+      className={isSticky ? "sticky" : undefined}
       isScrolledToLeft={scrollState.scrolledToLeft}
       isScrolledToRight={scrollState.scrolledToRight}
       style={{
