d14f72d654035e6d3800527b343354d76e135d1d
Karl Kallavus <karl.kallavus@heathmont.net>
2021-12-01 15:22:29 +0100

    feat: test initial commit (#1497)
    * feat: test initial commit
    
    * fix: update deps
    
    * feat: add test step to ci

---
+++ next-docs/public/examples/accordion/__tests__/index.test.tsx
@@ -0,0 +1,30 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import renderer, { act } from 'react-test-renderer';
+import Accordion from '../Accordion';
+import { moonDesignLight, ThemeProvider } from '@heathmont/moon-themes';
+
+const renderWithTheme = (component: JSX.Element) => (
+  <ThemeProvider theme={moonDesignLight}>{component}</ThemeProvider>
+);
+
+describe('Accordion', () => {
+  const testRenderer = renderer.create(renderWithTheme(<Accordion />));
+  const testInstance = testRenderer.root;
+
+  it('renders', () => {
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('expands', () => {
+    act(() => {
+      const button = testInstance.findByType('button');
+      button.props.onClick();
+    });
+
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+});
