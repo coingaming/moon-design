c6a7af5e9ebd4f23833ba53999706aeede6a0e10
Karl Kallavus <karl.kallavus@heathmont.net>
2021-12-23 14:21:03 +0100

    feat: radiobutton tests

---
+++ next-docs/public/examples/radiobutton/radiobutton/__tests__/index.test.tsx
@@ -0,0 +1,32 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { moonDesignLight, ThemeProvider } from '@heathmont/moon-themes';
+import renderer from 'react-test-renderer';
+
+import Default from '../Default';
+import Checked from '../Checked';
+import Disabled from '../Disabled';
+
+const renderWithTheme = (component: JSX.Element) => (
+  <ThemeProvider theme={moonDesignLight}>{component}</ThemeProvider>
+);
+
+describe('Radiobutton', () => {
+  it('renders', () => {
+    const testRenderer = renderer.create(renderWithTheme(<Default />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Checked', () => {
+    const testRenderer = renderer.create(renderWithTheme(<Checked />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Disabled', () => {
+    const testRenderer = renderer.create(renderWithTheme(<Disabled />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+});
