efa4600c6a19002667a82c4e8b7d347661c5490b
Karl Kallavus <karl.kallavus@heathmont.net>
2021-12-23 13:39:47 +0100

    feat: carousel tests

--- next-docs/public/examples/carousel/CustomizedArrow.tsx
+++ next-docs/public/examples/carousel/CustomizedArrow.tsx
@@ -3,19 +3,19 @@ import { Carousel } from '@heathmont/moon-components';
 import { rem } from '@heathmont/moon-utils';
 import styled from 'styled-components';
 
+const ExampleContent = styled.div(({ theme: { color, radius } }) => ({
+  width: '20rem',
+  height: '12rem',
+  borderRadius: rem(radius.small),
+  background: color.goku[80],
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontSize: '2rem',
+}));
+
 const Example = () => {
   const items = Array.from({ length: 25 }, (value, index) => index);
-
-  const ExampleContent = styled.div(({ theme: { color, radius } }) => ({
-    width: '20rem',
-    height: '12rem',
-    borderRadius: rem(radius.small),
-    background: color.goku[80],
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: '2rem',
-  }));
 
   return (
     <div className="flex justify-around items-center w-full">
