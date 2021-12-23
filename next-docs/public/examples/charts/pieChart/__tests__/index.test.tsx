c995082362a0089e9541ad2cac955016adcae1ab
Karl Kallavus <karl.kallavus@heathmont.net>
2021-12-23 13:20:58 +0100

    feat: charts tests

---
+++ next-docs/public/examples/charts/pieChart/__tests__/index.test.tsx
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { moonDesignLight, ThemeProvider } from '@heathmont/moon-themes';
+import renderer from 'react-test-renderer';
+
+import Default from '../Default';
+import Loading from '../Loading';
+
+
+const renderWithTheme = (component: JSX.Element) => (
+  <ThemeProvider theme={moonDesignLight}>{component}</ThemeProvider>
+);
+
+describe('Pie Chart', () => {
+  it('renders', () => {
+    const testRenderer = renderer.create(renderWithTheme(<Default />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Loading', () => {
+    const testRenderer = renderer.create(renderWithTheme(<Loading />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+});
---
+++ next-docs/public/examples/charts/verticalChart/__tests__/index.test.tsx
@@ -0,0 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { moonDesignLight, ThemeProvider } from '@heathmont/moon-themes';
+import renderer from 'react-test-renderer';
+
+import NoData from '../NoData';
+import Active from '../Active';
+
+const renderWithTheme = (component: JSX.Element) => (
+  <ThemeProvider theme={moonDesignLight}>{component}</ThemeProvider>
+);
+
+describe('Table Chart', () => {
+  it('renders NoData', () => {
+    const testRenderer = renderer.create(renderWithTheme(<NoData />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Active', () => {
+    const testRenderer = renderer.create(renderWithTheme(<Active />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+});
