2d4ec0d56e3dfd46d1254971d0806aa982eaf2d8
Karl Kallavus <karl.kallavus@heathmont.net>
2021-12-23 14:23:13 +0100

    feat: search tests

---
+++ next-docs/public/examples/search/__tests__/index.test.tsx
@@ -0,0 +1,28 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { moonDesignLight, ThemeProvider } from '@heathmont/moon-themes';
+import renderer from 'react-test-renderer';
+
+import PreviewSearch from '../PreviewSearch';
+import PreviewSearchLoading from '../PreviewSearchLoading';
+
+const renderWithTheme = (component: JSX.Element) => (
+  <ThemeProvider theme={moonDesignLight}>{component}</ThemeProvider>
+);
+
+describe('Search', () => {
+  it('renders', () => {
+    const testRenderer = renderer.create(renderWithTheme(<PreviewSearch />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Loading State', () => {
+    const testRenderer = renderer.create(
+      renderWithTheme(<PreviewSearchLoading />)
+    );
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+});
