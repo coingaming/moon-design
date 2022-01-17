67c541e36c7f13ee68b54d35a86117c003d26a4b
Alexis Azanza <alexis@devtailor.com>
2022-01-17 18:30:45 +0200

    Added more tests

--- next-docs/public/examples/badge/__tests__/index.test.tsx
+++ next-docs/public/examples/badge/__tests__/index.test.tsx
@@ -7,6 +7,9 @@ import { moonDesignLight, ThemeProvider } from '@heathmont/moon-themes';
 import renderer from 'react-test-renderer';
 
 import Example from '../Example';
+import Icons from '../Icons';
+import BackgroundColor from '../BackgroundColor';
+import Size from '../Size';
 
 const renderWithTheme = (component: JSX.Element) => (
   <ThemeProvider theme={moonDesignLight}>{component}</ThemeProvider>
@@ -15,6 +18,21 @@ const renderWithTheme = (component: JSX.Element) => (
 describe('Checkbox', () => {
   it('renders', () => {
     const testRenderer = renderer.create(renderWithTheme(<Example />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders with icons', () => {
+    const testRenderer = renderer.create(renderWithTheme(<Icons />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders with custom background color', () => {
+    const testRenderer = renderer.create(renderWithTheme(<BackgroundColor />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders with small size', () => {
+    const testRenderer = renderer.create(renderWithTheme(<Size />));
     expect(testRenderer.toJSON()).toMatchSnapshot();
   });
 });
