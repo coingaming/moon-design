5d518eeee8b42849fb1245056548e03450320202
Alexis Azanza <alexis@devtailor.com>
2022-01-19 16:28:30 +0200

    Use newTokens for checkbox (#1557)

--- packages/core/src/checkbox/styles/CheckboxLabel.ts
+++ packages/core/src/checkbox/styles/CheckboxLabel.ts
@@ -6,14 +6,12 @@ interface CheckboxLabelProps {
   variant?: Variants;
 }
 
-const checkboxSize = (theme: Theme) => theme.newTokens.size.twoxsmall;
-
 const CheckboxLabel = styled.label<CheckboxLabelProps>(
   ({ theme }) => ({
     display: 'inline-block',
     position: 'relative',
     alignItems: 'center',
-    minHeight: checkboxSize(theme) /* [1] */,
+    minHeight: theme.newTokens.size.twoxsmall /* [1] */,
   }),
   ({ variant, theme }) => {
     if (variant === 'option') {
