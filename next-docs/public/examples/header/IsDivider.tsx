907d31400e7f337b9cf2c8ba5e285465443442db
Alexis Azanza <alexis@devtailor.com>
2022-01-20 10:19:54 +0200

    Use newTokens for header

---
+++ next-docs/public/examples/header/IsDivider.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Header } from '@heathmont/moon-core';
+
+const Example = () => (
+  <div className="flex justify-around items-center w-full">
+    <Header isDivider={true}>Title</Header>
+  </div>
+);
+
+export default Example;
