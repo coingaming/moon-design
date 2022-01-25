afd31acbd3c987209fc689907ef5a5e18242f6f0
Alexis Azanza <alexis@devtailor.com>
2022-01-25 16:19:07 +0200

    Use newTokens for tooltip (#1575)

--- packages/core/src/tooltip/Tooltip.ts
+++ packages/core/src/tooltip/Tooltip.ts
@@ -1,47 +1,49 @@
 import { rem } from '@heathmont/moon-utils';
 import styled from 'styled-components';
 
-const Tooltip = styled.div<{ position?: string }>(({ theme }) => [
-  {
-    display: 'inline-block',
-    textAlign: 'right',
-    color: theme.colorNew.goten,
-    background: theme.colorNew.hit,
-    fontSize: rem(12),
-    lineHeight: rem(16),
-    padding: rem(12),
-    borderRadius: rem(4),
-    position: 'relative',
-    boxShadow: theme.shadows.sm,
-    // '&::after': {
-    //   content: '""',
-    //   borderColor: 'transparent',
-    //   borderTopColor: theme.colorNew.hit,
-    //   borderWidth: rem(8),
-    //   borderBottomWidth: 0,
-    //   borderStyle: 'solid',
-    //   width: 0,
-    //   height: 0,
-    //   position: 'absolute',
-    //   top: '100%',
-    //   left: rem(12),
-    //   marginTop: `-${rem(1)}`,
-    //   // boxShadow: theme.shadows.sm,
-    // },
-  },
-  // ({ position }) =>
-  //   position === 'bottom' && {
-  //     '&::after': {
-  //       top: 'initial',
-  //       marginTop: 0,
-  //       borderBottomWidth: rem(8),
-  //       bottom: '100%',
-  //       marginBottom: `-${rem(1)}`,
-  //       borderTopWidth: 0,
-  //       borderBottomColor: theme.colorNew.hit,
-  //       // boxShadow: theme.shadows.sm,
-  //     },
-  //   },
-]);
+const Tooltip = styled.div<{ position?: string }>(
+  ({ theme: { colorNew, newTokens } }) => [
+    {
+      display: 'inline-block',
+      textAlign: 'right',
+      color: colorNew.goten,
+      background: colorNew.hit,
+      fontSize: rem(12),
+      lineHeight: rem(16),
+      padding: rem(12),
+      borderRadius: rem(4),
+      position: 'relative',
+      boxShadow: newTokens.boxShadow.small,
+      // '&::after': {
+      //   content: '""',
+      //   borderColor: 'transparent',
+      //   borderTopColor: theme.colorNew.hit,
+      //   borderWidth: rem(8),
+      //   borderBottomWidth: 0,
+      //   borderStyle: 'solid',
+      //   width: 0,
+      //   height: 0,
+      //   position: 'absolute',
+      //   top: '100%',
+      //   left: rem(12),
+      //   marginTop: `-${rem(1)}`,
+      //   // boxShadow: newTokens.boxShadow.small,
+      // },
+    },
+    // ({ position }) =>
+    //   position === 'bottom' && {
+    //     '&::after': {
+    //       top: 'initial',
+    //       marginTop: 0,
+    //       borderBottomWidth: rem(8),
+    //       bottom: '100%',
+    //       marginBottom: `-${rem(1)}`,
+    //       borderTopWidth: 0,
+    //       borderBottomColor: theme.colorNew.hit,
+    //       // boxShadow: newTokens.boxShadow.small,
+    //     },
+    //   },
+  ]
+);
 
 export default Tooltip;
