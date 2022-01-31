5681f44f8d1704c121754a7e7cf1692d07eface8
Alexis Azanza <alexis@devtailor.com>
2022-01-31 08:01:40 +0200

    Makre radioButton rtl friendly

--- packages/core/src/radioButton/styles/RadioButtonCaption.ts
+++ packages/core/src/radioButton/styles/RadioButtonCaption.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const RadioButtonCaption = styled.span(({ theme }) => ({
   color: theme.colorNew.trunks,
-  marginLeft: theme.newTokens.space.xsmall,
+  marginInlineStart: theme.newTokens.space.xsmall,
 }));
 
 export default RadioButtonCaption;
