ba7e7c680b54da7f911dc07419764916ab81e57c
Alexis Azanza <alexis@devtailor.com>
2022-02-01 18:22:55 +0200

    Added rtl test

--- next-docs/public/examples/radiobutton/radiobutton/__tests__/index.test.tsx
+++ next-docs/public/examples/radiobutton/radiobutton/__tests__/index.test.tsx
@@ -14,9 +14,20 @@ const renderWithTheme = (component: JSX.Element) => (
   <ThemeProvider theme={moonDesignLight}>{component}</ThemeProvider>
 );
 
+const renderWithThemeRTL = (component: JSX.Element) => (
+  <ThemeProvider theme={moonDesignLight}>
+    <div dir="rtl">{component}</div>
+  </ThemeProvider>
+);
+
 describe('Radiobutton', () => {
   it('renders', () => {
     const testRenderer = renderer.create(renderWithTheme(<Default />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders RTL', () => {
+    const testRenderer = renderer.create(renderWithThemeRTL(<Default />));
     expect(testRenderer.toJSON()).toMatchSnapshot();
   });
 
