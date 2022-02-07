e0b0c2d118efd608711a4581e786d90a78ae61a2
Alexis Azanza <alexis@devtailor.com>
2022-02-07 13:18:28 +0200

    chore: use newTokens for search [B1M2-5050] (#1570)
    * Use newTokens for search
    
    * Used calc() for adding rem
    
    * Use calc
    
    * Removed remaning old tokens, converted old to new
    
    * Use rem on settings usage
    
    * Use transitionDuration from the old theme
    
    * Reverted datepicker snapshots
    
    * Fixed transition duration
    
    * fix: remove unesessary transitions [B1M2-5050]
    
    * Updated search snapshot files
    
    * Updated snapshots
    
    Co-authored-by: Karl Kallavus <karl.kallavus@heathmont.net>
    Co-authored-by: Dmytro Kireiev <dmytro.kireiev@gmail.com>

--- packages/core/src/search/private/components/Input.ts
+++ packages/core/src/search/private/components/Input.ts
@@ -15,15 +15,15 @@ const { borderWidth, padding } = inputConfig;
 export type SearchInputSize = 'small' | 'medium';
 
 export const SearchClearButton = styled(Button)(
-  ({ theme: { colorNew, space } }) => ({
+  ({ theme: { colorNew, newTokens } }) => ({
     position: 'absolute',
     top: 0,
-    right: borderWidth,
+    right: rem(borderWidth),
     height: '100%',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    padding: `0 ${rem(space.small)}`,
+    padding: `0 ${newTokens.space.xsmall}`,
     border: 'none',
     background: 'transparent',
     appearance: 'none',
@@ -33,27 +33,27 @@ export const SearchClearButton = styled(Button)(
     },
     '[dir=rtl] &': {
       right: 'auto',
-      left: borderWidth,
+      left: rem(borderWidth),
     },
   })
 );
 
 export const SearchClearIcon = styled(GenericClose)<
   React.SVGProps<SVGSVGElement>
->(({ theme: { base, colorNew } }) => ({
+>(({ theme: { colorNew } }) => ({
   color: colorNew.trunks,
-  fontSize: rem(base.lineHeight),
+  fontSize: rem(20),
 }));
 
 export const SearchInputIcon = styled(GenericSearch)<
   React.SVGProps<SVGSVGElement> & { $size: SearchInputSize }
->(({ $size, theme: { base, colorNew } }) => ({
+>(({ $size, theme: { colorNew } }) => ({
   position: 'absolute',
   top: '50%',
   left: rem(padding[$size] + borderWidth),
   transform: 'translateY(-50%)',
   zIndex: zIndex.searchIcon,
-  fontSize: rem(base.lineHeight),
+  fontSize: rem(20),
   color: colorNew.trunks,
   pointerEvents: 'none',
   '[dir=rtl] &': {
@@ -64,16 +64,16 @@ export const SearchInputIcon = styled(GenericSearch)<
 
 export const SearchMenuIcon = styled(GenericMenu)<
   React.SVGProps<SVGSVGElement>
->(({ theme: { base, colorNew, space } }) => ({
+>(({ theme: { colorNew, newTokens } }) => ({
   position: 'absolute',
   top: '50%',
-  right: rem(space.default),
+  right: newTokens.space.small,
   color: colorNew.trunks,
   transform: 'translateY(-50%)',
   zIndex: zIndex.searchIcon,
-  fontSize: rem(base.lineHeight),
+  fontSize: rem(20),
   '[dir=rtl] &': {
-    left: rem(space.default),
+    left: newTokens.space.small,
     right: 'auto',
   },
 }));
@@ -83,69 +83,54 @@ export const SearchInput = styled.input<
     hasBorder: boolean;
     $size: SearchInputSize;
   }
->(
-  ({
-    $size,
-    hasBorder,
-    theme: {
-      breakpoint,
-      colorNew,
-      fontSize,
-      fontWeight,
-      radius,
-      space,
-      transitionDuration,
-    },
-  }) => ({
-    fontWeight: fontWeight.normal,
-    fontSize: rem(fontSize.body),
-    width: '100%',
-    lineHeight: 1,
-    position: 'relative',
-    paddingTop: 0,
-    paddingRight: rem(padding[$size] + space.default),
-    paddingBottom: 0,
-    paddingLeft: rem(2 * padding[$size] + space.default),
-    textOverflow: 'ellipsis',
-    overflow: 'hidden',
-    backgroundColor: colorNew.goku,
-    color: colorNew.trunks,
-    borderRadius: rem(radius.default),
-    border: hasBorder ? `${rem(borderWidth)} solid transparent` : 'none',
-    outline: 0,
-    zIndex: zIndex.searchInput,
-    transition: `${inputConfig.transition}`,
-    transitionDuration: `${transitionDuration.default}s`,
-    '[dir=rtl] &': {
-      paddingLeft: rem(padding[$size] + space.default),
-      paddingRight: rem(2 * padding[$size] + space.default),
-    },
-    '&::placeholder': {
-      color: 'inherit',
-    },
-    '&:hover': {
-      backgroundColor: colorNew.gohan,
-      borderColor: colorNew.beerus,
-    },
-    '&:active, &:focus': {
-      backgroundColor: colorNew.gohan,
+>(({ $size, hasBorder, theme: { breakpoint, colorNew, newTokens } }) => ({
+  fontWeight: newTokens.font.weight.normal,
+  fontSize: rem(14),
+  width: '100%',
+  lineHeight: 1,
+  position: 'relative',
+  paddingTop: 0,
+  paddingRight: `calc(${rem(padding[$size])} + ${newTokens.space.small})`,
+  paddingBottom: 0,
+  paddingLeft: `calc(${rem(2 * padding[$size])} + ${newTokens.space.small})`,
+  textOverflow: 'ellipsis',
+  overflow: 'hidden',
+  backgroundColor: colorNew.goku,
+  color: colorNew.trunks,
+  borderRadius: newTokens.borderRadius.medium,
+  border: hasBorder ? `${rem(borderWidth)} solid transparent` : 'none',
+  outline: 0,
+  zIndex: zIndex.searchInput,
+  transition: `color ${newTokens.transition.default}, background-color ${newTokens.transition.default}`,
+  '[dir=rtl] &': {
+    paddingLeft: `calc(${rem(padding[$size])} + ${newTokens.space.small})`,
+    paddingRight: `calc(${rem(2 * padding[$size])} + ${newTokens.space.small})`,
+  },
+  '&::placeholder': {
+    color: 'inherit',
+  },
+  '&:hover': {
+    backgroundColor: colorNew.gohan,
+    borderColor: colorNew.beerus,
+  },
+  '&:active, &:focus': {
+    backgroundColor: colorNew.gohan,
+    borderColor: colorNew.piccolo,
+    color: colorNew.bulma,
+  },
+  [mq(breakpoint.medium, 'max-width')]: {
+    [':focus:not(:placeholder-shown)']: {
       borderColor: colorNew.piccolo,
-      color: colorNew.bulma,
     },
-    [mq(breakpoint.medium, 'max-width')]: {
-      [':focus:not(:placeholder-shown)']: {
-        borderColor: colorNew.piccolo,
-      },
-    },
-  })
-);
+  },
+}));
 
 export const SearchBox = styled.span<{
   $size: SearchInputSize;
-}>(({ $size, theme: { fontSize } }) => ({
+}>(({ $size }) => ({
   display: 'flex',
   position: 'relative',
-  fontSize: rem(fontSize.body),
+  fontSize: rem(14),
   overflow: 'visible',
   width: '100%',
   height: rem(inputConfig.height[$size]),
