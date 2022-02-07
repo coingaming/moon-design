26d26c4a77779352cb068fbfbf8fb5e58462174a
Alexis Azanza <alexis@devtailor.com>
2022-02-07 06:54:28 +0200

    Start with reel

---
+++ next-docs/pages/components/reel.tsx
@@ -0,0 +1,62 @@
+import Default from '../../public/examples/switch/Default';
+import Sizes from '../../public/examples/switch/Sizes';
+import Theme from '../../public/examples/switch/Theme';
+import ThemeCaption from '../../public/examples/switch/ThemeCaption';
+import useExamples from '../../utils/useExamples';
+
+export default function PageReel() {
+  const examples = useExamples('switch');
+
+  return (
+    <>
+      <section className="mt-8">
+        <h1 className="text-5xl font-semibold">Switch</h1>
+        <p className="text-lg mt-4">
+          An alternate checkbox appearance for simulating on/off state.
+        </p>
+      </section>
+
+      {/* Default */}
+      <section className="mt-8">
+        <Preview
+          title="Default"
+          preview={<Default />}
+          code={examples ? examples.Default : 'Loading'}
+        />
+      </section>
+
+      {/* Sizes */}
+      <section className="mt-8">
+        <Preview
+          title="Sizes"
+          preview={<Sizes />}
+          code={examples ? examples.Sizes : 'Loading'}
+        />
+      </section>
+
+      <section className="mt-8">
+        <Preview
+          title="Dark &amp; Light mode"
+          preview={<Theme />}
+          code={examples ? examples.Theme : 'Loading'}
+        />
+      </section>
+
+      <section className="mt-8">
+        <Preview
+          title="With captions"
+          preview={<Caption />}
+          code={examples ? examples.Caption : 'Loading'}
+        />
+      </section>
+
+      <section className="mt-8">
+        <Preview
+          title="Dark &amp; Light mode with captions"
+          preview={<ThemeCaption />}
+          code={examples ? examples.ThemeCaption : 'Loading'}
+        />
+      </section>
+    </>
+  );
+}
