015ce049473963023de9bb23e95fc835c68d395d
Karl Kallavus <karl.kallavus@heathmont.net>
2022-02-18 17:18:36 +0530

    build: .env.example

--- next-docs/consts/consts.ts
+++ next-docs/consts/consts.ts
@@ -1,4 +1,4 @@
 export const ELIXIR_ENABLED =
-  process.env.NEXT_PUBLIC_ELIXIR_ENABLED === '1' || false;
+  process.env.NEXT_PUBLIC_ELIXIR_ENABLED === 'true' || false;
 export const ELIXIR_BASE_URL =
   process.env.NEXT_PUBLIC_ELIXIR_BASE_URL || 'http://localhost:4000/';
