179df6a437168a9e4c7698ee752c17418a7f4aba
Alexis Azanza <alexis@devtailor.com>
2022-02-22 00:21:53 +0200

    Added tests

---
+++ next-docs/public/examples/accordionNew/__tests__/index.test.tsx
@@ -0,0 +1,62 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { moonDesignLight, ThemeProvider } from '@heathmont/moon-themes';
+import renderer from 'react-test-renderer';
+import Default from '../Default';
+import ContentOutside from '../ContentOutside';
+import HeaderContent from '../HeaderContent';
+import Sizes from '../Sizes';
+import SizesContentOutside from '../SizesContentOutside';
+import WithoutButton from '../WithoutButton';
+
+const renderWithTheme = (component: JSX.Element) => (
+  <ThemeProvider theme={moonDesignLight}>{component}</ThemeProvider>
+);
+
+const renderWithThemeRTL = (component: JSX.Element) => (
+  <ThemeProvider theme={moonDesignLight}>
+    <div dir="rtl">{component}</div>
+  </ThemeProvider>
+);
+
+describe('Accordion New', () => {
+  it('renders', () => {
+    const testRenderer = renderer.create(renderWithTheme(<Default />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders RTL', () => {
+    const testRenderer = renderer.create(renderWithThemeRTL(<Default />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders content outside', () => {
+    const testRenderer = renderer.create(renderWithTheme(<ContentOutside />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders header content', () => {
+    const testRenderer = renderer.create(renderWithTheme(<HeaderContent />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders sizes variants', () => {
+    const testRenderer = renderer.create(renderWithTheme(<Sizes />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders sizes variants with content outside', () => {
+    const testRenderer = renderer.create(
+      renderWithTheme(<SizesContentOutside />)
+    );
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders without button', () => {
+    const testRenderer = renderer.create(renderWithTheme(<WithoutButton />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+});
