bdf22c98c2f30af98f5ca4a920e6c93a21baa299
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2022-02-23 16:37:37 +0200

    fix: remove old buttons from docs [B1M2-5375] (#1641)
    * fix: remove old button from docs [B1M2-5375]
    
    * fix: update moon colors [B1M2-5375]
    
    * fix: tests [B1M2-5375]

--- next-docs/public/examples/button/Secondary.tsx
+++
@@ -1,5 +0,0 @@
-import { Button } from '@heathmont/moon-components';
-
-const Example = () => <Button variant="secondary">Secondary</Button>;
-
-export default Example;
--- next-docs/public/examples/button/Sizes.tsx
+++ next-docs/public/examples/button/Sizes.tsx
@@ -1,19 +1,11 @@
-import { Button } from '@heathmont/moon-components';
+import { Button } from '@heathmont/moon-core';
 
 const Example = () => (
   <div className="flex justify-around w-full items-center">
-    <Button variant="primary" size="xsmall">
-      Xsmall
-    </Button>
-    <Button variant="primary" size="small">
-      Small
-    </Button>
-    <Button variant="primary" size="medium">
-      Medium
-    </Button>
-    <Button variant="primary" size="large">
-      Large
-    </Button>
+    <Button size="small">Small</Button>
+    <Button>Medium is default</Button>
+    <Button size="large">Large</Button>
+    <Button size="xlarge">xLarge</Button>
   </div>
 );
 
