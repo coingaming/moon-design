b00ca0db83683f70bbe0e48d5a535090ea01a6a7
Alexis Azanza <alexis@devtailor.com>
2022-03-03 17:43:04 +0200

    Added initial files

---
+++ packages/core/src/banner/Banner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export interface BannerProps {
+  title: string;
+  description: string;
+}
+
+const Banner: React.FC<BannerProps> = ({ title, description }) => <></>;
+
+export default Banner;
