913ccaf351f95b49bde5a97558873db51af8610d
Alexis Azanza <alexis@devtailor.com>
2022-03-07 14:25:14 +0200

    Finished tests

--- next-docs/public/examples/banner/__tests__/index.test.tsx
+++ next-docs/public/examples/banner/__tests__/index.test.tsx
@@ -5,15 +5,45 @@
 import React from 'react';
 import { moonDesignLight, ThemeProvider } from '@heathmont/moon-themes';
 import renderer from 'react-test-renderer';
+import Buttons from '../Buttons';
 import Default from '../Default';
+import HeaderIcon from '../HeaderIcon';
+import Mobile from '../Mobile';
+import Singleline from '../Singleline';
 
 const renderWithTheme = (component: JSX.Element) => (
   <ThemeProvider theme={moonDesignLight}>{component}</ThemeProvider>
 );
 
+const renderWithThemeRTL = (component: JSX.Element) => (
+  <ThemeProvider theme={moonDesignLight}>
+    <div dir="rtl">{component}</div>
+  </ThemeProvider>
+);
+
 describe('Banner', () => {
   it('renders', () => {
     const testRenderer = renderer.create(renderWithTheme(<Default />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders with buttons', () => {
+    const testRenderer = renderer.create(renderWithTheme(<Buttons />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders with header icon', () => {
+    const testRenderer = renderer.create(renderWithTheme(<HeaderIcon />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders in mobile mode', () => {
+    const testRenderer = renderer.create(renderWithTheme(<Mobile />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders in single line mode', () => {
+    const testRenderer = renderer.create(renderWithTheme(<Singleline />));
     expect(testRenderer.toJSON()).toMatchSnapshot();
   });
 });
