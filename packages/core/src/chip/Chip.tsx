37390e3ed9b7edc7d0580d7b1b011fa577f7bf70
Alexis Azanza <alexis@devtailor.com>
2022-03-08 18:19:17 +0200

    Fix padding

--- packages/core/src/chip/Chip.tsx
+++ packages/core/src/chip/Chip.tsx
@@ -1,23 +1,34 @@
 import React from 'react';
+import { SharedTheme } from '@heathmont/moon-themes';
 import { rem } from '@heathmont/moon-utils';
 import { rgba } from 'polished';
 import styled from 'styled-components';
+
+const determinePadding = (newTokens: SharedTheme['newTokens'], size?: Size) => {
+  if (size === 'small') {
+    return `${newTokens.space.threexsmall} ${newTokens.space.twoxsmall}`;
+  }
+
+  return `${newTokens.space.twoxsmall} ${newTokens.space.xsmall}`;
+};
+
+type Size = 'small' | 'medium';
 
 type ChipProps = {
   isActive?: boolean;
   iconLeft?: React.ReactElement;
   iconRight?: React.ReactElement;
-  size?: 'small' | 'medium';
+  size?: Size;
 };
 
-const StyledChip = styled.button<ChipProps>(({ isActive, size, theme }) => [
+const StyledChip = styled.button<ChipProps>(({ size, theme }) => [
   {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: `${rem(theme.radius.default)}`,
-    padding: `${rem(8)}`,
+    padding: determinePadding(theme.newTokens, size),
     transition: `${theme.transitionDuration.default}s`,
     '&:hover': {
       background: rgba(theme.color.piccolo[100], 0.2),
