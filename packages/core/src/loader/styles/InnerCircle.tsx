ffa55ba007f1355b48826d7c41cc6e5f6b6ff6f2
Alexis Azanza <alexis@devtailor.com>
2022-03-15 17:39:37 +0200

    Trying to fix width of the loader by size prop

--- packages/core/src/loader/styles/InnerCircle.tsx
+++ packages/core/src/loader/styles/InnerCircle.tsx
@@ -18,33 +18,37 @@ export interface Props {
 
 const InnerCircle = styled.span<Props>(({ color, theme }) => ({
   width: '100%',
-  height: '50%',
+  height: '50%', // 50%
   position: 'absolute',
-  marginTop: '50%',
-  background: `linear-gradient(90deg, ${determineColor(
-    theme,
-    color,
-    true
-  )}, ${determineColor(theme, color, false)})`,
+  marginTop: '40%', // 50%
+  // background: `linear-gradient(90deg, ${determineColor(
+  //   theme,
+  //   color,
+  //   true
+  // )}, ${determineColor(theme, color, false)})`,
+  background: `linear-gradient(90deg, red, pink)`,
+  // Dark part
   '&::before': {
     content: '""',
-    width: '100%',
-    height: '100%',
+    width: '100%', // 100%
+    height: '100%', // 100%
     position: 'absolute',
-    marginTop: '-50%',
-    background: `linear-gradient(90deg, ${determineColor(
-      theme,
-      color,
-      false
-    )}, ${determineColor(theme, color, false)})`,
+    marginTop: '-55%', // -50%
+    // background: `linear-gradient(90deg, ${determineColor(
+    //   theme,
+    //   color,
+    //   false
+    // )}, ${determineColor(theme, color, false)})`,
+    background: `linear-gradient(90deg, green, blue)`,
   },
+  // Light part
   '&::after': {
     content: '""',
-    width: '80%',
-    height: '160%',
+    width: '80%', // 80%
+    height: '160%', // 160%
     position: 'absolute',
-    marginTop: '-40%',
-    marginInlineStart: '10%',
+    marginTop: '-30%', // -40%,
+    marginInlineStart: '10%', // 10%
     background: 'white',
     borderRadius: '50%',
   },
