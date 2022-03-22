88bb00036d85fddb19c7985c4e7a22c294589017
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2022-03-22 14:28:38 +0200

    fix: Chip component [B1M2-5407]

--- next-docs/public/examples/chip/Sizes.tsx
+++ next-docs/public/examples/chip/Sizes.tsx
@@ -3,7 +3,7 @@ import { Chip } from '@heathmont/moon-core';
 const Example = () => (
   <div className="flex justify-around items-center w-full">
     <Chip size="small">Small</Chip>
-    <Chip size="medium">Medium is Default</Chip>
+    <Chip>Medium is Default</Chip>
   </div>
 );
 
--- next-docs/public/examples/chip/WithStroke.tsx
+++ next-docs/public/examples/chip/WithStroke.tsx
@@ -5,56 +5,44 @@ import { rem } from '@heathmont/moon-utils';
 const Example = () => (
   <div className="flex flex-col w-full gap-10">
     <div className="flex justify-around items-center w-full">
-      <Chip iconLeft={<MediaMusic fontSize={rem(24)} />} isStroke size="small">
+      <Chip iconLeft={<MediaMusic />} isStroke size="small">
         Left Icon
       </Chip>
-      <Chip
-        iconRight={<SoftwareDownload fontSize={rem(24)} />}
-        isStroke
-        size="small"
-      >
+      <Chip iconRight={<SoftwareDownload />} isStroke size="small">
         Right Icon
       </Chip>
       <Chip
-        iconRight={<SoftwareDownload fontSize={rem(24)} />}
-        iconLeft={<MediaMusic fontSize={rem(24)} />}
+        iconRight={<SoftwareDownload />}
+        iconLeft={<MediaMusic />}
         isStroke
         size="small"
       >
         Left/Right Icons
       </Chip>
       <Chip
-        iconRight={<SoftwareDownload fontSize={rem(24)} />}
-        iconLeft={<MediaMusic fontSize={rem(24)} />}
+        iconRight={<SoftwareDownload />}
+        iconLeft={<MediaMusic />}
         isStroke
         size="small"
       />
-      <Chip
-        iconOnly={<SoftwareDownload fontSize={rem(24)} />}
-        isStroke
-        size="small"
-      />
+      <Chip iconOnly={<SoftwareDownload />} isStroke size="small" />
     </div>
     <div className="flex justify-around items-center w-full">
-      <Chip iconLeft={<MediaMusic fontSize={rem(24)} />} isStroke>
+      <Chip iconLeft={<MediaMusic />} isStroke>
         Left Icon
       </Chip>
-      <Chip iconRight={<SoftwareDownload fontSize={rem(24)} />} isStroke>
+      <Chip iconRight={<SoftwareDownload />} isStroke>
         Right Icon
       </Chip>
-      <Chip
-        iconRight={<SoftwareDownload fontSize={rem(24)} />}
-        iconLeft={<MediaMusic fontSize={rem(24)} />}
-        isStroke
-      >
+      <Chip iconRight={<SoftwareDownload />} iconLeft={<MediaMusic />} isStroke>
         Left/Right Icons
       </Chip>
       <Chip
-        iconRight={<SoftwareDownload fontSize={rem(24)} />}
-        iconLeft={<MediaMusic fontSize={rem(24)} />}
+        iconRight={<SoftwareDownload />}
+        iconLeft={<MediaMusic />}
         isStroke
       />
-      <Chip iconOnly={<SoftwareDownload fontSize={rem(24)} />} isStroke />
+      <Chip iconOnly={<SoftwareDownload />} isStroke />
     </div>
   </div>
 );
