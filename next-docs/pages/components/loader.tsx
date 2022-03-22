3e16dc6ac2fecd5f2fd5a842ab5e43ff71d55907
Alexis Azanza <alexis@devtailor.com>
2022-03-22 17:28:00 +0200

    Added props table

--- next-docs/pages/components/loader.tsx
+++ next-docs/pages/components/loader.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Preview from '../../components/codePreview/Preview';
+import Table from '../../components/Table';
 import Customization from '../../public/examples/loader/Customization';
 import Default from '../../public/examples/loader/Default';
 import Sizes from '../../public/examples/loader/Sizes';
@@ -36,6 +37,28 @@ export default function PageLoader() {
           title="Sizes"
           preview={<Sizes />}
           code={examples ? examples.Sizes : 'Loading'}
+        />
+      </section>
+
+      <section className="mt-14">
+        <h2 className="text-2xl mb-4">Props</h2>
+        <Table
+          data={[
+            {
+              name: 'color',
+              type: 'ColorProps',
+              required: false,
+              default: 'bulma',
+              description: 'Color of the component',
+            },
+            {
+              name: 'size',
+              type: 'xsmall | small | medium | large | xlarge',
+              required: false,
+              default: 'xsmall',
+              description: 'Size of the component',
+            },
+          ]}
         />
       </section>
     </>
