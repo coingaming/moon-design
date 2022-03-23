ef5bd6f316976355adc322339edbd5fef4aaf056
oykos-development-hub <hub@oykos.me>
2022-03-23 20:25:56 +0100

    refactor: add reverse checkbox direction example

---
+++ next-docs/public/examples/checkbox/RightOrientation.tsx
@@ -0,0 +1,10 @@
+import { Checkbox } from '@heathmont/moon-core';
+
+const ReverseDirection = () => (
+  <Checkbox
+    label="I agree to receive bonus & marketing emails."
+    dir='rtl'
+  />
+);
+
+export default ReverseDirection;
