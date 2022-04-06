5d7855852abe159b6aae8c5db4a9ae472c0a0eda
oykos-development-hub <hub@oykos.me>
2022-04-06 09:45:05 +0200

    build: changes after running build

--- packages/assets/src/logos/LogoBetaddaShort.tsx
+++ packages/assets/src/logos/LogoBetaddaShort.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import styled from 'styled-components';
 import { ColorProps } from '@heathmont/moon-themes';
 import { themed } from '@heathmont/moon-utils';
-import styled from 'styled-components';
 
-const Svg = (props: React.SVGProps<SVGSVGElement>) => <svg width="1em" height="1em" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><title>{'Betadda.com'}</title><circle cx={14.5} cy={14.5} r={13.5} fill="#FF9933" /><circle cx={7.5} cy={7.5} r={7.5} fill="#1CA30F" /></svg>;
+const Svg = (props: React.SVGProps<SVGSVGElement>) => <svg width="1em" height="1em" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><title>{"Betadda.com"}</title><circle cx={14.5} cy={14.5} r={13.5} fill="#FF9933" /><circle cx={7.5} cy={7.5} r={7.5} fill="#1CA30F" /></svg>;
 
 type SvgProps = {
   color?: ColorProps;
--- packages/assets/src/logos/LogoMissions.tsx
+++ packages/assets/src/logos/LogoMissions.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import styled from 'styled-components';
 import { ColorProps } from '@heathmont/moon-themes';
 import { themed } from '@heathmont/moon-utils';
-import styled from 'styled-components';
 
 const Svg = (props: React.SVGProps<SVGSVGElement>) => <svg width="1em" height="1em" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><ellipse cx={27.1917} cy={21.3057} rx={10.3709} ry={10.3709} transform="rotate(-15 27.1917 21.3057)" fill="#FF385C" /><ellipse cx={13.7017} cy={26.8477} rx={10.3709} ry={10.3709} transform="rotate(-15 13.7017 26.8477)" fill="#6A36CA" /><circle cx={15.8012} cy={13.7017} r={10.3709} transform="rotate(-15 15.8012 13.7017)" fill="#FFB319" /></svg>;
 
