ceae973745dd38edde351a273c22f7e4cded7168
oykos-development-hub <hub@oykos.me>
2022-04-06 09:35:36 +0200

    fix: lint fix

--- next-docs/public/examples/authCode/__tests__/index.test.tsx
+++ next-docs/public/examples/authCode/__tests__/index.test.tsx
@@ -6,6 +6,9 @@ import React from 'react';
 import { moonDesignLight, ThemeProvider } from '@heathmont/moon-themes';
 import renderer from 'react-test-renderer';
 import Default from '../Default';
+import FourChars from "../FourChars";
+import OnlyDigits from "../OnlyDigits";
+import ErrorState from "../ErrorState";
 
 const renderWithTheme = (component: JSX.Element) => (
   <ThemeProvider theme={moonDesignLight}>{component}</ThemeProvider>
@@ -14,6 +17,21 @@ const renderWithTheme = (component: JSX.Element) => (
 describe('AuthCode', () => {
   it('renders', () => {
     const testRenderer = renderer.create(renderWithTheme(<Default />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Custom length', () => {
+    const testRenderer = renderer.create(renderWithTheme(<FourChars />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Only digits', () => {
+    const testRenderer = renderer.create(renderWithTheme(<OnlyDigits />));
+    expect(testRenderer.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders Error state', () => {
+    const testRenderer = renderer.create(renderWithTheme(<ErrorState />));
     expect(testRenderer.toJSON()).toMatchSnapshot();
   });
 });
