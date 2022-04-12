2c2ef855273218d9ff4f315367de1df2a42269ea
filip-andjelic <filip.andjelic.private@gmail.com>
2022-04-12 22:30:32 +0200

    feat: align Switch component with design. add disabled state

--- next-docs/pages/components/switch.tsx
+++ next-docs/pages/components/switch.tsx
@@ -1,5 +1,6 @@
 import Preview from '../../components/codePreview/Preview';
 import Default from '../../public/examples/switch/Default';
+import Disabled from "../../public/examples/switch/Disabled";
 import Rtl from '../../public/examples/switch/Rtl';
 import Sizes from '../../public/examples/switch/Sizes';
 import Theme from '../../public/examples/switch/Theme';
@@ -44,6 +45,14 @@ export default function PageSwitch({ isRtl }: Props) {
         <Preview
           title="Dark &amp; Light mode"
           preview={<Theme />}
+          code={examples ? examples.Theme : 'Loading'}
+        />
+      </section>
+
+      <section className="mt-8">
+        <Preview
+          title="Disabled"
+          preview={<Disabled />}
           code={examples ? examples.Theme : 'Loading'}
         />
       </section>
