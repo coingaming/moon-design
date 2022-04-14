cecee57ea0b7ad3819bc784937d2fac0201a212b
yarema184 <yuliana.yarema@heathmont.net>
2022-04-14 16:05:01 +0300

    fix: comment styles

--- next-docs/pages/core/loader.tsx
+++ next-docs/pages/core/loader.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Loader } from '@moon/core';
-import styles from '@moon/core/lib/loader/styles.module.css';
+// import styles from '@moon/core/lib/loader/styles.module.css';
 
 const Example = () => {
-
   return (
     <div className="theme-moon-dark">
       <section className="mt-8">Loader</section>
       {/* Variants */}
-      <section className={`${styles.moon__loader} mt-8` }>
+      <section className={` mt-8`}>
         <div className="flex justify-around w-full items-center">
           <Loader />
         </div>
