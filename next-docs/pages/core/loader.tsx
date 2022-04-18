b5b75c783aebd7ed6ef55ede100e4f83c8271da0
yarema184 <yuliana.yarema@heathmont.net>
2022-04-18 15:12:32 +0300

    feat: add loader tw

--- next-docs/pages/core/loader.tsx
+++ next-docs/pages/core/loader.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Loader } from '@moon/core';
-// import styles from '@moon/core/lib/loader/styles.module.css';
 
 const Example = () => {
   return (
     <div className="theme-moon-dark">
-      <section className="mt-8">Loader</section>
+      <section className="mt-8 text-3xl font-bold">Loader</section>
       {/* Variants */}
-      <section className={` mt-8`}>
-        <div className="flex justify-around w-full items-center">
+      <section className={`mt-8`}>
+        <div className="text-2xl font-bold pb-4">Default</div>
+        <div className="flex justify-around w-full items-center bg-gray-50 py-6 rounded-lg">
           <Loader />
+        </div>
+        <div className="text-2xl font-bold pb-4">Sizes</div>
+        <div className="flex justify-around w-full items-center bg-gray-50 py-6 rounded-lg mb-6">
+          <Loader size="twoxsmall" />
+          <Loader size="xsmall" />
+          <Loader size="small" />
+          <Loader />
+          <Loader size="large" />
+        </div>
+        <div className="text-2xl font-bold pb-4">Colours</div>
+        <div className="flex justify-around w-full items-center bg-gray-50 py-6 rounded-lg">
+          <Loader color="border-dodoria" />
+          <Loader color="border-trunks" />
+          <Loader color="border-raditz" />
         </div>
       </section>
     </div>
--- workspaces/core/src/loader/Loader.tsx
+++ workspaces/core/src/loader/Loader.tsx
@@ -1,14 +1,56 @@
 import React from 'react';
-// import styles from './styles.module.css';
+import classNames from '../private/utils/classnames';
+
+export type Size = 'twoxsmall' | 'xsmall' | 'small' | 'medium' | 'large';
 
 export type LoaderProps = {
-  // TODO Type color
   color?: string;
-  // TODO use Size enum
   size?: string;
 };
 
-const Loader: React.FC<LoaderProps> = ({ color, size = 'medium' }) => (
-  <div className={`moon__loader h-12 w-12 bg-piccolo block`} />
+const loaderSize = (size?: string) => {
+  switch (size) {
+    case 'twoxsmall':
+      return 'w-4 h-4';
+    case 'xsmall':
+      return 'w-6 h-6';
+    case 'small':
+      return 'w-8 h-8';
+    case 'medium':
+      return 'w-10 h-10';
+    case 'large':
+      return 'w-12 h-12';
+    default:
+      return;
+  }
+};
+
+const loaderDivBorder = (size?: string) => {
+  switch (size) {
+    case 'twoxsmall':
+    case 'xsmall':
+    case 'small':
+      return 'border-2 border-solid';
+    case 'medium':
+    case 'large':
+      return 'border-4 border-solid';
+    default:
+      return;
+  }
+};
+
+const Loader: React.FC<LoaderProps> = ({
+  color = 'border-hit',
+  size = 'medium',
+}) => (
+  <div className={classNames(loaderSize(size), 'relative rounded-full')}>
+    <div
+      className={classNames(
+        loaderDivBorder(size),
+        color,
+        'spinner-border animate-spin inline-block w-full h-full rounded-full border-r-transparent'
+      )}
+    />
+  </div>
 );
 export default Loader;
