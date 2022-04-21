ce270866d10af76be0546341e3e4c38ea09df988
filip-andjelic <filip.andjelic.private@gmail.com>
2022-04-21 15:28:52 +0200

    refactor: replace icons from moon-assets with icons from moon-icons in next-docs

--- next-docs/public/examples/badge/Icons.tsx
+++ next-docs/public/examples/badge/Icons.tsx
@@ -1,12 +1,13 @@
 import { Badge } from '@heathmont/moon-components';
-import { IconProfile } from '@heathmont/moon-assets';
+import { GenericUser } from '@heathmont/moon-icons';
+import { rem } from "@heathmont/moon-utils";
 
 const Example = () => (
   <div className="flex justify-around w-full">
-    <Badge variant="default" iconLeft={<IconProfile color="#fff" />}>
+    <Badge variant="default" iconLeft={<GenericUser fontSize={rem(23)}/>}>
       Default
     </Badge>
-    <Badge variant="default" iconRight={<IconProfile color="#fff" />}>
+    <Badge variant="default" iconRight={<GenericUser fontSize={rem(23)}/>}>
       Default
     </Badge>
   </div>
