42fa4689c47cc929a7af939a559e85b8192b9bb2
oykos-development-hub <contact@oykos.me>
2022-04-28 10:26:26 +0200

    MDS-56 switch component (#1705)
    * feat: implement clean Switch branch changes
    
    * build: update tests and build changes
    
    Co-authored-by: filip-andjelic <filip.andjelic.private@gmail.com>
    Co-authored-by: Karl Kallavus <karl.kallavus@heathmont.net>

---
+++ packages/core/src/switch/styles/Input.ts
@@ -0,0 +1,40 @@
+import React from "react";
+import {rem} from "@heathmont/moon-utils";
+import hideVisually from "polished/lib/mixins/hideVisually";
+import styled from "styled-components";
+import {Size} from "../private/Size";
+
+const Input = styled.input<{
+  switchWidth: string;
+  colorScheme?: boolean;
+  isRtl?: boolean;
+  checked?: boolean;
+  disabled?: boolean;
+  size?: Size;
+  button?: boolean;
+}>(
+  ({ colorScheme, isRtl, theme: { color, space }, size, button, switchWidth }) => ({
+    ...hideVisually(),
+    '&:checked + span': {
+      backgroundColor: colorScheme ? undefined : color.piccolo[100],
+      '&::before': {
+        backgroundColor: colorScheme ? undefined : color.goten[100],
+        left: rem(space.xsmall),
+        right: 'initial',
+        transform: isRtl
+          ? `translateX(calc(${switchWidth} / ${
+            size === '2xs' ? 2.4 : size || button ? 2.15 : 2
+          }) * -1)`
+          : `translateX(calc(${switchWidth} / ${
+            size === '2xs' ? 2.4 : size || button ? 2.15 : 2
+          }))`,
+      },
+    },
+  })
+) as React.FC<{}>;
+
+Input.defaultProps = {
+  type: 'checkbox',
+};
+
+export default Input;
