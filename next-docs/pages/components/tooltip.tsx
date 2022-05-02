5d3f6d153635388e6a9e569202ae452e4f3aa7db
filip-andjelic <filip.andjelic.private@gmail.com>
2022-05-02 12:36:34 +0200

    refactor: update props table for tooltip component

--- next-docs/pages/components/tooltip.tsx
+++ next-docs/pages/components/tooltip.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Preview from '../../components/codePreview/Preview';
+import PropsTable from "../../components/PropsTable";
 import Content from "../../public/examples/tooltip/Content";
 import Default from '../../public/examples/tooltip/Default';
 import Positions from "../../public/examples/tooltip/Positions";
@@ -43,6 +44,61 @@ export default function PageTooltip() {
           code={examples ? examples.Positions : 'Loading'}
         />
       </section>
+
+      <PropsTable
+        title="Props for Tooltip component"
+        data={[
+          {
+            name: 'children',
+            type: 'React.ReactElement',
+            required: true,
+            default: '-',
+            description: 'Component or element which Tooltip wraps around',
+          },
+          {
+            name: 'text',
+            type: 'string',
+            required: false,
+            default: '-',
+            description: 'Textual content for Tooltip',
+          },
+          {
+            name: 'icon',
+            type: 'React.ReactElement',
+            required: false,
+            default: '-',
+            description: 'Icon shown in the Tooltip',
+          },
+          {
+            name: 'content',
+            type: 'React.ReactElement',
+            required: false,
+            default: '-',
+            description: 'Custom component shown in the Tooltip',
+          },
+          {
+            name: 'position',
+            type: "'top' | 'bottom' | 'left' | 'right'",
+            required: false,
+            default: 'top',
+            description: 'Where Tooltip will be shown',
+          },
+          {
+            name: 'show',
+            type: 'boolean',
+            required: false,
+            default: 'false',
+            description: 'Should Tooltip be shown persistently',
+          },
+          {
+            name: 'fixed',
+            type: 'boolean',
+            required: false,
+            default: 'true',
+            description: 'Should Tooltip be rendered relative to parent or body element',
+          },
+        ]}
+      />
     </>
   );
 }
