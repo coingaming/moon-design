f0b46736858b1e04be233ff9eb16830f3e5c5544
yarema184 <yuliana.yarema@heathmont.net>
2022-05-04 10:00:42 +0300

    feat: add preset for TW

--- next-docs/tailwind.config.js
+++ next-docs/tailwind.config.js
@@ -1,62 +1,13 @@
-//as Variant of using rgba color with opacity
-// function withOpacityValue(hexColor, rgbColor) {
-function withOpacityValue(hexColor) {
-  return ({ opacityValue }) => {
-    if (opacityValue === undefined) {
-      return `rgb(var(${hexColor}))`;
-    }
-    // return `rgb(from var(${hexColor}) r g b / ${opacityValue})`;
-    return `rgb(var(${hexColor}) / ${opacityValue})`;
-  };
-}
-
-const plugin = require('tailwindcss/plugin');
-
 module.exports = {
   content: [
     './public/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './node_modules/@moon/core/**/*.{js,ts,jsx,tsx}',
-    // './node_modules/@moon/core/**/**/*.{js,ts,jsx,tsx}',
   ],
+  presets: [require('@moon/core/src/private/presets/ds-moon-preset')],
   theme: {
     extend: {
-      borderRadius: {
-        interactive: '8px',
-      },
-      borderWidth: {
-        interactive: '2px',
-      },
-      boxShadow: {
-        inset: '0 0 0 1px rgb(var(--piccolo)) inset',
-        interactive: '0 0 0 2px  rgb(var(--piccolo)) inset',
-        focus: `0 0 0 4px rgba(var(--piccolo), .13)`,
-      },
-      keyframes: {
-        error: {
-          '10%, 90%': { transform: 'translate3d(-1px, 0, 0)' },
-          '20%, 80%': { transform: 'translate3d(2px, 0, 0)' },
-          '30%, 50%, 70%': { transform: 'translate3d(-4px, 0, 0)' },
-          '40%, 60%': { transform: 'translate3d(4px, 0, 0)' },
-        },
-        pulse2: {
-          '10%, 90%': { transform: 'translate3d(-1px, 0, 0)' },
-          '0%': { transform: 'scale(1)' },
-          '70%': { transform: 'scale(1)', boxShadow: '0 0 0 30px transparent' },
-          '100%': { transform: 'scale(1)', boxShadow: '0 0 0 0 transparent' },
-        },
-        explode: {
-          '0%': { transform: 'translate(-50%, -50%) scale(0)', opacity: 1 },
-          '50%': { transform: 'translate(-50%, -50%) scale(1)', opacity: 0.4 },
-          '80%': { transform: 'translate(-50%, -50%) scale(1)', opacity: 0 },
-          '100%': { transform: 'translate(-50%, -50%) scale(1)', opacity: 0 },
-        },
-        rotation: {
-          '0%': { transform: 'rotate(0deg)' },
-          '100%': { transform: 'rotate(360deg)' },
-        },
-      },
       colors: {
         background: '#f4f4f4',
         border: {
@@ -76,117 +27,7 @@ module.exports = {
         developer: 'rgba(149, 241, 213, 0.9)',
         transparent: 'transparent',
         current: 'currentColor',
-        piccolo: {
-          DEFAULT: withOpacityValue('--backup-piccolo'),
-          // DEFAULT: withOpacityValue('--piccolo', ' --backup-piccolo'),
-          // DEFAULT: 'var(--piccolo, --backup-piccolo)',
-          20: '#4e46b420',
-        },
-        hit: {
-          DEFAULT: 'var(--hit)',
-        },
-        beerus: {
-          100: 'var(--beerus)',
-        },
-        goku: {
-          DEFAULT: 'var(--goku)',
-        },
-        gohan: {
-          DEFAULT: 'var(--gohan)',
-        },
-        bulma: {
-          DEFAULT: withOpacityValue('--bulma'),
-        },
-        trunks: {
-          DEFAULT: withOpacityValue('--trunks'),
-        },
-        goten: {
-          DEFAULT: 'var(--goten)',
-        },
-        popo: {
-          DEFAULT: 'var(--popo)',
-        },
-        // support colours
-        krillin: {
-          DEFAULT: 'var(--krillin)',
-          60: 'var(--krillin-60)',
-          10: 'var(--krillin-10)',
-        },
-        chiChi: {
-          DEFAULT: 'var(--chichi)',
-          60: 'var(--chichi-60)',
-          10: 'var(--chichi-10)',
-        },
-        roshi: {
-          DEFAULT: 'var(--roshi)',
-          60: 'var(--roshi-60)',
-          10: 'var(--roshi-10)',
-        },
-        dodoria: {
-          DEFAULT: 'var(--dodoria)',
-          60: 'var(--dodoria-60)',
-          10: 'var(--dodoria-10)',
-        },
-        cell: {
-          DEFAULT: 'var(--cell)',
-          60: 'var(--cell-60)',
-          10: 'var(--cell-10)',
-        },
-        raditz: {
-          DEFAULT: 'var(--raditz)',
-          60: 'var(--raditz-60)',
-          10: 'var(--raditz-10)',
-        },
-        whis: {
-          DEFAULT: 'var(--whis)',
-          60: 'var(--whis-60)',
-          10: 'var(--whis-10)',
-        },
-        frieza: {
-          DEFAULT: 'var(--frieza)',
-          60: 'var(--frieza-60)',
-          10: 'var(--frieza-10)',
-        },
-        nappa: {
-          DEFAULT: 'var(--nappa)',
-          60: 'var(--nappa-60)',
-          10: 'var(--nappa-10)',
-        },
       },
     },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('tailwindcss-rtl'),
-    plugin(function ({ addComponents }) {
-      addComponents({
-        '.btn-primary': {
-          color: 'var(--goten)',
-          backgroundColor: `var(--piccolo)`,
-        },
-        '.btn-secondary': {
-          color: 'var(--bulma)',
-          background: 'none',
-          boxShadow: `inset 0 0 0 1px var(--trunks)`,
-          '&:hover': {
-            boxShadow: `inset 0 0 0 1px var(--bulma)`,
-          },
-        },
-        '.btn-tertiary': {
-          color: 'var(--goten)',
-          backgroundColor: 'var(--hit)',
-        },
-        '.anim-error': {
-          transform: 'translate3d(0, 0, 0)',
-          backfaceVisibility: 'hidden',
-          perspective: '1000px',
-        },
-        '.anim-pulse': {
-          boxShadow: '0 0 0 0 var(--piccolo)',
-        },
-      });
-    }),
-  ],
 };
---
+++ workspaces/core/src/private/presets/ds-moon-preset.js
@@ -0,0 +1,166 @@
+//as Variant of using rgba color with opacity
+function withOpacityValue(rgbColor) {
+  return ({ opacityValue }) => {
+    if (opacityValue === undefined) {
+      return `rgb(var(${rgbColor}))`;
+    }
+    return `rgb(var(${rgbColor}) / ${opacityValue})`;
+  };
+}
+
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        averta:
+          'var(--averta) -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif',
+      },
+      borderRadius: {
+        interactive: '8px',
+      },
+      borderWidth: {
+        interactive: '2px',
+      },
+      boxShadow: {
+        inset: '0 0 0 1px rgb(var(--piccolo)) inset',
+        interactive: '0 0 0 2px  rgb(var(--piccolo)) inset',
+        focus: `0 0 0 4px rgba(var(--piccolo), .13)`,
+      },
+      keyframes: {
+        error: {
+          '10%, 90%': { transform: 'translate3d(-1px, 0, 0)' },
+          '20%, 80%': { transform: 'translate3d(2px, 0, 0)' },
+          '30%, 50%, 70%': { transform: 'translate3d(-4px, 0, 0)' },
+          '40%, 60%': { transform: 'translate3d(4px, 0, 0)' },
+        },
+        explode: {
+          '0%': { transform: 'translate(-50%, -50%) scale(0)', opacity: 1 },
+          '50%': { transform: 'translate(-50%, -50%) scale(1)', opacity: 0.4 },
+          '80%': { transform: 'translate(-50%, -50%) scale(1)', opacity: 0 },
+          '100%': { transform: 'translate(-50%, -50%) scale(1)', opacity: 0 },
+        },
+        rotation: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+        pulse2: {
+          '10%, 90%': { transform: 'translate3d(-1px, 0, 0)' },
+          '0%': { transform: 'scale(1)' },
+          '70%': { transform: 'scale(1)', boxShadow: '0 0 0 30px transparent' },
+          '100%': { transform: 'scale(1)', boxShadow: '0 0 0 0 transparent' },
+        },
+      },
+      colors: {
+        piccolo: {
+          DEFAULT: withOpacityValue('--piccolo'),
+        },
+        hit: {
+          DEFAULT: withOpacityValue('--hit'),
+        },
+        beerus: {
+          100: withOpacityValue('--beerus'),
+        },
+        goku: {
+          DEFAULT: withOpacityValue('--goku'),
+        },
+        gohan: {
+          DEFAULT: withOpacityValue('--gohan'),
+        },
+        bulma: {
+          DEFAULT: withOpacityValue('--bulma'),
+        },
+        trunks: {
+          DEFAULT: withOpacityValue('--trunks'),
+        },
+        goten: {
+          DEFAULT: withOpacityValue('--goten'),
+        },
+        popo: {
+          DEFAULT: withOpacityValue('--popo'),
+        },
+        // support colours
+        krillin: {
+          DEFAULT: withOpacityValue('--krillin'),
+          60: withOpacityValue('--krillin-60'),
+          10: withOpacityValue('--krillin-10'),
+        },
+        chiChi: {
+          DEFAULT: withOpacityValue('--chichi'),
+          60: withOpacityValue('--chichi-60'),
+          10: withOpacityValue('--chichi-10'),
+        },
+        roshi: {
+          DEFAULT: withOpacityValue('--roshi'),
+          60: withOpacityValue('--roshi-60'),
+          10: withOpacityValue('--roshi-10'),
+        },
+        dodoria: {
+          DEFAULT: withOpacityValue('--dodoria'),
+          60: withOpacityValue('--dodoria-60'),
+          10: withOpacityValue('--dodoria-10'),
+        },
+        cell: {
+          DEFAULT: withOpacityValue('--cell'),
+          60: withOpacityValue('--cell-60'),
+          10: withOpacityValue('--cell-10'),
+        },
+        raditz: {
+          DEFAULT: withOpacityValue('--raditz'),
+          60: withOpacityValue('--raditz-60'),
+          10: withOpacityValue('--raditz-10'),
+        },
+        whis: {
+          DEFAULT: withOpacityValue('--whis'),
+          60: withOpacityValue('--whis-60'),
+          10: withOpacityValue('--whis-10'),
+        },
+        frieza: {
+          DEFAULT: withOpacityValue('--frieza'),
+          60: withOpacityValue('--frieza-60'),
+          10: withOpacityValue('--frieza-10'),
+        },
+        nappa: {
+          DEFAULT: withOpacityValue('--nappa'),
+          60: withOpacityValue('--nappa-60'),
+          10: withOpacityValue('--nappa-10'),
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss-rtl'),
+    plugin(function ({ addComponents }) {
+      addComponents({
+        '.btn-primary': {
+          color: 'rgb(var(--goten))',
+          backgroundColor: `rgb(var(--piccolo))`,
+        },
+        '.btn-secondary': {
+          color: 'rgb(var(--bulma))',
+          background: 'none',
+          boxShadow: `inset 0 0 0 1px rgb(var(--trunks)/1)`,
+          '&:hover': {
+            boxShadow: `inset 0 0 0 1px rgb(var(--bulma))`,
+          },
+        },
+        '.btn-tertiary': {
+          color: 'rgb(var(--goten))',
+          backgroundColor: 'rgb(var(--hit))',
+        },
+        '.anim-error': {
+          transform: 'translate3d(0, 0, 0)',
+          backfaceVisibility: 'hidden',
+          perspective: '1000px',
+        },
+        '.anim-pulse': {
+          boxShadow: '0 0 0 0 rgb(var(--piccolo))',
+        },
+      });
+    }),
+  ],
+};
