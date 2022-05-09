fc151b7554d27fa92ca53adf98dc8f2dd251dd7a
oykos-development-hub <contact@oykos.me>
2022-05-09 13:19:26 +0200

    Fix table props for components (#1713)
    * feature: added missing props tables to component pages
    
    * refactor: change table props for some components
    
    * fix: review avatar and others (#1714)
    
    * fix: align next pages naming [MDS-18] (#1716)
    
    * MDS-21 add search without results popup (#1717)
    
    * feat: add search component props omitting results popup element
    
    * feat: add tests for search case without results
    
    Co-authored-by: filip-andjelic <filip.andjelic.private@gmail.com>
    
    * feat: Migrate to React 18 [MDS-52] (#1704)
    
    * fix: remove minor versions from package.json files [MDS-52]
    
    * feat: update @types/react and @types/react-dom to latest 16.9.xx [MDS-52-1]
    
    * feat: update @types/react to latest 16.x.x [MDS-52-1]
    
    * feat: update @types/react and @types/react-dom to earliest 17.x.x [MDS-52-1]
    
    * feat: update @types/react and @types/react-dom to latest 17.x.x [MDS-52-1]
    
    * feat: update react to v18 and @types to latest 17.x.x [MDS-52-1]
    
    * feat: update @types/react-dom and @types/react-test-renderer to v18 [MDS-52-1]
    
    * feat: update next to latest version [MDS-52]
    
    * fix: StaticImageData import [MDS-52]
    
    * fix: missing children types [MDS-52]
    
    * fix: refactor button, chip, loader and checkbox [MDS-52]
    
    * Feat/tooltip component (#1715)
    
    * feat: setting up basic behavior for tooltip component
    
    * feat: developing all positions for tooltip component
    
    * feat: show tooltip only on hover
    
    * feat: add persistent showing for tooltip component
    
    * feat: add persistent showing for tooltip component
    
    * feat: add fixed positioning for tooltip
    
    * feat: add fixed positioning for tooltip
    
    * feat: improve fixed positioning for tooltip
    
    * feat: improve fixed positioning for tooltip
    
    * feat: improve tooltip style
    
    * feat: remove tooltip on scroll and resize events
    
    * refactor: improve code
    
    * refactor: update tests for tooltip component
    
    * refactor: update props table for tooltip component
    
    Co-authored-by: filip-andjelic <filip.andjelic.private@gmail.com>
    
    * feature: added missing props tables to component pages
    
    * refactor: change table props for some components
    
    Co-authored-by: filip-andjelic <filip.andjelic.private@gmail.com>
    Co-authored-by: Karl Kallavus <karl.kallavus@heathmont.net>
    Co-authored-by: Dmytro Kireiev <dmytro.kireiev@gmail.com>

--- next-docs/pages/components/search.tsx
+++ next-docs/pages/components/search.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Preview from '../../components/codePreview/Preview';
+import PropsTable from "../../components/PropsTable";
 import OmitResults from "../../public/examples/search/OmitResults";
 import PreviewSearch from '../../public/examples/search/PreviewSearch';
 import PreviewSearchLoading from '../../public/examples/search/PreviewSearchLoading';
@@ -41,6 +42,110 @@ export default function PageSearch() {
           code={examples ? examples.OmitResults : 'Loading'}
         />
       </section>
+
+      <PropsTable
+        title="Props"
+        data={[
+          {
+            name: 'placeholder',
+            type: 'string',
+            required: false,
+            default: '-',
+            description: 'Search input placeholder',
+          },
+          {
+            name: 'isOpen',
+            type: 'boolean',
+            required: false,
+            default: 'false',
+            description: 'Open/close search',
+          },
+          {
+            name: 'onSubmit',
+            type: 'React.ReactFormEvent',
+            required: false,
+            default: '-',
+            description: 'Event that happens on submit',
+          },
+          {
+            name: 'size',
+            type: 'SearchInputSize',
+            required: false,
+            default: '-',
+            description: 'Search input size',
+          },
+          {
+            name: 'onChange',
+            type: 'React.ChangeEvent',
+            required: false,
+            default: '-',
+            description: 'Event that happens when user types something',
+          },
+          {
+            name: 'closeButton',
+            type: 'JSX.element',
+            required: false,
+            default: '-',
+            description: 'Specify close button',
+          },
+          {
+            name: 'hasBorder',
+            type: 'boolean',
+            required: false,
+            default: 'false',
+            description: 'Display border around search input',
+          },
+          {
+            name: 'onBlur',
+            type: 'React.FocusEvent',
+            required: false,
+            default: '-',
+            description: 'Event that happens when user clicks outside of search input',
+          },
+          {
+            name: 'onClear',
+            type: 'React.MouseEvent',
+            required: false,
+            default: '-',
+            description: 'Event that happens when user clears search input',
+          },
+          {
+            name: 'onFocus',
+            type: 'React.FocusEvent',
+            required: false,
+            default: '-',
+            description: 'Event that happens when user clicks on search input',
+          },
+          {
+            name: 'onKeyDown',
+            type: 'React.KeyboardEvent',
+            required: false,
+            default: '-',
+            description: 'Event that happens when user presses the key',
+          },
+          {
+            name: 'onKeyUp',
+            type: 'React.KeyboardEvent',
+            required: false,
+            default: '-',
+            description: 'Event that happens when user releases the key',
+          },
+          {
+            name: 'query',
+            type: 'string',
+            required: false,
+            default: '-',
+            description: '-',
+          },
+          {
+            name: 'results',
+            type: 'JSX.Elements',
+            required: false,
+            default: '-',
+            description: 'Results from search query',
+          },
+        ]}
+      />
     </>
   );
 }
