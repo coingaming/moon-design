be3d62c007be5314c74b1289201c9176eb3e23e5
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2022-05-16 15:37:56 +0300

    fix: add security headers and reduce images size [MDS-77]

--- next-docs/next.config.js
+++ next-docs/next.config.js
@@ -15,3 +15,38 @@ const withTM = require('next-transpile-modules')([
 ]); // pass the modules you would like to see transpiled
 
 module.exports = withTM();
+
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+];
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
+  poweredByHeader: false,
+};
