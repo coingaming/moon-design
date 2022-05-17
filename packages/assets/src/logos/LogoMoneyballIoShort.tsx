2bc308d6b1f376048c4ad53f9d73be2acc033717
karl-kallavus <karl@parity.io>
2022-05-17 14:06:19 +0530

    chore: bump snapshots

--- packages/assets/src/logos/LogoMoneyballIoShort.tsx
+++ packages/assets/src/logos/LogoMoneyballIoShort.tsx
@@ -3,20 +3,7 @@ import { ColorProps } from '@heathmont/moon-themes';
 import { themed } from '@heathmont/moon-utils';
 import styled from 'styled-components';
 
-const Svg = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    width="1em"
-    height="1em"
-    viewBox="0 0 28 28"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <title>{'Moneyball.io'}</title>
-    <circle cx={14.5} cy={14.5} r={13.5} fill="#FF9933" />
-    <circle cx={7.5} cy={7.5} r={7.5} fill="#1CA30F" />
-  </svg>
-);
+const Svg = (props: React.SVGProps<SVGSVGElement>) => <svg width="1em" height="1em" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}><path fillRule="evenodd" clipRule="evenodd" d="M14.8635 7.40273H13.1367C9.49312 7.40273 6.53945 10.3564 6.53945 14C6.53945 17.6436 9.49312 20.5973 13.1367 20.5973H14.8635C18.5071 20.5973 21.4608 17.6436 21.4608 14C21.4608 10.3564 18.5071 7.40273 14.8635 7.40273ZM13.1367 1.21289C6.0746 1.21289 0.349609 6.93788 0.349609 14C0.349609 21.0621 6.07455 26.7871 13.1367 26.7871H14.8635C21.9256 26.7871 27.6507 21.0621 27.6507 14C27.6507 6.93788 21.9256 1.21289 14.8635 1.21289H13.1367Z" fill="#EF3124" /></svg>;
 
 type SvgProps = {
   color?: ColorProps;
@@ -24,15 +11,20 @@ type SvgProps = {
   width?: string | number;
   fontSize?: string | number;
 };
-const MoneyballIoShort = styled(Svg)<SvgProps>(
-  ({ color, height, width, fontSize, theme }) => ({
-    ...(color && {
-      color: themed('color', color)(theme),
-    }),
-    height,
-    width,
-    fontSize,
-  })
-);
-MoneyballIoShort.defaultProps = {};
-export default MoneyballIoShort;
+const LogoMoneyballIoShort = styled(Svg)<SvgProps>(({
+  color,
+  height,
+  width,
+  fontSize,
+  theme
+}) => ({ ...(color && {
+    color: themed('color', color)(theme)
+  }),
+  height,
+  width,
+  fontSize
+}));
+LogoMoneyballIoShort.defaultProps = {
+    
+  };
+export default LogoMoneyballIoShort;
