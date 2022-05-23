2ad00af1ef3b5863b7f0e58d6668b174925a00c1
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2022-05-23 17:44:58 +0300

    fix: chip component [MDS-91]

--- workspaces/core/src/button/private/buttonStyles/buttonCommonStyles.ts
+++ workspaces/core/src/button/private/buttonStyles/buttonCommonStyles.ts
@@ -3,7 +3,7 @@ import type ButtonProps from '../types/ButtonProps';
 
 const buttonCommonStyles = ({ disabled }: ButtonProps): string => {
   return classNames(
-    'relative z-0 flex justify-center items-center font-medium no-underline overflow-hidden select-none outline-none transition-all duration-200 active:scale-90 focus-visible:shadow-focus',
+    'relative z-0 flex justify-center items-center font-medium no-underline overflow-hidden select-none outline-none transition duration-200 active:scale-90 focus-visible:shadow-focus',
     disabled && 'opacity-30 cursor-not-allowed'
   );
 };
