9e53943a6000ec1ebbb4ce01de6610e7becb9bc1
yarema184 <yuliana.yarema@heathmont.net>
2022-05-31 16:59:07 +0300

    fix: small fixes

--- next-docs/public/examples/loader/DefaultTW.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Loader } from "@heathmont/moon-core-tw";
-
-const Example = () => (<div className="flex justify-around w-full items-center bg-gray-50 py-6 rounded-lg">
-  <Loader />
-</div>);
-
-export default Example;
