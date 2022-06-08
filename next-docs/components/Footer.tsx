aa2249906ba78284cb504e4dcda819fd5879cbea
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2022-06-08 11:46:51 +0300

    fix: vision page [MDS-114] (#1774)
    * fix: links [MDS-117]
    
    * fix: layout issues [MDS-117]
    
    * fix: component pages [MDS-117]
    
    * fix: add ghost chip to docs [MDS-117]
    
    * fix: more component pages [MDS-117]
    
    * fix: more component pages [MDS-117]
    
    * fix: more component pages [MDS-117]
    
    * fix: more component pages [MDS-117]
    
    * fix: manifest page [MDS-115]
    
    * fix: SettingsButton [MDS-115]
    
    * fix: vision page [MDS-114]
    
    * fix: footer [MDS-114]

--- next-docs/components/Footer.tsx
+++ next-docs/components/Footer.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link';
 import Logo from './sidebar/Logo';
 
 const Footer = () => (
-  <footer className="flex gap-x-8 gap-y-12 pt-16 pb-8 flex-wrap items-center text-moon-16 text-bulma justify-between">
-    <div className="flex gap-x-8 gap-y-12 flex-wrap items-center">
+  <footer className="relative z-10 flex gap-x-8 gap-y-6 pt-16 pb-6 flex-wrap items-center text-moon-16 text-bulma justify-center md:justify-between">
+    <div className="flex gap-x-8 gap-y-6 flex-wrap justify-center items-center">
       <Logo height={16} />
-      <div className="flex gap-x-8 gap-y-12 flex-wrap items-center">
+      <div className="flex gap-x-8 gap-y-4 flex-wrap justify-center items-center">
         <Link href="https://github.com/coingaming/moon-design">
           <a className="hover:underline" target="_blank" rel="noreferrer">
             Github
@@ -13,12 +13,17 @@ const Footer = () => (
         </Link>
         <Link href="https://www.figma.com/community/file/1002945721703152933">
           <a className="hover:underline" target="_blank" rel="noreferrer">
-            Figma Community
+            Figma
           </a>
         </Link>
         <Link href="https://moonds.medium.com/">
           <a className="hover:underline" target="_blank" rel="noreferrer">
             Medium
+          </a>
+        </Link>
+        <Link href="https://www.linkedin.com/company/moon-io/">
+          <a className="hover:underline" target="_blank" rel="noreferrer">
+            LinkedIn
           </a>
         </Link>
       </div>
