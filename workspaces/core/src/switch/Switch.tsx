f896129c3ee1061440bc12065d1e336a6d4a069c
filip-andjelic <filip.andjelic.private@gmail.com>
2022-07-15 09:45:49 +0200

    refactor: improve switch tailwind component

--- workspaces/core/src/switch/Switch.tsx
+++ workspaces/core/src/switch/Switch.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classNames from '../private/utils/classnames';
+import getSizeClasses from "./private/getSizeClasses";
 import IconMoon from './private/iconMoon';
 import IconSun from './private/iconSun';
 import SwitchProps, { Size } from './private/switchProps';
@@ -11,47 +12,8 @@ const Switch: React.FC<SwitchProps> = ({
   onChange,
   isRTL,
   theme,
-  size = 'md',
+  size = Size.MEDIUM,
 }) => {
-  const getSizes = () => {
-    let classes = '';
-
-    if (!button) {
-      switch (size) {
-        case Size.XSMALL:
-          classes += 'w-[32px] h-[16px] after:w-[12px] after:h-[12px] ';
-          classes += isRTL ? 'after:translate-x-3 peer-checked:after:translate-x-0' : 'peer-checked:after:translate-x-3';
-          break;
-        case Size.SMALL:
-          classes += 'w-[44px] h-[24px] after:w-[16px] after:h-[16px] ';
-          classes += isRTL ? 'after:translate-x-5 peer-checked:after:translate-x-0' : 'peer-checked:after:translate-x-5';
-          break;
-        case Size.MEDIUM:
-          classes += 'w-[60px] h-[32px] after:w-[24px] after:h-[24px] ';
-          classes += isRTL ? 'after:translate-x-7 peer-checked:after:translate-x-0' : 'peer-checked:after:translate-x-7';
-          break;
-        default:
-          classes += 'w-[60px] h-[32px] after:w-[24px] after:h-[24px] ';
-          classes += isRTL ? 'after:translate-x-7 peer-checked:after:translate-x-0' : 'peer-checked:after:translate-x-7';
-          break;
-      }
-
-      classes += ' after:rounded-full ';
-    }
-    else {
-      if (size === Size.LARGE) {
-        classes += 'w-[122px] h-[48px] top-[191px] left-[577px] after:w-[57px] after:h-[40px] after:z-0 peer-checked:after:z-0 after:top-1 after:left-1 after:px-2 after:py-1 after:rounded-[8px] rounded-[12px] p-1 gap-1 ';
-      }
-      else if (size === Size.MEDIUMBUTTONS) {
-        classes += 'w-[106px] h-[40px] top-[195px] left-[161px] after:w-[49px] after:h-[32px] after:z-0 peer-checked:after:z-0 after:top-1 after:left-1 after:px-1 after:py-1 after:rounded-[8px] rounded-[12px] p-1 gap-1 ';
-      }
-
-      classes += isRTL ? 'after:translate-x-full peer-checked:after:translate-x-0' : 'peer-checked:after:translate-x-full';
-    }
-
-    return classes;
-  }
-
   return (
     <>
       <label
@@ -79,7 +41,7 @@ const Switch: React.FC<SwitchProps> = ({
         <span
           className={classNames(
             'flex items-center flex-shrink-0 p-1 rounded-full duration-300 ease-in-out bg-beerus peer-checked:bg-piccolo after:bg-white after:shadow-md after:duration-300',
-            `${getSizes()}`,
+            `${getSizeClasses({button: !!button, size, isRTL})}`,
             theme && 'bg-beerus peer-checked:bg-black after:z-1 peer-checked:cursor-pointer after:cursor-pointer',
             disabled ? 'cursor-not-allowed peer-checked:cursor-not-allowed after:cursor-not-allowed' : 'cursor-pointer peer-checked:cursor-pointer after:cursor-pointer'
           )}
