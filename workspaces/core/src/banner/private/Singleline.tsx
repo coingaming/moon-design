94e88f319749eae3fadb3938a38076d785e7bd27
filip-andjelic <filip.andjelic.private@gmail.com>
2022-07-15 09:56:29 +0200

    refactor: implement yuliana feedback for banner component

--- workspaces/core/src/banner/private/Singleline.tsx
+++ workspaces/core/src/banner/private/Singleline.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 export interface Props {
   description: string;
-  link: React.ReactElement;
+  link?: React.ReactElement;
 }
 
 const Singleline: React.FC<Props> = ({ description, link }) => {
   return (<div className='w-full bg-goten rounded-lg py-2 pl-3 flex-row'>
     <div className='sm:flex flex-row'>
       <p className='flex items-center align-middle'>{description}</p>
-      <div className='flex sm:justify-end sm:items-center pl-1 gap-32'>{link}</div>
+      {!!link && <div className='flex sm:justify-end sm:items-center pl-1 gap-32'>{link}</div>}
     </div>
   </div>);
 };
