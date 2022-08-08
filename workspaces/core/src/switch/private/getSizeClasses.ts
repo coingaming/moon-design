cdbf2b15f0ad8b5c53c5e74f829f433c0418ff3e
filip-andjelic <filip.andjelic.private@gmail.com>
2022-08-08 16:44:34 +0200

    fix: replace PX with REM where possible for switch component

--- workspaces/core/src/switch/private/getSizeClasses.ts
+++ workspaces/core/src/switch/private/getSizeClasses.ts
@@ -14,19 +14,19 @@ const getSizeClasses = ({
   if (!button) {
     switch (size) {
       case '2xs':
-        classes += 'w-[32px] h-[16px] after:w-[12px] after:h-[12px] ';
+        classes += 'w-8 h-4 after:w-3 after:h-[12px] ';
         classes += isRTL
           ? 'after:translate-x-3 peer-checked:after:translate-x-0'
           : 'peer-checked:after:translate-x-3';
         break;
       case 'xs':
-        classes += 'w-[44px] h-[24px] after:w-[16px] after:h-[16px] ';
+        classes += 'w-11 h-6 after:w-4 after:h-4 ';
         classes += isRTL
           ? 'after:translate-x-5 peer-checked:after:translate-x-0'
           : 'peer-checked:after:translate-x-5';
         break;
       default:
-        classes += 'w-[60px] h-[32px] after:w-[24px] after:h-[24px] ';
+        classes += 'w-[3.75rem] h-8 after:w-6 after:h-6 ';
         classes += isRTL
           ? 'after:translate-x-7 peer-checked:after:translate-x-0'
           : 'peer-checked:after:translate-x-7';
@@ -37,10 +37,10 @@ const getSizeClasses = ({
   } else {
     if (size === 'lg') {
       classes +=
-        'w-[122px] h-[48px] top-[191px] left-[577px] after:w-[57px] after:h-[40px] after:z-0 peer-checked:after:z-0 after:top-1 after:left-1 after:px-2 after:py-1 after:rounded-[8px] rounded-[12px] p-1 gap-1 ';
+        'w-[122px] h-[48px] after:w-[57px] after:h-[40px] after:z-0 peer-checked:after:z-0 after:top-1 after:left-1 after:px-2 after:py-1 after:rounded-[8px] rounded-[12px] p-1 gap-1 ';
     } else if (size === 'md') {
       classes +=
-        'w-[106px] h-[40px] top-[195px] left-[161px] after:w-[49px] after:h-[32px] after:z-0 peer-checked:after:z-0 after:top-1 after:left-1 after:px-1 after:py-1 after:rounded-[8px] rounded-[12px] p-1 gap-1 ';
+        'w-[106px] h-[40px] after:w-[49px] after:h-[32px] after:z-0 peer-checked:after:z-0 after:top-1 after:left-1 after:px-1 after:py-1 after:rounded-[8px] rounded-[12px] p-1 gap-1 ';
     }
 
     classes += isRTL
