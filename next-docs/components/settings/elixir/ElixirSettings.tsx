2c1e0b75084bc030998db9e3620ead74a18e6c7b
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2022-08-26 15:45:45 +0300

    feat: remove internal brands [MDS-200] (#1867)
    * fix: merge develop
    
    * feat: remove internal brands [MDS-200]

--- next-docs/components/settings/elixir/ElixirSettings.tsx
+++ next-docs/components/settings/elixir/ElixirSettings.tsx
@@ -1,66 +1,33 @@
 import { useContext, useState } from 'react';
-import {
-  LogoSportsbetShort,
-  LogoBitcasinoShort,
-  LogoMoonDesignShort,
-  LogoBetaddaShort,
-} from '@heathmont/moon-assets';
 import useRtl from '../../../utils/useRtl';
 import ReactElixirSwitcher from '../common/ReactElixirSwitcher';
 import SettingsButton from '../common/SettingsButton';
 import DarkLightModeSwitcher from '../react/DarkLightModeSwitch';
 import RTLModeSwitch from '../react/RTLModeSwitch';
-import { BrandThemeButton } from './BrandThemeButton';
 import { store } from './ElixirThemeProvider';
-import { elixirThemeKeys, ElixirThemeType } from './store';
 
 interface Props {
   toggleReactAndElixir: () => void;
   isElixirEnabled: boolean;
 }
 
-const Logos = {
-  sportsbet: <LogoSportsbetShort fontSize="1rem" />,
-  bitcasino: <LogoBitcasinoShort fontSize="1rem" />,
-  moonDesign: <LogoMoonDesignShort fontSize="1rem" />,
-  betadda: <LogoBetaddaShort fontSize="1rem" />,
-};
-
-const getLogo = (themeKey: string | ElixirThemeType): JSX.Element =>
-  Logos[themeKey as LogosKeys] || Logos.moonDesign;
-
-type LogosKeys = keyof typeof Logos;
-
 const ElixirSettings: React.FC<Props> = ({
   toggleReactAndElixir,
   isElixirEnabled,
 }) => {
   const {
-    state: { isDarkMode, isElixir },
+    state: { isDarkMode },
     dispatch,
   } = useContext(store);
   const [isOpened, setIsOpened] = useState(false);
   const toggle = () => setIsOpened(!isOpened);
   const { toggleRtl, isRtlEnabled } = useRtl();
-  const setElixirTheme = (theme: ElixirThemeType) => {
-    dispatch({ type: 'setTheme', payload: theme });
-  };
   return (
     <>
       <SettingsButton onClick={toggle} />
       {isOpened && (
         <>
           <div className="ltr:right-4 rtl:left-4 fixed bottom-16 z-50">
-            {elixirThemeKeys.map((themeKey: ElixirThemeType) => (
-              <BrandThemeButton
-                key={themeKey}
-                onClick={() => setElixirTheme(themeKey)}
-              >
-                {getLogo(themeKey)}
-              </BrandThemeButton>
-            ))}
-          </div>
-          <div className="ltr:right-4 rtl:left-4 fixed bottom-28 z-50">
             <DarkLightModeSwitcher
               toggle={() =>
                 dispatch({
@@ -72,10 +39,10 @@ const ElixirSettings: React.FC<Props> = ({
               isEnabled={isDarkMode}
             />
           </div>
-          <div className="ltr:right-4 rtl:left-4 fixed bottom-40 z-50">
+          <div className="ltr:right-4 rtl:left-4 fixed bottom-[6.75rem] z-50">
             <RTLModeSwitch toggle={toggleRtl} isEnabled={isRtlEnabled} />
           </div>
-          <div className="ltr:right-4 rtl:left-4 fixed bottom-40 z-50">
+          <div className="ltr:right-4 rtl:left-4 fixed bottom-[9.5rem] z-50">
             <ReactElixirSwitcher
               toggle={toggleReactAndElixir}
               isReactEnabled={!isElixirEnabled}
