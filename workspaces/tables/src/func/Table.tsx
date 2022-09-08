0c53ab432b98f967c7f43f084870f0dedfa9a66c
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2022-09-08 15:39:50 +0300

    fix: style table

--- workspaces/tables/src/func/Table.tsx
+++ workspaces/tables/src/func/Table.tsx
@@ -6,9 +6,8 @@ import {
   th,
   trHeader,
   trBody,
-  trBodyInner,
-  tableContainer,
   thead,
+  tbody,
 } from '../styled/StyledTable';
 
 type Props<Columns, Data> = {
@@ -32,36 +31,34 @@ export default function Table<
     );
 
   return (
-    <div className={tableContainer}>
-      <table {...getTableProps()} className={table}>
-        <thead className={thead}>
-          {headerGroups.map((headerGroup) => (
-            <tr className={trHeader} {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th className={th} {...column.getHeaderProps()}>
-                  {column.render('Header')}
-                </th>
-              ))}
+    <table {...getTableProps()} className={table}>
+      <thead className={thead}>
+        {headerGroups.map((headerGroup) => (
+          <tr className={trHeader} {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th className={th} {...column.getHeaderProps()}>
+                {column.render('Header')}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()} className={tbody}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr className={trBody} {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return (
+                  <td className={td} {...cell.getCellProps()}>
+                    {cell.render('Cell')}
+                  </td>
+                );
+              })}
             </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()} className={trBody}>
-          {rows.map((row, i) => {
-            prepareRow(row);
-            return (
-              <tr className={trBody} {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return (
-                    <td className={td} {...cell.getCellProps()}>
-                      {cell.render('Cell')}
-                    </td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
