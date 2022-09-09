8ea94f08001454c23a90d1eda9f2cb618bf676a1
yarema184 <yuliana.yarema@heathmont.net>
2022-09-09 14:33:03 +0300

    fix: tw select br ans label size for sm

--- workspaces/core/src/select/Select.tsx
+++ workspaces/core/src/select/Select.tsx
@@ -60,7 +60,12 @@ const Select: React.FC<SelectProps<BaseOptionType[]>> = ({
         >
           <div className="relative">
             {label && size !== 'xl' && (
-              <Listbox.Label className="block text-moon-16 text-bulma pb-2">
+              <Listbox.Label
+                className={classNames(
+                  'block text-bulma pb-2',
+                  size === 'sm' ? 'text-moon-14' : 'text-moon-16'
+                )}
+              >
                 {label}
               </Listbox.Label>
             )}
