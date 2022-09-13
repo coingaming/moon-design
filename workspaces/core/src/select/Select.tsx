09a08c51138591e9519123e2d72a8d53748eacd3
yarema184 <yuliana.yarema@heathmont.net>
2022-09-13 10:33:30 +0300

    fix: tw select types

--- workspaces/core/src/select/Select.tsx
+++ workspaces/core/src/select/Select.tsx
@@ -12,21 +12,21 @@ export type BaseOptionType = {
 };
 
 export type SelectSize = 'sm' | 'md' | 'lg' | 'xl';
-export type SelectProps<T extends readonly object[]> = {
+export type SelectProps<T extends readonly object[], BaseOptionType> = {
   size?: SelectSize | string;
   label?: JSX.Element | string;
   placeholder?: JSX.Element | string;
   isError?: boolean;
   hintText?: JSX.Element | string;
   value?: BaseOptionType;
-  onChange?: (value?: BaseOptionType | string | null) => void;
+  onChange?: (value?: BaseOptionType) => void;
   options: T;
   formatOptionLabel?: (data?: BaseOptionType) => JSX.Element | string;
   menuWidth?: string;
   disabled?: boolean;
 };
 
-const Select: React.FC<SelectProps<BaseOptionType[]>> = ({
+const Select: React.FC<SelectProps<BaseOptionType[], BaseOptionType>> = ({
   label,
   placeholder,
   size = 'md',
