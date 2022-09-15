2a7f4bbd6432d7fa14758dc995b1a7c3b158df21
filip-andjelic <contact@oykos.me>
2022-09-15 11:43:21 +0200

    feat: Added tabs component to templates

--- workspaces/core/src/tabs/Tabs.tsx
+++ workspaces/core/src/tabs/Tabs.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, ReactElement, ReactNode, useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Tab } from '@headlessui/react';
 
 type TabsProps = {
@@ -10,6 +10,7 @@ type TabsProps = {
   verticalTabPills?: boolean;
   verticalOverline?: boolean;
   isUnderline?: boolean;
+  isTemplateTab?: boolean;
 };
 
 const Tabs: React.FC<TabsProps> = ({
@@ -20,16 +21,14 @@ const Tabs: React.FC<TabsProps> = ({
   verticalTabPills,
   verticalUnderline,
   verticalOverline,
-  isUnderline
+  isUnderline,
+  isTemplateTab
 }) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
   return (
     <>
       {!isVertical && !isSegmented && (
-        <Tab.Group
-          selectedIndex={selectedIndex}
-          onChange={setSelectedIndex}
-        >
+        <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
           {isUnderline && (
             <Tab.List className="flex p-1 ">
               {items.map((item) => {
@@ -39,11 +38,16 @@ const Tabs: React.FC<TabsProps> = ({
                       <button
                         className={
                           selected
-                            ? 'border border-b-piccolo border-x-0 border-t-0 text-piccolo px-3 py-2'
-                            : 'text-black px-3 py-2 hover:text-piccolo hover:bg-beerus hover:rounded-lg'
+                            ? `border border-b-piccolo border-x-0 border-t-0 text-piccolo px-3 py-2 ring-transparent focus:outline-none ${
+                                isTemplateTab && 'text-white'
+                              }`
+                            : `text-black px-3 py-2 hover:text-piccolo hover:bg-beerus hover:rounded-lg ${
+                                isTemplateTab &&
+                                '!text-trunks hover:text-trunks hover:bg-transparent'
+                              }`
                         }
                       >
-                        <p className="leading-6 self-left"> {item} </p>
+                        <p className={`leading-6 self-left`}>{item}</p>
                       </button>
                     )}
                   </Tab>
@@ -53,34 +57,31 @@ const Tabs: React.FC<TabsProps> = ({
           )}
 
           {isTabPills && (
-              <Tab.List className="flex p-1">
-                {items.map((item) => {
-                  return (
-                    <Tab as={Fragment}>
-                      {({ selected }) => (
-                        <button
-                          className={
-                            selected
-                              ? 'bg-piccolo text-white rounded-lg px-3 py-2 focus:border-none text-left'
-                              : 'text-black px-3 py-2 hover:bg-beerus hover:text-piccolo text-left hover:rounded-lg'
-                          }
-                        >
-                          <p className="leading-6 left-3"> {item} </p>
-                        </button>
-                      )}
-                    </Tab>
-                  );
-                })}
-              </Tab.List>
+            <Tab.List className="flex p-1">
+              {items.map((item) => {
+                return (
+                  <Tab as={Fragment}>
+                    {({ selected }) => (
+                      <button
+                        className={
+                          selected
+                            ? 'bg-piccolo text-white rounded-lg px-3 py-2 focus:border-none text-left ring-transparent focus:outline-none'
+                            : 'text-black px-3 py-2 hover:bg-beerus hover:text-piccolo text-left hover:rounded-lg'
+                        }
+                      >
+                        <p className="leading-6 left-3"> {item} </p>
+                      </button>
+                    )}
+                  </Tab>
+                );
+              })}
+            </Tab.List>
           )}
         </Tab.Group>
       )}
 
       {isVertical && (
-        <Tab.Group
-          selectedIndex={selectedIndex}
-          onChange={setSelectedIndex}
-        >
+        <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex} vertical>
           {verticalUnderline && (
             <Tab.List className="flex flex-col p-1 mb-2">
               {items.map((item) => {
@@ -90,7 +91,7 @@ const Tabs: React.FC<TabsProps> = ({
                       <button
                         className={
                           selected
-                            ? 'border border-l-piccolo border-y-0 border-r-0 text-piccolo px-3 py-2 text-left'
+                            ? 'border border-l-piccolo border-y-0 border-r-0 text-piccolo px-3 py-2 text-left ring-transparent focus:outline-none'
                             : 'text-black px-3 py-2 hover:text-piccolo hover:bg-beerus hover:rounded-lg text-left'
                         }
                       >
@@ -112,7 +113,7 @@ const Tabs: React.FC<TabsProps> = ({
                       <button
                         className={
                           selected
-                            ? 'border border-r-piccolo border-y-0 border-l-0 text-piccolo px-3 py-2 text-left'
+                            ? 'border border-r-piccolo border-y-0 border-l-0 text-piccolo px-3 py-2 text-left ring-transparent focus:outline-none'
                             : 'text-black px-3 py-2 hover:text-piccolo hover:bg-beerus hover:rounded-lg text-left'
                         }
                       >
@@ -134,7 +135,7 @@ const Tabs: React.FC<TabsProps> = ({
                       <button
                         className={
                           selected
-                            ? 'bg-piccolo text-white rounded-lg px-3 py-2 focus:border-none text-left'
+                            ? 'bg-piccolo text-white rounded-lg px-3 py-2 focus:border-none text-left ring-transparent focus:outline-none'
                             : 'text-black px-3 py-2 hover:bg-beerus hover:text-piccolo text-left hover:rounded-lg'
                         }
                       >
@@ -150,10 +151,7 @@ const Tabs: React.FC<TabsProps> = ({
       )}
 
       {isSegmented && !isVertical && (
-        <Tab.Group
-          selectedIndex={selectedIndex}
-          onChange={setSelectedIndex}
-        >
+        <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
           <Tab.List className="flex p-1 bg-beerus rounded-xl">
             {items.map((item) => {
               return (
@@ -162,7 +160,7 @@ const Tabs: React.FC<TabsProps> = ({
                     <button
                       className={
                         selected
-                          ? 'bg-gohan text-black rounded-lg px-3 py-2 focus:border-none '
+                          ? 'bg-gohan text-black rounded-lg px-3 py-2 focus:border-none ring-transparent focus:outline-none'
                           : 'text-black px-3 py-2'
                       }
                     >
