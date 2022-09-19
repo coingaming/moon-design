413ea327cd159ad4ec46a6ec98c9fbe38187983d
filip-andjelic <contact@oykos.me>
2022-09-19 13:09:57 +0200

    fix: Fixed segment control

--- next-docs/public/examples/tabsTW/SegmentControl.tsx
+++ next-docs/public/examples/tabsTW/SegmentControl.tsx
@@ -1,30 +1,54 @@
 import { Tab } from '@headlessui/react';
 import React, { Fragment, useState } from 'react';
 
-const items = [
-  '$Link', '$Link'
-]
+const items = ['$Link', '$Link', '$Link', '$Link'];
 
 const Example = () => {
   return (
     <>
       <Tab.Group>
-        <Tab.List className="flex p-1 bg-beerus rounded-xl">
+        <Tab.List className="flex h-10 p-1 bg-beerus rounded-xl">
           {items.map((item: string) => {
             return (
-              <Tab as={Fragment}>
-                {({ selected }) => (
-                  <button
-                    className={
-                      selected
-                        ? 'bg-gohan text-black rounded-lg px-3 py-2 focus:border-none '
-                        : 'text-black px-3 py-2'
-                    }
-                  >
-                    <p className="leading-6 self-left"> {item} </p>
-                  </button>
-                )}
-              </Tab>
+              <div className="mr-2">
+                <Tab as={Fragment}>
+                  {({ selected }) => (
+                    <button
+                      className={
+                        selected
+                          ? 'bg-gohan  text-bulma rounded-lg px-2 py-1 focus:border-none shadow-sm '
+                          : 'text-bulma px-2 py-1'
+                      }
+                    >
+                      <span className="leading-6 self-left"> {item} </span>
+                    </button>
+                  )}
+                </Tab>
+              </div>
+            );
+          })}
+        </Tab.List>
+      </Tab.Group>
+
+      <Tab.Group>
+        <Tab.List className="flex h-12 p-1 bg-beerus rounded-xl">
+          {items.map((item: string) => {
+            return (
+              <div className="mr-2">
+                <Tab as={Fragment}>
+                  {({ selected }) => (
+                    <button
+                      className={
+                        selected
+                          ? 'bg-gohan text-bulma rounded-lg px-3 py-2 focus:border-none shadow-sm '
+                          : 'text-bulma px-3 py-2'
+                      }
+                    >
+                      <span className="leading-6 self-left"> {item} </span>
+                    </button>
+                  )}
+                </Tab>
+              </div>
             );
           })}
         </Tab.List>
