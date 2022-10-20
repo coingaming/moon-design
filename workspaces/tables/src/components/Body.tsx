38157d955dca3cc3fc177dad0e258ac554d0e71d
filip-andjelic <contact@oykos.me>
2022-10-20 18:07:00 +0200

    test

--- workspaces/tables/src/components/Body.tsx
+++ workspaces/tables/src/components/Body.tsx
@@ -1,18 +1,21 @@
-import React, {forwardRef, ReactNode} from 'react';
+import React, { forwardRef, ReactNode } from 'react';
 
 type BodyProps = {
   children?: ReactNode;
   reactTableProps?: any;
 };
 
-const Body = forwardRef<HTMLDivElement,  BodyProps>(({
-  children,
-  reactTableProps,
-}) => {
-  return (<div
-    {...reactTableProps}
-    className='flex flex-col gap-1 relative z-0 no-scrollbar'
-  >{children}</div>);
-});
+const Body = forwardRef<HTMLDivElement, BodyProps>(
+  ({ children, reactTableProps }) => {
+    return (
+      <div
+        {...reactTableProps}
+        className="flex flex-col gap-1 relative z-0 no-scrollbar"
+      >
+        {children}
+      </div>
+    );
+  }
+);
 
 export default Body;
