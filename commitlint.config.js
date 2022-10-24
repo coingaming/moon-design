36a55a72485eb38f3e9efc0b02ed1d277c4d00e0
filip-andjelic <contact@oykos.me>
2022-10-24 13:30:32 +0200

    feat: pre-commit hook improvement

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,24 @@
 module.exports = {
-  extends: [
-    "@commitlint/config-conventional"
-  ],
-   rules: {
-        "type-enum": [2, "always", ["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test", "button"]],
-  }
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'button',
+      ],
+    ],
+    'body-full-stop': [2, 'never', '.'],
+  },
 };
