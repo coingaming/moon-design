04127f5256c3b9ce45989dad859dca9bf2869ae4
oladetounjed <oladetountemitayo@gmail.com>
2022-11-08 10:10:14 +0200

    feat: BASE1-3301 Snackbar Implementation

--- workspaces/core/src/snackbar/Snackbar.tsx
+++ workspaces/core/src/snackbar/Snackbar.tsx
@@ -2,7 +2,10 @@ import React, { Children, createContext, useContext, useEffect } from 'react';
 import {
   ControlsClose,
   GenericCheckAlternative,
-  GenericInfo, GenericClose, GenericAlarm, GenericCheckRounded
+  GenericInfo,
+  GenericClose,
+  GenericAlarm,
+  GenericCheckRounded,
 } from '@heathmont/moon-icons-tw';
 import * as ToastPrimitive from '@radix-ui/react-toast';
 import classNames from '../private/utils/classnames';
@@ -18,13 +21,11 @@ type SnackbarProps = {
     | 'bottom-right';
   ref?: null;
   className?: string;
-  type?: 'success' | 'error' | 'info' | 'warning';
   isOpen: boolean;
   setOpenSnackbar: () => void;
 };
 
 type IconProps = {
-  backgroundColor?: string;
   className?: string;
 };
 type ContentProps = {
@@ -44,74 +45,43 @@ const SnackbarRoot: React.FC<SnackbarProps> = ({
   children,
   ref,
   className,
-  type,
   isOpen,
   setOpenSnackbar,
   ...rest
 }) => {
-  const [open, setOpen] = React.useState(isOpen || false);
-  useEffect(() => {
-    if (isOpen) {
-      console.log(isOpen)
-      setOpen(false);
-      setTimeout(() => {
-        setOpen(true);
-      }, 400);
-    } else {
-      setOpen(true);
-    }
-  }, [isOpen]);
   return (
-    <ToastPrimitive.Provider swipeDirection="right" duration={autoClose || 6000}>
+    <ToastPrimitive.Provider
+      swipeDirection="right"
+      duration={autoClose || 6000}
+    >
       <ToastPrimitive.Root
-        open={open}
-        onOpenChange={setOpen}
+        open={isOpen}
+        onOpenChange={setOpenSnackbar}
         ref={ref}
         className={classNames(
           'z-50 fixed right-4 left-4 w-auto shadow-lg rounded-xl bg-goku px-4 py-6 bg-goku text-left flex items-center transition gap-2 medium',
           'md:w-full md:max-w-sm md:right-4',
-          "data-[state=open]:animate-[toast-slide-in-bottom_150ms_cubic-bezier(0.16, 1, 0.3, 1)]",
-          "md:data-[state=open]:animate-[toast-slide-in-right_150ms_cubic-bezier(0.16, 1, 0.3, 1)]",
-          "radix-state-closed:animate-[toast-hide_100ms_ease-in_forwards]",
-          "radix-swipe-end:animate-[toast-swipe-out_100ms_ease-out_forwards]",
-          "translate-x-radix-toast-swipe-move-x",
-          "radix-swipe-cancel:translate-x-0 radix-swipe-cancel:duration-200 radix-swipe-cancel:ease-[ease]",
+          'data-[state=open]:animate-[toast-slide-in-bottom_150ms_cubic-bezier(0.16, 1, 0.3, 1)]',
+          'md:data-[state=open]:animate-[toast-slide-in-right_150ms_cubic-bezier(0.16, 1, 0.3, 1)]',
+          'radix-state-closed:animate-[toast-hide_100ms_ease-in_forwards]',
+          'radix-swipe-end:animate-[toast-swipe-out_100ms_ease-out_forwards]',
+          'translate-x-radix-toast-swipe-move-x',
+          'radix-swipe-cancel:translate-x-0 radix-swipe-cancel:duration-200 radix-swipe-cancel:ease-[ease]',
           position === 'top-left' && 'top-4 md:left-4 md:right-auto',
-          position === 'top-center'
-           && 'top-4  ml-auto mr-auto md:left-0 md:right-0',
-          position === 'top-right'&& 'top-4 md:right-4 md:left-auto',
-          position === 'bottom-left'&& 'bottom-4 md:left-4 md:right-auto',
-          position === 'bottom-center'
-           && 'bottom-4  ml-auto mr-auto md:left-0 md:right-0',
-          position === 'bottom-right'&& 'bottom-4 md:right-4 md:left-auto',
+          position === 'top-center' &&
+            'top-4  ml-auto mr-auto md:left-0 md:right-0',
+          position === 'top-right' && 'top-4 md:right-4 md:left-auto',
+          position === 'bottom-left' && 'bottom-4 md:left-4 md:right-auto',
+          position === 'bottom-center' &&
+            'bottom-4  ml-auto mr-auto md:left-0 md:right-0',
+          position === 'bottom-right' && 'bottom-4 md:right-4 md:left-auto',
           className
         )}
         {...rest}
       >
-        {/* Confirming color tokens from Rustam, will be updated accordingly */}
-        {type === 'success' && (
-          <Icon className="bg-chiChi">
-            <GenericCheckRounded className="text-[32px] text-dodoria" />
-          </Icon>
-        )}
-         {type === 'info' && (
-          <Icon className="bg-chiChi">
-            <GenericInfo className="text-[32px] text-dodoria" />
-          </Icon>
-        )}
-         {type === 'warning' && (
-          <Icon className="bg-chiChi">
-            <GenericAlarm className="text-[32px] text-dodoria" />
-          </Icon>
-        )}
-         {type === 'error' && (
-          <Icon className="bg-chiChi">
-            <GenericClose className="text-[32px] text-dodoria" />
-          </Icon>
-        )}
-        {type ? <Content>{children}</Content> : children}
+        {children}
       </ToastPrimitive.Root>
-      <ToastPrimitive.Viewport /> 
+      <ToastPrimitive.Viewport />
     </ToastPrimitive.Provider>
   );
 };
@@ -145,21 +115,9 @@ const Content: React.FC<ContentProps> = ({ children, className }) => {
   return <div className={classNames(className)}>{children}</div>;
 };
 
-const Icon: React.FC<IconProps> = ({
-  backgroundColor,
-  children,
-  className,
-}) => {
+const Icon: React.FC<IconProps> = ({ children, className }) => {
   return (
-    <div
-      className={classNames(
-        'p-1 rounded-lg',
-        backgroundColor ? backgroundColor : '',
-        className
-      )}
-    >
-      {children}
-    </div>
+    <div className={classNames('p-1 rounded-lg', className)}>{children}</div>
   );
 };
 
@@ -171,12 +129,48 @@ const Close: React.FC = () => {
   );
 };
 
+const Success: React.FC = () => {
+  return (
+    <Icon className="bg-chiChi">
+      <GenericCheckRounded className="text-[32px] text-dodoria" />
+    </Icon>
+  );
+};
+
+const Info: React.FC = () => {
+  return (
+    <Icon className="bg-chiChi">
+      <GenericInfo className="text-[32px] text-dodoria" />
+    </Icon>
+  );
+};
+
+const Error: React.FC = () => {
+  return (
+    <Icon className="bg-chiChi">
+      <GenericClose className="text-[32px] text-dodoria" />
+    </Icon>
+  );
+};
+
+const Warning: React.FC = () => {
+  return (
+    <Icon className="bg-chiChi">
+      <GenericAlarm className="text-[32px] text-dodoria" />
+    </Icon>
+  );
+};
+
 const Snackbar = Object.assign(SnackbarRoot, {
   Content,
   Header,
   Message,
   Close,
   Icon,
+  Success,
+  Info,
+  Error,
+  Warning,
 });
 
 export default Snackbar;
