8535f4b5367bfe64f58758fbf015f2746c901119
yarema184 <yuliana.yarema@heathmont.net>
2022-11-08 13:08:50 +0200

    fix: use PageSection

--- next-docs/components/ComponentAnatomy.tsx
+++ next-docs/components/ComponentAnatomy.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
+import PageSection from './PageSection';
 
 const ComponentAnatomy: React.FC = ({ children }) => {
   return (
-    <div className="flex flex-col gap-6">
-      <h2 className="text-moon-24 font-medium">Anatomy</h2>
+    <PageSection title="Anatomy">
       <div className="p-4 flex bg-goku rounded-moon-s-sm">
         <pre className="w-full overflow-scroll text-moon-14 text-bulma">
           {children}
         </pre>
       </div>
-    </div>
+    </PageSection>
   );
 };
 
