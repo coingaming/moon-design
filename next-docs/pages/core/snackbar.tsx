7c8c1f9b5d11a36986c41aacc8eec129491ccda1
Oladetoun Temitayo Micheal <oladetountemitayo@gmail.com>
2022-11-14 12:37:50 +0200

    feat:BASE1-3301 Snackbar on Moon Design Tailwind (#1963)
    * feat: BASE1-3301 new snackbar on Moon Design
    
    * feat: BASE1-3001 new snackbar component on Tailwind
    
    * feat: BASE1-3001 new snackbar component on Tailwind
    
    * feat: BASE1-3301 Snackbar Implementation
    
    * feat: BASE1-3301 Snackbar Implementation
    
    * feat: BASE1-3301 Snackbar Implementation
    
    * feat: BASE1-3301 Snackbar Implementation
    
    * feat: BASE1-3301 Snackbar Implementation
    
    * feat: BASE1-3301 Snackbar Implementation
    
    * feat: BASE1-3301 Snackbar Implementation
    
    * feat: BASE1-3301 Snackbar Implementation
    
    * fix: add unit tests
    
    * fix: BASE1-3301 Add Snackbar to Moon Design
    
    * fix: BASE1-3301 Update Snapshot tests due to additional useCallback on example components
    
    Co-authored-by: Dmytro Kireiev <dmytro.kireiev@gmail.com>

---
+++ next-docs/pages/core/snackbar.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import Preview from '../../components/codePreview/Preview';
+import ComponentPageDescription from '../../components/ComponentPageDescription';
+import PropsTable from '../../components/PropsTable';
+import Default from '../../public/examples/snackbarTW/Default';
+import Positions from '../../public/examples/snackbarTW/Positions';
+import Types from '../../public/examples/snackbarTW/Types';
+import Variants from '../../public/examples/snackbarTW/Variants';
+import useExamples from '../../utils/useExamples';
+
+const Example = () => {
+  const examples = useExamples('SnackbarTW');
+  return (
+    <>
+      <ComponentPageDescription title="Snackbar" isInProgress>
+        <p>
+          A type of alert which appears in a layer above other content, visually
+          similar to a mobile or desktop push notification.
+        </p>
+      </ComponentPageDescription>
+      <Preview
+        title="Default"
+        preview={<Default />}
+        code={examples ? examples.Default : 'Loading'}
+      />
+      <Preview
+        title="Snackbar positions"
+        preview={<Positions />}
+        code={examples ? examples.Positions : 'Loading'}
+      />
+      <Preview
+        title="Snackbar types"
+        preview={<Types />}
+        code={examples ? examples.Types : 'Loading'}
+      />
+      <Preview
+        title="Snackbar variants"
+        preview={<Variants />}
+        code={examples ? examples.Variants : 'Loading'}
+      />
+      <PropsTable
+        title="Snackbar props"
+        data={[
+          {
+            name: 'autoClose',
+            type: 'number',
+            required: false,
+            default: 6000,
+            description: 'Change the duration the snackbar takes',
+          },
+          {
+            name: 'children',
+            type: 'React.ReactNode',
+            required: false,
+            default: '-',
+            description: 'Children content',
+          },
+          {
+            name: 'className',
+            type: 'Tailwind classes',
+            required: false,
+            default: '-',
+            description: 'Change the styles',
+          },
+          {
+            name: 'containerClassName',
+            type: 'Tailwind classes',
+            required: false,
+            default: '-',
+            description: 'Change the styles of the icon container',
+          },
+          {
+            name: 'iconClassName',
+            type: 'Tailwind classes',
+            required: false,
+            default: '-',
+            description: 'Change the styles of the icon',
+          },
+          {
+            name: 'position',
+            required: true,
+            type: 'top-left | top-center | top-right | bottom-left | bottom-center | bottom-right',
+            default: 'top-right',
+            description: 'Change the position of snackbar',
+          },
+        ]}
+      />
+    </>
+  );
+};
+
+export default Example;
