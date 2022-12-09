854390ab3b10b024c89a91cbf0132656e410c6c5
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2022-12-09 15:38:05 +0200

    Feat/theme empire (#2020)
    * empire
    
    * fix: update index.ts file
    
    * feat: add empire theme
    
    * feat: add empire theme
    
    Co-authored-by: Daniil Sugonyaev <daniil.sugonyaev@yolo.com>

--- workspaces/themes/src/index.ts
+++ workspaces/themes/src/index.ts
@@ -1,2 +1,9 @@
 import './betadda.css';
 import './bitcasino.css';
+import './empire.css';
+import './lab.css';
+import './livecasino.css';
+import './moon.css';
+import './partners.css';
+import './sportsbet.css';
+import './tradeart.css';
