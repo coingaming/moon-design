306b27f016be1d58cd4c6fba7f3aebc90ae202ab
SNSemenova <svetlana.semenova@heathmont.net>
2022-12-15 12:00:35 +0200

    feat: accordion component in headless approach (#2025)
    * feat: accordion component in headless approach
    
    * updated pnpm lock file
    
    * fix: typescript fixes for accordion
    
    * fix: update pnpm-lock file
    
    * fix: pnpm install
    
    * fix: pnpm install with older version
    
    * fix: update test
    
    * fix: remove test for accordeon button

--- next-docs/public/examples/accordionTW/Disabled.tsx
+++ next-docs/public/examples/accordionTW/Disabled.tsx
@@ -1,16 +1,24 @@
 import { Accordion } from '@heathmont/moon-core-tw';
+import { ControlsChevronRight } from '@heathmont/moon-icons-tw';
 
 const Example = () => (
-  <Accordion title="Test accordion" disabled>
-    <div className="text-moon-14">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </div>
+  <Accordion>
+    <Accordion.Item disabled value="item-1">
+      <Accordion.Header className="moon-open:[&_svg]:rotate-90" disabled>
+        <Accordion.Button>
+          <span>Test accordion</span>
+          <ControlsChevronRight
+            className="text-trunks text-moon-16 transition-transform transition-200"
+          />
+        </Accordion.Button>
+      </Accordion.Header>
+      <Accordion.Content>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </Accordion.Content>
+    </Accordion.Item>
   </Accordion>
 );
 
---
+++ next-docs/public/examples/accordionTW/Divider.tsx
@@ -0,0 +1,25 @@
+import { Accordion } from '@heathmont/moon-core-tw';
+import { ControlsChevronRight } from '@heathmont/moon-icons-tw';
+
+const Example = () => (
+  <Accordion>
+      <Accordion.Item value="item-3">
+        <Accordion.Header className="border-b">
+          <Accordion.Button className="moon-open:[&_svg]:rotate-90">
+            <span>Test accordion</span>
+            <ControlsChevronRight
+              className="text-trunks text-moon-16 transition-transform transition-200"
+            />
+          </Accordion.Button>
+        </Accordion.Header>
+        <Accordion.Content className="pt-2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Content>
+      </Accordion.Item>
+    </Accordion>
+);
+
+export default Example;
