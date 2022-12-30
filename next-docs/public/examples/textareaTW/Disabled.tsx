8ea2f78063d351cbda5d34d166281d5e2c7257ea
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2022-12-30 14:38:19 +0200

    feat: add textarea [MDS-322] (#2041)
    * feat: add textarea [MDS-322]
    
    * feat: add textarea [MDS-322]
    
    * fix: replace PropsTable and TokenTable with Table component
    
    * fix: revert tables [MDS-322]
    
    * fix: revert component pages tables [MDS-322]
    
    * fix: revert core pages tables [MDS-322]
    
    * fix: revert  pages tables [MDS-322]

---
+++ next-docs/public/examples/textareaTW/Disabled.tsx
@@ -0,0 +1,5 @@
+import { Textarea } from '@heathmont/moon-core-tw';
+
+const Example = () => <Textarea disabled placeholder="Disabled" />;
+
+export default Example;
--- workspaces/core/src/index.ts
+++ workspaces/core/src/index.ts
@@ -52,6 +52,8 @@ export { default as TextInput } from './textInput/TextInput';
 export * from './textInput/TextInput';
 export { default as TextInputGroup } from './textInputGroup/TextInputGroup';
 export * from './textInputGroup/TextInputGroup';
+export { default as Textarea } from './textarea/Textarea';
+export * from './textarea/Textarea';
 export { default as Tooltip } from './tooltip/Tooltip';
 export * from './tooltip/Tooltip';
 export { default as mergeClassnames } from './utils/mergeClassnames';
