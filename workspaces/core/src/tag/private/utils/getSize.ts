c1b3dd219597ec324ee33a2446828a46ee5c5725
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2023-01-20 15:20:11 +0200

    Mds 362 (#2072)
    * feat: update Tag component [MDS-362]
    
    * feat: update Tag component [MDS-362]

--- workspaces/core/src/tag/private/utils/getSize.ts
+++ workspaces/core/src/tag/private/utils/getSize.ts
@@ -3,26 +3,26 @@ import type TagProps from '../types/TagProps';
 const setSpacing = ({ size, iconRight, iconLeft }: TagProps) => {
   if (size === '2xs') {
     if (iconLeft && !iconRight) {
-      return 'py-0 ps-0.5 pe-2';
+      return 'py-0 ps-0.5 pe-2 h-4';
     }
     if (!iconLeft && iconRight) {
-      return 'py-0 ps-2 pe-0.5';
+      return 'py-0 ps-2 pe-0.5 h-4';
     }
     if (iconLeft && iconRight) {
-      return 'py-0 px-0.5';
+      return 'py-0 px-0.5 h-4';
     }
-    return 'py-0 px-2';
+    return 'py-0 px-2 h-4';
   }
   if (iconLeft && !iconRight) {
-    return 'py-1 ps-1 pe-2';
+    return 'py-1 ps-1 pe-2 h-6';
   }
   if (!iconLeft && iconRight) {
-    return 'py-1 ps-2 pe-1';
+    return 'py-1 ps-2 pe-1 h-6';
   }
   if (iconLeft && iconRight) {
-    return 'p-1';
+    return 'p-1 h-6';
   }
-  return 'py-1 px-2';
+  return 'py-1 px-2 h-6';
 };
 
 export default setSpacing;
--- workspaces/core/src/tag/private/utils/setHeight.ts
+++
@@ -1,7 +0,0 @@
-import type TagProps from '../types/TagProps';
-
-const setHeight = ({ size }: TagProps) => {
-  return size === '2xs' ? 'h-4' : 'h-6';
-};
-
-export default setHeight;
