8414582261eb0870cf8da3541234db7eaa79563d
yarema184 <yuliana.yarema@heathmont.net>
2023-02-13 15:36:01 +0200

    fix: set autoFocus as props for search

--- next-docs/public/examples/search/Default.tsx
+++ next-docs/public/examples/search/Default.tsx
@@ -1,55 +1,61 @@
-import { useState } from "react";
-import { MenuItem, Search, searchFilterItems, searchGetItemIndex, useOpenSearch } from '@heathmont/moon-core-tw';
+import { useState } from 'react';
+import {
+  MenuItem,
+  Search,
+  searchFilterItems,
+  searchGetItemIndex,
+  useOpenSearch,
+} from '@heathmont/moon-core-tw';
 
 const Example = () => {
   const [open, setOpen] = useState<boolean>(false);
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
 
   const filteredItems = searchFilterItems(
     [
       {
-        heading: "Home",
-        id: "home",
+        heading: 'Home',
+        id: 'home',
         items: [
           {
-            id: "home",
-            children: "Home",
-            href: "#home",
+            id: 'home',
+            children: 'Home',
+            href: '#home',
           },
           {
-            id: "settings",
-            children: "Settings",
-            href: "#settings",
+            id: 'settings',
+            children: 'Settings',
+            href: '#settings',
           },
           {
-            id: "projects",
-            children: "Projects",
+            id: 'projects',
+            children: 'Projects',
             closeOnSelect: false,
             onClick: () => {
-              alert("projects");
+              alert('projects');
             },
           },
         ],
       },
       {
-        heading: "Other",
-        id: "advanced",
+        heading: 'Other',
+        id: 'advanced',
         items: [
           {
-            id: "developer-settings",
-            children: "Developer settings",
-            href: "#developer-settings",
+            id: 'developer-settings',
+            children: 'Developer settings',
+            href: '#developer-settings',
           },
           {
-            id: "privacy-policy",
-            children: "Privacy policy",
-            href: "#privacy-policy",
+            id: 'privacy-policy',
+            children: 'Privacy policy',
+            href: '#privacy-policy',
           },
           {
-            id: "log-out",
-            children: "Log out",
+            id: 'log-out',
+            children: 'Log out',
             onClick: () => {
-              alert("Logging out...");
+              alert('Logging out...');
             },
           },
         ],
@@ -62,39 +68,50 @@ const Example = () => {
 
   return (
     <div className="w-full xl:mx-32">
-    <Search
-      onChangeSearch={setSearch}
-      onChangeOpen={setOpen}
-      search={search}
-      isOpen={open}
-      clear={Clear}
-    >
-      {filteredItems.length ? (
-        filteredItems.map((list: any) => (
-          <Search.List key={list.id}>
-            <Search.ListHeading>{list.heading}</Search.ListHeading>
-            {list.items.map(({ id, children, href, ...rest }: any) => (
-              <Search.ListItem
-                key={id}
-                index={searchGetItemIndex(filteredItems, id)}
-                closeOnSelect={true}
-                {...rest}
-              >
-                {(selected: boolean) => href ? <a href={href}><MenuItem isActive={selected}>
-                  <MenuItem.Title>{children}</MenuItem.Title>
-                  <span className="text-moon-12 text-trunks">{href}</span>
-                </MenuItem></a> : <MenuItem isActive={selected}>
-                  <MenuItem.Title>{children}</MenuItem.Title>
-                  <span className="text-moon-12 text-trunks">Action</span>
-                </MenuItem>}
-              </Search.ListItem>
-            ))}
-          </Search.List>
-        ))
-      ) : (
-        <Search.FreeSearchAction />
-      )}
-    </Search>
+      <Search
+        onChangeSearch={setSearch}
+        onChangeOpen={setOpen}
+        search={search}
+        isOpen={open}
+        clear={Clear}
+        autoFocus={true}
+      >
+        {filteredItems.length ? (
+          filteredItems.map((list: any) => (
+            <Search.List key={list.id}>
+              <Search.ListHeading>{list.heading}</Search.ListHeading>
+              {list.items.map(({ id, children, href, ...rest }: any) => (
+                <Search.ListItem
+                  key={id}
+                  index={searchGetItemIndex(filteredItems, id)}
+                  closeOnSelect={true}
+                  {...rest}
+                >
+                  {(selected: boolean) =>
+                    href ? (
+                      <a href={href}>
+                        <MenuItem isActive={selected}>
+                          <MenuItem.Title>{children}</MenuItem.Title>
+                          <span className="text-moon-12 text-trunks">
+                            {href}
+                          </span>
+                        </MenuItem>
+                      </a>
+                    ) : (
+                      <MenuItem isActive={selected}>
+                        <MenuItem.Title>{children}</MenuItem.Title>
+                        <span className="text-moon-12 text-trunks">Action</span>
+                      </MenuItem>
+                    )
+                  }
+                </Search.ListItem>
+              ))}
+            </Search.List>
+          ))
+        ) : (
+          <Search.FreeSearchAction />
+        )}
+      </Search>
     </div>
   );
 };
