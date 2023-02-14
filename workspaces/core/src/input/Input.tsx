797fc924fd834ee4701142aa407c93982b957fac
yarema184 <yuliana.yarema@heathmont.net>
2023-02-14 15:25:44 +0200

    fix: ts issues and focus-visible

--- workspaces/core/src/input/Input.tsx
+++ workspaces/core/src/input/Input.tsx
@@ -48,12 +48,13 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
           'block w-full max-w-full py-0 px-4 m-0 appearance-none text-[1rem] text-bulma transition-shadow box-border relative z-[2]',
           'bg-gohan shadow-input hover:shadow-input-hov',
           'focus:shadow-input-focus focus:outline-none',
+          'focus-visible::shadow-input-focus focus-visible::outline-none',
           error &&
-            'shadow-input-err hover:shadow-input-err focus:shadow-input-err',
+            'shadow-input-err hover:shadow-input-err focus:shadow-input-err focus-visible:shadow-input-err',
           getSizeStyles(size as string),
           'before:box-border after:box-border',
           'placeholder:text-trunks placeholder:opacity-100 placeholder:transition-opacity placeholder:delay-75',
-          'read-only:outline-0 read-only:border-none read-only:cursor-not-allowed read-only:hover:shadow-input read-only:focus:shadow-input',
+          'read-only:outline-0 read-only:border-none read-only:cursor-not-allowed read-only:hover:shadow-input read-only:focus:shadow-input read-only:focus-visible:shadow-input',
           type === 'number' && 'input-number-clear',
           type === 'date' && 'input-d',
           type === 'date' && isRtl && 'input-d-rtl',
@@ -63,7 +64,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
           type === 'datetime-local' && isRtl && 'input-dt-local-rtl',
           size === 'lg' && 'input-lg-dt-shared',
           'input-dt-shared',
-          'invalid:shadow-input-err invalid:hover:shadow-input-err invalid:focus:shadow-input-err',
+          'invalid:shadow-input-err invalid:hover:shadow-input-err invalid:focus:shadow-input-err invalid:focus-visible:shadow-input-err',
           disabled && 'opacity-30 cursor-not-allowed',
           className && className
         )}
