cc4e58d09fd3268894d89df5c3aaa3de9dcd88f9
Svetlana Semenova <svetlana.semenova@heathmont.net>
2023-03-08 13:39:29 +0200

    feat: add new Bitcasino themes for styled components

---
+++ packages/themes/src/newBitcasinoDark/newBitcasinoDark.ts
@@ -0,0 +1,88 @@
+import avertaStd from '../sharedTokens/avertaStd';
+import componentsTokens from '../sharedTokens/componentsTokens';
+import sharedTokens, { Theme } from '../sharedTokens/sharedTokens';
+import supportColors from '../supportColors/supportColors';
+import supportiveColors from '../v2/colors/supportiveColors';
+
+const boxShadow =
+  '0px 2.4px 7.2px rgba(0, 0, 0, 0.18), 0px 12.8px 28.8px rgba(0, 0, 0, 0.22)';
+
+const color = {
+  hit: {
+    120: '#000000',
+    100: '#000000',
+    80: '#000000',
+  },
+  goku: {
+    100: '#101010',
+    80: '#101010',
+    40: '#101010',
+    10: '#101010',
+  },
+  gohan: {
+    100: '#1E1E1E',
+    80: '#1E1E1E',
+    40: '#1E1E1E',
+    10: '#1E1E1E',
+  },
+  beerus: {
+    100: '#343434',
+  },
+  bulma: {
+    100: '#FFFFFF',
+  },
+  trunks: {
+    100: '#858585',
+  },
+};
+
+const colorNew = {
+  piccolo: '#FFFFFF',
+  hit: '#000000',
+  beerus: '#343434',
+  goku: '#101010',
+  gohan: '#1E1E1E',
+  bulma: '#FFFFFF',
+  trunks: '#858585',
+  goten: '#FFFFFF',
+  popo: '#000000',
+};
+
+const newBitcasinoDark: Theme = {
+  ...sharedTokens,
+  brand: 'Bitcasino.io',
+  fontFace: avertaStd.fontFace,
+  fontFamily: avertaStd.fontStack,
+  fontSize: {
+    body: 14,
+  },
+  fontWeight: avertaStd.fontWeight,
+  boxShadow,
+  radius: {
+    small: 4,
+    default: 8,
+    largest: 8,
+  },
+  color: {
+    ...supportColors,
+    text: color.bulma[100],
+    background: color.goku[100],
+    piccolo: {
+      120: '#FFFFFF',
+      100: '#FFFFFF',
+      80: '#FFFFFF',
+    },
+    goten: {
+      100: '#FFFFFF',
+    },
+    ...color,
+  },
+  colorNew: {
+    ...colorNew,
+    ...supportiveColors,
+  },
+  ...componentsTokens(colorNew),
+  colorScheme: 'dark',
+};
+
+export default newBitcasinoDark;
---
+++ packages/themes/src/newBitcasinoLight/newBitcasinoLight.ts
@@ -0,0 +1,70 @@
+import bitcasinoDark from '../bitcasinoDark/bitcasinoDark';
+import componentsTokens from '../sharedTokens/componentsTokens';
+import { Theme } from '../sharedTokens/sharedTokens';
+import supportiveColors from '../v2/colors/supportiveColors';
+
+const color = {
+  hit: {
+    120: '#FFFFFF',
+    100: '#FFFFFF',
+    80: '#FFFFFF',
+  },
+  goku: {
+    100: '#FFFFFF',
+    80: '#FFFFFF',
+    40: '#FFFFFF',
+    10: '#FFFFFF',
+  },
+  gohan: {
+    100: '#F2F2F2',
+    80: '#F2F2F2',
+    40: '#F2F2F2',
+    10: '#F2F2F2',
+  },
+  beerus: {
+    100: '#E8E8E8',
+  },
+  bulma: {
+    100: '#1B1414',
+  },
+  trunks: {
+    100: '#858585',
+  },
+};
+
+const colorNew = {
+  piccolo: '#0F0F0F',
+  hit: '#FFFFFF',
+  beerus: '#E8E8E8',
+  goku: '#FFFFFF',
+  gohan: '#F2F2F2',
+  bulma: '#1B1414',
+  trunks: '#858585',
+  goten: '#FFFFFF',
+  popo: '#000000',
+};
+
+const { color: bitcasinoDarkColors } = bitcasinoDark;
+
+const newBitcasinoLight: Theme = {
+  ...bitcasinoDark,
+  color: {
+    ...bitcasinoDarkColors,
+    text: color.bulma[100],
+    background: color.goku[100],
+    piccolo: {
+      120: '#0F0F0F',
+      100: '#0F0F0F',
+      80: '#0F0F0F',
+    },
+    ...color,
+  },
+  colorNew: {
+    ...colorNew,
+    ...supportiveColors,
+  },
+  ...componentsTokens(colorNew),
+  colorScheme: 'light',
+};
+
+export default newBitcasinoLight;
