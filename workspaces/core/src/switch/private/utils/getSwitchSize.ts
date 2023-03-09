3e573228f869584679ed7fdade4cc54b9b3974e2
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2023-03-09 11:07:19 +0200

    fix: Switch width

--- workspaces/core/src/switch/private/utils/getSwitchSize.ts
+++ workspaces/core/src/switch/private/utils/getSwitchSize.ts
@@ -3,7 +3,7 @@ import type Size from '../types/Size';
 const getSwitchSize = (size?: Size) => {
   switch (size) {
     case '2xs':
-      return 'w-8 h-4 p-0.5';
+      return 'w-7 h-4 p-0.5';
     case 'xs':
       return 'w-11 h-6 p-1';
     case 'sm':
