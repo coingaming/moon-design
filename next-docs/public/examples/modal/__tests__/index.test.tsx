2e718bf9100d968f8713d592aaf81d95093d1b7a
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2023-03-23 16:59:35 +0200

    fix: rewrite Modal tests [MDS-454]

--- next-docs/public/examples/modal/__tests__/index.test.tsx
+++ next-docs/public/examples/modal/__tests__/index.test.tsx
@@ -2,87 +2,59 @@
  * @jest-environment jsdom
  */
 
-import React from 'react';
-import {
-  moonDesignLight,
-  moonDesignDark,
-  ThemeProvider,
-} from '@heathmont/moon-themes';
-import renderer from 'react-test-renderer';
+import '../../../../__mocks__/ResizeObserver';
 import BigContent from '../BigContent';
 import Default from '../Default';
 import StyledContent from '../StyledContent';
 import WithSelect from '../WithSelect';
+import { act, cleanup, fireEvent, render } from '@testing-library/react';
 
-const renderWithLightTheme = (component: JSX.Element) => (
-  <ThemeProvider theme={moonDesignLight}>{component}</ThemeProvider>
-);
+const withRtl = (component: JSX.Element) => <div dir="rtl">{component}</div>;
 
-const renderWithDarkTheme = (component: JSX.Element) => (
-  <ThemeProvider theme={moonDesignDark}>{component}</ThemeProvider>
-);
-
-const renderWithRtl = (component: JSX.Element) => (
-  <div dir="rtl">{component}</div>
-);
-
-describe('Modal in Light Theme', () => {
-  it('renders Default', () => {
-    const testRenderer = renderer.create(renderWithLightTheme(<Default />));
-    expect(testRenderer.toJSON()).toMatchSnapshot();
+describe('Modal', () => {
+  afterEach(cleanup);
+  it('renders Default', async () => {
+    const tree = render(<Default />);
+    await act(() => fireEvent.click(tree.getByRole('button')));
+    expect(tree).toMatchSnapshot();
   });
-  it('renders BigContent', () => {
-    const testRenderer = renderer.create(renderWithLightTheme(<BigContent />));
-    expect(testRenderer.toJSON()).toMatchSnapshot();
+  it('renders BigContent', async () => {
+    const tree = render(<BigContent />);
+    await act(() => fireEvent.click(tree.getByRole('button')));
+    expect(tree).toMatchSnapshot();
   });
-  it('renders StyledContent', () => {
-    const testRenderer = renderer.create(
-      renderWithLightTheme(<StyledContent />)
-    );
-    expect(testRenderer.toJSON()).toMatchSnapshot();
+  it('renders StyledContent', async () => {
+    const tree = render(<StyledContent />);
+    await act(() => fireEvent.click(tree.getByRole('button')));
+    expect(tree).toMatchSnapshot();
   });
-  it('renders WithSelect', () => {
-    const testRenderer = renderer.create(renderWithLightTheme(<WithSelect />));
-    expect(testRenderer.toJSON()).toMatchSnapshot();
-  });
-});
-
-describe('Modal in Dark Theme', () => {
-  it('renders Default', () => {
-    const testRenderer = renderer.create(renderWithDarkTheme(<Default />));
-    expect(testRenderer.toJSON()).toMatchSnapshot();
-  });
-  it('renders BigContent', () => {
-    const testRenderer = renderer.create(renderWithDarkTheme(<BigContent />));
-    expect(testRenderer.toJSON()).toMatchSnapshot();
-  });
-  it('renders StyledContent', () => {
-    const testRenderer = renderer.create(
-      renderWithDarkTheme(<StyledContent />)
-    );
-    expect(testRenderer.toJSON()).toMatchSnapshot();
-  });
-  it('renders WithSelect', () => {
-    const testRenderer = renderer.create(renderWithDarkTheme(<WithSelect />));
-    expect(testRenderer.toJSON()).toMatchSnapshot();
+  it('renders WithSelect', async () => {
+    const tree = render(<WithSelect />);
+    await act(() => fireEvent.click(tree.getByRole('button')));
+    expect(tree).toMatchSnapshot();
   });
 });
 
 describe('Modal in RTL', () => {
-  it('renders Default', () => {
-    const testRenderer = renderer.create(renderWithRtl(<Default />));
-    expect(testRenderer.toJSON()).toMatchSnapshot();
+  afterEach(cleanup);
+  it('renders Default', async () => {
+    const tree = render(withRtl(<Default />));
+    await act(() => fireEvent.click(tree.getByRole('button')));
+    expect(tree).toMatchSnapshot();
   });
-  it('renders BigContent', () => {
-    const testRenderer = renderer.create(renderWithRtl(<BigContent />));
-    expect(testRenderer.toJSON()).toMatchSnapshot();
+  it('renders BigContent', async () => {
+    const tree = render(withRtl(<BigContent />));
+    await act(() => fireEvent.click(tree.getByRole('button')));
+    expect(tree).toMatchSnapshot();
   });
-  it('renders StyledContent', () => {
-    const testRenderer = renderer.create(renderWithRtl(<StyledContent />));
-    expect(testRenderer.toJSON()).toMatchSnapshot();
+  it('renders StyledContent', async () => {
+    const tree = render(withRtl(<StyledContent />));
+    await act(() => fireEvent.click(tree.getByRole('button')));
+    expect(tree).toMatchSnapshot();
   });
-  it('renders WithSelect', () => {
-    const testRenderer = renderer.create(renderWithRtl(<WithSelect />));
-    expect(testRenderer.toJSON()).toMatchSnapshot();
+  it('renders WithSelect', async () => {
+    const tree = render(withRtl(<WithSelect />));
+    await act(() => fireEvent.click(tree.getByRole('button')));
+    expect(tree).toMatchSnapshot();
   });
 });
