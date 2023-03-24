aaa4817214367e02aed865c6ea80e61b643b1ef1
moon-ds <entwickler.js@gmail.com>
2023-03-24 22:31:07 +0500

    feat: Select, Trigger and Hint options added

---
+++ workspaces/core/src/combobox/private/types/SelectProps.ts
@@ -0,0 +1,10 @@
+type SelectProps = {
+  label?: JSX.Element | string;
+  placeholder?: JSX.Element | string;
+  open?: boolean;
+  value?: undefined;
+  innerLabel?: boolean;
+  className?: string;
+};
+
+export default SelectProps
