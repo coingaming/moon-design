3baf5ef6ddf1f63ac9829294ec6db306a65f75ea
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2023-04-05 12:07:09 +0300

    fix: update footer [MDS-475]

--- next-docs/components/Footer.tsx
+++ next-docs/components/Footer.tsx
@@ -1,47 +1,57 @@
 import Logo from './sidebar/Logo';
 
 const Footer = () => (
-  <footer className="relative z-10 flex gap-x-8 gap-y-6 pt-16 pb-6 flex-wrap items-center text-moon-16 text-bulma justify-center md:justify-between">
-    <div className="flex gap-x-8 gap-y-6 flex-wrap justify-center items-center">
-      <Logo height={16} />
-      <div className="flex gap-x-8 gap-y-4 flex-wrap justify-center items-center">
-        <a
-          href="https://github.com/coingaming/moon-design"
-          className="hover:underline"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-        <a
-          href="https://www.figma.com/community/file/1002945721703152933"
-          className="hover:underline"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Figma
-        </a>
-        <a
-          href="https://moonds.medium.com/"
-          className="hover:underline"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Medium
-        </a>
-        <a
-          href="https://www.linkedin.com/company/moon-io/"
-          className="hover:underline"
-          target="_blank"
-          rel="noreferrer"
-        >
-          LinkedIn
-        </a>
+  <footer className="theme-moon-dark relative -mx-5 xl:mx-0 mt-32 xl:mt-0 xl:fixed xl:z-40 xl:bottom-0 xl:ltr:left-80 xl:rtl:left-0 xl:ltr:right-0 xl:rtl:right-80 flex gap-x-8 gap-y-6 py-6 px-5 xl:px-20 2xl:px-32 flex-wrap items-center text-moon-16 text-bulma bg-goku justify-center md:justify-between">
+    <div className="flex gap-x-8 gap-y-6 flex-col xl:flex-row w-full max-w-screen-xl justify-between items-center">
+      <div className="flex gap-x-8 gap-y-6 flex-wrap justify-center items-center">
+        <Logo height={16} />
+        <div className="flex gap-x-8 gap-y-4 flex-wrap justify-center items-center">
+          <a
+            href="https://github.com/coingaming/moon-design"
+            className="hover:underline"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+          <a
+            href="https://www.figma.com/community/file/1002945721703152933"
+            className="hover:underline"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Figma
+          </a>
+          <a
+            href="https://moonds.medium.com/"
+            className="hover:underline"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Medium
+          </a>
+          <a
+            href="https://www.linkedin.com/company/moon-io/"
+            className="hover:underline"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LinkedIn
+          </a>
+        </div>
       </div>
+      <p>
+        With the help of{' '}
+        <a
+          href="https://yolo.com/"
+          className="hover:underline font-medium"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Yolo Group
+        </a>
+      </p>
     </div>
-    <p>
-      With the help of <span className="font-medium">Yolo Group</span>
-    </p>
   </footer>
 );
 
--- next-docs/components/sidebar/Version.tsx
+++ next-docs/components/sidebar/Version.tsx
@@ -2,7 +2,7 @@ import { LogoMoonDesignShort } from '@heathmont/moon-assets';
 import packageInfo from '../../package.json';
 
 const Version = () => (
-  <div className="fixed bottom-0 left-0 w-80 py-6 px-8 flex text-moon-14 bg-goku gap-2 border-t border-t-beerus">
+  <div className="fixed bottom-0 ltr:left-0 rtl:right-0 w-80 py-6 px-8 flex text-moon-14 bg-goku gap-2 border-t border-t-beerus">
     <LogoMoonDesignShort fontSize="1.5rem" />
     <p>
       Version <span className="font-medium">{packageInfo.version}</span>
