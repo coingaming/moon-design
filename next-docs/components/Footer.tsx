032a42ce002d3c95eb382a3a5bb7ee1c16273f30
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2023-04-06 15:14:34 +0300

    fix: update footer (#2177)

--- next-docs/components/Footer.tsx
+++ next-docs/components/Footer.tsx
@@ -1,11 +1,11 @@
-import Logo from './sidebar/Logo';
+import Version from './sidebar/Version';
 
 const Footer = () => (
-  <footer className="theme-moon-dark relative -mx-5 xl:mx-0 mt-32 xl:mt-0 xl:fixed xl:z-40 xl:bottom-0 xl:ltr:left-80 xl:rtl:left-0 xl:ltr:right-0 xl:rtl:right-80 flex gap-x-8 gap-y-6 py-6 px-5 xl:px-20 2xl:px-32 flex-wrap items-center text-moon-16 text-bulma bg-goku justify-center md:justify-between">
-    <div className="flex gap-x-8 gap-y-6 flex-col xl:flex-row w-full max-w-screen-xl justify-between items-center">
-      <div className="flex gap-x-8 gap-y-6 flex-wrap justify-center items-center">
-        <Logo height={16} />
-        <div className="flex gap-x-8 gap-y-4 flex-wrap justify-center items-center">
+  <footer className="theme-moon-dark relative lg:fixed -mx-5 lg:mx-0 mt-16 lg:mt-0 lg:z-40 lg:bottom-0 inset-x-0 flex lg:flex-row gap-x-8 gap-y-6 2xl:ps-80 flex-wrap items-center text-moon-14 text-bulma bg-goku justify-center md:justify-between">
+    <div className="w-full py-5 px-5 lg:ps-8 lg:pe-5 xl:pe-20 2xl:px-32">
+      <div className="flex 2xl:max-w-screen-xl gap-x-4 gap-y-6 flex-col lg:flex-row w-full justify-between items-center">
+        <Version />
+        <div className="flex gap-x-4 gap-y-6 flex-wrap justify-center items-center text-trunks">
           <a
             href="https://github.com/coingaming/moon-design"
             className="hover:underline"
@@ -14,6 +14,7 @@ const Footer = () => (
           >
             Github
           </a>
+          <span>•</span>
           <a
             href="https://www.figma.com/community/file/1002945721703152933"
             className="hover:underline"
@@ -22,6 +23,7 @@ const Footer = () => (
           >
             Figma
           </a>
+          <span>•</span>
           <a
             href="https://moonds.medium.com/"
             className="hover:underline"
@@ -30,6 +32,7 @@ const Footer = () => (
           >
             Medium
           </a>
+          <span>•</span>
           <a
             href="https://www.linkedin.com/company/moon-io/"
             className="hover:underline"
@@ -39,18 +42,19 @@ const Footer = () => (
             LinkedIn
           </a>
         </div>
+        <p>
+          With the help of{' '}
+          <a
+            href="https://yolo.com/"
+            className="hover:underline font-medium"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Yolo Group
+          </a>{' '}
+          ❤️
+        </p>
       </div>
-      <p>
-        With the help of{' '}
-        <a
-          href="https://yolo.com/"
-          className="hover:underline font-medium"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Yolo Group
-        </a>
-      </p>
     </div>
   </footer>
 );
