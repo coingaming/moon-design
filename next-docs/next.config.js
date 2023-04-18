27a33fad0097f706b68e07f8f1f19218a6fa14cb
Karl Kallavus <232199+karl-kallavus@users.noreply.github.com>
2023-04-18 20:03:47 +0800

    fix & dedupe

--- next-docs/next.config.js
+++ next-docs/next.config.js
@@ -1,27 +1,6 @@
-// const analyzer = require('./utils/analyzer');
 const webpack = require('./utils/webpack');
 
-// const withTM = require('next-transpile-modules')([
-//   '@heathmont/moon-accordion',
-//   '@heathmont/moon-assets',
-//   '@heathmont/moon-charts',
-//   '@heathmont/moon-components',
-//   '@heathmont/moon-core',
-//   '@heathmont/moon-draggabletable',
-//   '@heathmont/moon-icons',
-//   '@heathmont/moon-pagination',
-//   '@heathmont/moon-popover',
-//   '@heathmont/moon-table',
-//   '@heathmont/moon-themes',
-//   '@heathmont/moon-utils',
-//   '@heathmont/moon-core-tw',
-// ]); // pass the modules you would like to see transpiled
-
-// // WARNING: This code does not work, works only the last export in the file. We need to delete or rewrite
-// module.exports = withTM();
-
 const config = {
-  // analyzer,
   webpack,
 };
 
