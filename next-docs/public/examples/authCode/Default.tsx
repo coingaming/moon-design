7e907bcf7b2e55f0201e10d2a55509528ca5f1bb
moon-ds <entwickler.js@gmail.com>
2023-05-11 17:31:04 +0500

    feat: try to apply react-hook-form in the Default example

--- next-docs/public/examples/authCode/Default.tsx
+++ next-docs/public/examples/authCode/Default.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
-import { AuthCode } from '@heathmont/moon-core-tw';
+import { AuthCode, Button, Form } from '@heathmont/moon-core-tw';
+import { Controller, useForm } from 'react-hook-form';
 
-const Example = () => (
-  <AuthCode
-    onChange={(value: string) => {}}
-  />
-);
+const Example = () => {
+  const { control, handleSubmit, setError, formState: { errors } } = useForm({
+    defaultValues: {
+      authCode: '',
+    }
+  });
+
+  const onSubmit = ({ authCode }: { authCode: string }) => {
+    alert(`Code sent: "${authCode}"`)
+  };
+/*
+  React.useEffect(() => {
+    setError("authCode", {
+      types: {
+        required: "This is a required field",
+        minLength: "Data length is less than required"
+      }
+    });
+  }, [setError]);
+*/
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)} className='flex flex-col justify-between items-end gap-y-2'>
+      <Controller
+        name='authCode'
+        control={control}
+        rules={{ minLength: 6, required: true }}
+        render={({ field }) => <AuthCode
+            {...field}
+            errorMessage={errors.authCode?.message}
+          />}
+        />
+      <Button as='button' type='submit' size='md'>Send code</Button>
+    </Form>
+  );
+}
 
 export default Example;
