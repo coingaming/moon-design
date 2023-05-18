80c225fb0103b86f13f467aa4683e11fd421890e
moon-ds <entwickler.js@gmail.com>
2023-05-18 14:08:31 +0500

    fix: try to add the filter function

--- next-docs/public/examples/searchCmdk/CommandMenu.tsx
+++ next-docs/public/examples/searchCmdk/CommandMenu.tsx
@@ -4,7 +4,6 @@ import {
   SearchCmdk,
 } from '@heathmont/moon-core-tw';
 
-
 const items = [
   { label: "Test1" },
   { label: "Test2" }
@@ -12,6 +11,7 @@ const items = [
 
 function CommandMenu() {
   const [open, setOpen] = useState(false);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
@@ -25,6 +25,8 @@ function CommandMenu() {
     document.addEventListener("keydown", down);
     return () => document.removeEventListener("keydown", down);
   }, []);
+
+  const filterItems = (value: string, search: string) => { console.log(value, search); return +value.includes(search); }
 
   return (
     <>
@@ -40,10 +42,13 @@ function CommandMenu() {
         open={open}
         onOpenChange={setOpen}
         label="Command Menu"
+        filter={filterItems}
       >
         <SearchCmdk.InputWrapper>
           <SearchCmdk.Icon />
           <SearchCmdk.Input
+            value={search}
+            onValueChange={setSearch}
             placeholder="Search for a component"
           />
           <SearchCmdk.Kbd onClick={() => setOpen(false)}>
@@ -55,19 +60,19 @@ function CommandMenu() {
           <SearchCmdk.NoResults>
             No Results
           </SearchCmdk.NoResults>
-          {items.map((item) => (
-            <SearchCmdk.ResultItem
-              key={item.label}
-              onSelect={() => {
-                setOpen(false);
-              }}
-            >
-              <MenuItem >
-                <MenuItem.Title>{item.label}</MenuItem.Title>
-                <span className="text-moon-14 text-piccolo">Action</span>
-              </MenuItem>
-            </SearchCmdk.ResultItem>
-          ))}
+            {items.map(({ label }) =>
+                <SearchCmdk.ResultItem
+                  key={label}
+                  onSelect={() => {
+                    setOpen(false);
+                  }}
+                >
+                  <MenuItem >
+                    <MenuItem.Title>{label}</MenuItem.Title>
+                    <span className="text-moon-14 text-piccolo">Action</span>
+                  </MenuItem>
+                </SearchCmdk.ResultItem>
+          )}
         </SearchCmdk.Result>
       </SearchCmdk>
     </>
