749ed91233bf612a0989b9dc65bdcab50b7b47d9
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2023-05-22 16:15:04 +0300

    fix: earth and moon rtl positioning [MDS-213] (#2213)
    * fix: eaeth and moon rtl positioning [MDS-213]
    
    * fix: rtl positioning of earth and moon [MDS-213]

--- next-docs/components/facing/MoonAndEarth.tsx
+++ next-docs/components/facing/MoonAndEarth.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { useViewportScroll, useTransform, motion } from 'framer-motion';
+import { useScroll, useTransform, motion } from 'framer-motion';
 import Image from 'next/image';
 import imageEarth from '../../public/earth.png';
 import imageMoon from '../../public/moon.png';
+import { useRtl } from '../rtl/RtlProvider';
 
 const MoonAndEarthAnimation = () => {
-  const { scrollYProgress } = useViewportScroll();
+  const { scrollYProgress } = useScroll();
   const earthScale = useTransform(
     scrollYProgress,
     (scrollYProgress) => 0.3 + scrollYProgress * 0.5
@@ -14,21 +15,26 @@ const MoonAndEarthAnimation = () => {
     scrollYProgress,
     (scrollYProgress) => 1 - scrollYProgress * 0.6
   );
-  const right = useTransform(scrollYProgress, [0, 1], [-500, -700]);
+  const { rtlEnabled } = useRtl();
+  const position = useTransform(scrollYProgress, [0, 1], [-500, -700]);
+  const className = rtlEnabled ? 'origin-top-right' : 'origin-top-left';
+  const xPosition = rtlEnabled
+    ? { left: position, right: 'auto' }
+    : { right: position, left: 'auto' };
   return (
     <>
       {/* Earth background image */}
       <motion.div
-        style={{ scale: earthScale, top: 300, right }}
-        className="origin-top-left fixed z-1 w-5/12"
+        style={{ scale: earthScale, ...xPosition }}
+        className={`${className} fixed z-1 top-72 w-5/12`}
       >
         <Image src={imageEarth} alt="Earth" />
       </motion.div>
 
       {/* Moon background image */}
       <motion.div
-        style={{ scale: moonScale, top: 250, right }}
-        className="origin-top-left fixed w-5/12"
+        style={{ scale: moonScale, ...xPosition }}
+        className={`${className} fixed top-60 w-5/12`}
       >
         <Image src={imageMoon} alt="Moon" />
       </motion.div>
