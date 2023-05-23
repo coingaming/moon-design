7849432105be3ac2d1eab1b7666e5a5c1e2959a9
moon-ds <entwickler.js@gmail.com>
2023-05-23 17:22:37 +0500

    fix: main page search fixed

--- next-docs/components/search/RenderResults.tsx
+++ next-docs/components/search/RenderResults.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { MenuItem, SearchCmdk } from '@heathmont/moon-core-tw';
 import actionHandler from './utils/actionHandler';
 import getAction, { Action } from './utils/getActions';
+import { SearchContext } from './SearchProvider';
 
 const RenderResults: React.FC<{ onSelectHandler?: (value: string) => void }> =
   ({ onSelectHandler }) => {
     const actions: Action[] = getAction();
+    const { search } = useContext(SearchContext);
+
+    const filterItems = (values: Action[], search: string) => { return values.filter(({ name }) => +name.includes(search)) };
+    const filteredActions = filterItems(actions, search || '');
+
     return (
       <SearchCmdk.Result>
         <SearchCmdk.NoResults>
           No results for your search...
         </SearchCmdk.NoResults>
-        {actions.map((item) => (
+        {filteredActions.map((item) => (
           <SearchCmdk.ResultItem key={item.id} onSelect={(v) => { actionHandler(item.perform, v, onSelectHandler) }}>
             <MenuItem onClick={() => item.perform()} className="hover:bg-goku">
               <MenuItem.Title>{item.name}</MenuItem.Title>
--- next-docs/components/search/SearchProvider.tsx
+++ next-docs/components/search/SearchProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 import { SearchCmdk } from '@heathmont/moon-core-tw';
 import useSearch from './hooks/useSearch';
 import RenderResult from './RenderResults';
@@ -6,6 +6,8 @@ import RenderResult from './RenderResults';
 type SearchState = {
   open?: boolean;
   setOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+  search?: string;
+  setSearch?: React.Dispatch<React.SetStateAction<string>>;
 };
 
 export const SearchContext = createContext<SearchState>({});
@@ -15,14 +17,19 @@ const SearchProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const { open, setOpen, closeSearch } = useSearch();
+  const [search, setSearch] = useState("");
 
   return (
-    <SearchContext.Provider value={{ open: open, setOpen: setOpen }}>
+    <SearchContext.Provider value={{ open: open, setOpen: setOpen, search: search, setSearch: setSearch }}>
       {open && <SearchCmdk.Overlay />}
-      <SearchCmdk open={open} onOpenChange={setOpen} label="Command Menu">
+      <SearchCmdk open={open} onOpenChange={setOpen} shouldFilter={false} label="Command Menu">
         <SearchCmdk.InputWrapper>
           <SearchCmdk.Icon />
-          <SearchCmdk.Input placeholder="Search for a component" />
+          <SearchCmdk.Input
+            value={search}
+            onValueChange={setSearch}
+            placeholder="Search for a component"
+          />
           <SearchCmdk.Kbd onClick={closeSearch}>Esc</SearchCmdk.Kbd>
         </SearchCmdk.InputWrapper>
         <SearchCmdk.Separator />
