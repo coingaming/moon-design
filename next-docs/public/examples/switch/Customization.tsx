4083d69c27598228c0094fd0264aa4f6c25aace2
Dmytro Kireiev <dmytro.kireiev@gmail.com>
2023-06-08 14:22:30 +0300

    fix: update Switch docs [MDS-550]

---
+++ next-docs/public/examples/switch/Customization.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { Switch } from '@heathmont/moon-core-tw';
+
+const Example = () => {
+  const [state, setState] = useState(true);
+  return (
+    <>
+      <Switch
+        checked={state}
+        onChange={setState}
+        className="bg-krillin moon-checked:bg-chichi"
+      />
+      <Switch
+        checked={state}
+        onChange={setState}
+        className="bg-nappa moon-checked:bg-roshi"
+      />
+    </>
+  );
+};
+
+export default Example;
