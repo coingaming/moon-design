2e0694553e48618ad93b6d88ab89ef172f17c187
yarema184 <yuliana.yarema@heathmont.net>
2023-10-16 11:46:46 +0300

    add input date custom examples

--- next-docs/public/examples/input/CustomDate.tsx
+++ next-docs/public/examples/input/CustomDate.tsx
@@ -1,18 +1,25 @@
 import { Input, Label } from '@heathmont/moon-core-tw';
+import { TimeCalendarDate } from '@heathmont/moon-icons-tw';
 
 const Example = () => (
   <>
     <div className="flex flex-col lg:flex-row justify-around items-end w-full gap-2">
       <div className="w-full">
         <Label>Date with placeholder</Label>
-        <Input
-          type="text"
-          aria-label="Date"
-          placeholder="yy/mm/dd"
-          onFocus={(e) => (e.target.type = 'date')}
-          onBlur={(e) => (e.target.type = 'text')}
-          pattern="[0-9]{2}/[0-9]{2}/[0-9]{2}"
-        />
+        <div className="relative">
+          <Input
+            type="text"
+            aria-label="Date"
+            placeholder="dd.mm.yyyy"
+            className="[&[type='date']::-webkit-calendar-picker-indicator]:bg-none cursor-default [&_.datetime-calendar-button_svg]:hidden"
+            onFocus={(e) => {
+              e.target.type = 'date';
+              e.target?.showPicker();
+            }}
+            onBlur={(e) => (e.target.type = 'text')}
+          />
+          <TimeCalendarDate className="text-bulma text-moon-24 absolute top-1/2 right-3 z-5 -mt-3 pointer-events-none " />
+        </div>
       </div>
       <div className="w-full">
         <Label htmlFor="time-type">Time</Label>
