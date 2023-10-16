37964d54cc3a0bc8285db6c9a49ab17ee8b03088
Karl Kallavus <232199+karl-kallavus@users.noreply.github.com>
2023-10-16 17:59:38 +0200

    quick nav

--- docs/app/components/server/accordion/page.tsx
+++ docs/app/components/server/accordion/page.tsx
@@ -6,12 +6,21 @@ import ExampleSection from '@/app/components/shared/ExampleSection';
 import { getExamples } from '@/app/utils/getExamples';
 import { MDX } from '@/components/MDX';
 
+function QuickNav({ items }: {items: string[]}) {
+  return <ul>
+    {items.map(item => <li key={item}><a href={`#${item}`}>{item}</a></li>)}
+  </ul>
+}
+
 export default async function Home() {
   const { server } = await getExamples();
+  const examplesList = Object.keys(server.accordion.examples);
+
   return (
     <div className="max-w-7xl flex flex-col gap-4 text-moon-14">
       <h1 className="font-medium text-moon-32">Accordion</h1>
       <MDX markdown={server.accordion.description} />
+      <QuickNav items={examplesList}/>
       <ExampleSection
         title="Default"
         component={<Default />}
