39b222fa627845e883dcaef172a5aa8b0beafa48
Karl Kallavus <232199+karl-kallavus@users.noreply.github.com>
2023-11-20 13:31:17 +0100

    fix: Minimap example (#2502)

--- next-docs/public/examples/table/MiniMap.tsx
+++ next-docs/public/examples/table/MiniMap.tsx
@@ -36,6 +36,78 @@ const Example = () => {
       ],
     },
     {
+      Header: 'Info1',
+      columns: [
+        {
+          Header: 'Age1',
+          accessor: 'age1',
+          width: 50,
+        },
+        {
+          Header: 'Visits1',
+          accessor: 'visits1',
+        },
+        {
+          Header: 'Activity1',
+          accessor: 'activity1',
+        },
+      ],
+    },
+    {
+      Header: 'Info2',
+      columns: [
+        {
+          Header: 'Age2',
+          accessor: 'age2',
+          width: 50,
+        },
+        {
+          Header: 'Visits2',
+          accessor: 'visits2',
+        },
+        {
+          Header: 'Activity2',
+          accessor: 'activity2',
+        },
+      ],
+    },
+    {
+      Header: 'Info3',
+      columns: [
+        {
+          Header: 'Age3',
+          accessor: 'age3',
+          width: 50,
+        },
+        {
+          Header: 'Visits3',
+          accessor: 'visits3',
+        },
+        {
+          Header: 'Activity3',
+          accessor: 'activity3',
+        },
+      ],
+    },
+    {
+      Header: 'Info4',
+      columns: [
+        {
+          Header: 'Age4',
+          accessor: 'age4',
+          width: 50,
+        },
+        {
+          Header: 'Visits4',
+          accessor: 'visits4',
+        },
+        {
+          Header: 'Activity4',
+          accessor: 'activity4',
+        },
+      ],
+    },
+    {
       Header: 'Progress',
       sticky: 'right',
       columns: [
@@ -55,9 +127,21 @@ const Example = () => {
         lastName: 'Test',
         age: <span>{Math.floor(index * 30)}</span>,
         visits: <span>{Math.floor(index * 100)}</span>,
+        activity: Math.floor(index * 100),
         progress: <span>{Math.floor(index * 100)}</span>,
         status: Math.floor(index * 100),
-        activity: Math.floor(index * 100),
+        age1: <span>{Math.floor(index * 30)}</span>,
+        visits1: <span>{Math.floor(index * 100)}</span>,
+        activity1: Math.floor(index * 100),
+        age2: <span>{Math.floor(index * 30)}</span>,
+        visits2: <span>{Math.floor(index * 100)}</span>,
+        activity2: Math.floor(index * 100),
+        age3: <span>{Math.floor(index * 30)}</span>,
+        visits3: <span>{Math.floor(index * 100)}</span>,
+        activity3: Math.floor(index * 100),
+        age4: <span>{Math.floor(index * 30)}</span>,
+        visits4: <span>{Math.floor(index * 100)}</span>,
+        activity4: Math.floor(index * 100),
       };
     });
   };
